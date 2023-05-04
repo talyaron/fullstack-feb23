cc9df9d6aecdb26e3c3cb85e82460fe075f832c9
ziv leloneck <119840698+zivll1991@users.noreply.github.com>
2023-05-04 13:22:22 +0300

    ver 1234

--- 04-Typescript/followup/zivll/dist/index.js
+++ 04-Typescript/followup/zivll/dist/index.js
@@ -2,7 +2,7 @@ var y = "My name is:";
 var x = "Ziv";
 console.log(y, x);
 var adult = 18;
-var userAge = 18;
+var userAge = prompt("מהו הגיל שלך?");
 if (userAge < adult) {
     console.log("You can not by any alcohol");
 }
