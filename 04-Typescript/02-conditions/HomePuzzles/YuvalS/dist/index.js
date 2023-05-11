44b92c2cfd284cb533fde9c61b39fb5f1b4af071
Shtig7 <128290647+Shtig7@users.noreply.github.com>
2023-05-11 20:19:49 +0300

    1

---
+++ 04-Typescript/02-conditions/HomePuzzles/YuvalS/dist/index.js
@@ -0,0 +1,14 @@
+// const Age = Number(prompt("Enter your age"))
+// if (Age >= 18) {
+//     document.write ("יכול לשתות")
+// }
+// else {
+//     document.write ("לא יכול לשתות")
+// }
+var gender = (prompt("what is your gender?"));
+if (gender === 'man') {
+    document.write("hello man");
+}
+else {
+    document.write("hello woman");
+}
---
+++ 04-Typescript/02-conditions/HomePuzzles/YuvalS/index.ts
@@ -0,0 +1,17 @@
+// const Age = Number(prompt("Enter your age"))
+// if (Age >= 18) {
+//     document.write ("יכול לשתות")
+// }
+
+// else {
+//     document.write ("לא יכול לשתות")
+// }
+
+const gender = (prompt("what is your gender?"))
+if (gender === 'man') {
+    document.write ("hello man")
+}
+
+else {
+    document.write ("hello woman")
+}
