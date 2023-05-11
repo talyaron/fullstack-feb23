af599855943f8327cc59971951731bf8f986f9b9
Shtig7 <128290647+Shtig7@users.noreply.github.com>
2023-05-11 20:36:43 +0300

    1

--- 04-Typescript/02-conditions/HomePuzzles/YuvalS/dist/index.js
+++ 04-Typescript/02-conditions/HomePuzzles/YuvalS/dist/index.js
@@ -5,10 +5,16 @@
 // else {
 //     document.write ("לא יכול לשתות")
 // }
-var gender = (prompt("what is your gender?"));
-if (gender === 'man') {
-    document.write("hello man");
+// const gender = (prompt("what is your gender?"))
+// if (gender === 'man') {
+//     document.write ("hello man")
+// }
+// else {
+//     document.write ("hello woman")
+// }
+function BMI(x, y) {
+    return x / (y * y);
 }
-else {
-    document.write("hello woman");
-}
+var weight = Number(prompt("Enter your weight"));
+var height = Number(prompt("Enter your height"));
+console.log(BMI(weight, height));
--- 04-Typescript/02-conditions/HomePuzzles/YuvalS/index.ts
+++ 04-Typescript/02-conditions/HomePuzzles/YuvalS/index.ts
@@ -7,11 +7,20 @@
 //     document.write ("לא יכול לשתות")
 // }
 
-const gender = (prompt("what is your gender?"))
-if (gender === 'man') {
-    document.write ("hello man")
+// const gender = (prompt("what is your gender?"))
+// if (gender === 'man') {
+//     document.write ("hello man")
+// }
+
+// else {
+//     document.write ("hello woman")
+// }
+
+function BMI (x: number, y: number): number {
+    return x / (y*y)
 }
 
-else {
-    document.write ("hello woman")
-}
+const weight = Number(prompt("Enter your weight"))
+const height = Number(prompt("Enter your height"))
+
+console.log(BMI(weight, height))
