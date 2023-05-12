b49f113f24796e1fe8fabfa6a14f4b305cf90f6b
Lee_D <leegreat21@gmail.com>
2023-05-12 19:30:18 +0300

    Tictactoe refinement

--- 04-Typescript/02-conditions/HomePuzzles/Lee/dist/exercise1.js
+++ 04-Typescript/02-conditions/HomePuzzles/Lee/dist/exercise1.js
@@ -1,4 +1,5 @@
 var turn = true; // true = x
+var btnClicked = 0;
 var btns = document.querySelectorAll(".btn");
 btns.forEach(function (b) {
     b.addEventListener("click", btnClick);
@@ -7,33 +8,59 @@ function btnClick() {
     var _this = this;
     if (this.textContent !== "")
         return;
+    btnClicked++;
     if (turn)
         this.textContent = "x";
     else
         this.textContent = "o";
-    if (checkWin()) {
+    var obj = checkWin();
+    if (obj.win) {
+        var btns_1 = document.querySelectorAll(".btn");
+        btns_1[obj.pos[0]].style.color = "red";
+        btns_1[obj.pos[1]].style.color = "red";
+        btns_1[obj.pos[2]].style.color = "red";
         setTimeout(function () {
             alert(_this.textContent + " is Win");
+            reset();
+        }, 100);
+    }
+    else if (obj === null || obj === void 0 ? void 0 : obj.isTie) {
+        setTimeout(function () {
+            alert(" is a Tie");
+            reset();
         }, 100);
     }
     turn = !turn;
 }
+function reset() {
+    var btns = document.querySelectorAll(".btn");
+    turn = !turn;
+    btnClicked = 0;
+    btns.forEach(function (b) {
+        b.textContent = "";
+        b.style.color = "";
+    });
+}
 function checkWin() {
     var btns = document.querySelectorAll(".btn");
+    var obj = { win: false, isTie: false, pos: [] };
     if (btns[0].textContent == btns[1].textContent && btns[1].textContent == btns[2].textContent && btns[2].textContent !== "")
-        return true;
+        obj = { win: true, isTie: false, pos: [0, 1, 2] };
     else if (btns[3].textContent == btns[4].textContent && btns[4].textContent == btns[5].textContent && btns[5].textContent !== "")
-        return true;
+        obj = { win: true, isTie: false, pos: [3, 4, 5] };
     else if (btns[6].textContent == btns[7].textContent && btns[7].textContent == btns[8].textContent && btns[8].textContent !== "")
-        return true;
+        obj = { win: true, isTie: false, pos: [6, 7, 8] };
     else if (btns[0].textContent == btns[3].textContent && btns[3].textContent == btns[6].textContent && btns[6].textContent !== "")
-        return true;
+        obj = { win: true, isTie: false, pos: [0, 3, 6] };
     else if (btns[1].textContent == btns[4].textContent && btns[4].textContent == btns[7].textContent && btns[7].textContent !== "")
-        return true;
+        obj = { win: true, isTie: false, pos: [1, 4, 7] };
     else if (btns[2].textContent == btns[5].textContent && btns[5].textContent == btns[8].textContent && btns[8].textContent !== "")
-        return true;
+        obj = { win: true, isTie: false, pos: [2, 5, 8] };
     else if (btns[0].textContent == btns[4].textContent && btns[4].textContent == btns[8].textContent && btns[8].textContent !== "")
-        return true;
+        obj = { win: true, isTie: false, pos: [0, 4, 8] };
     else if (btns[2].textContent == btns[4].textContent && btns[4].textContent == btns[6].textContent && btns[6].textContent !== "")
-        return true;
+        obj = { win: true, isTie: false, pos: [2, 4, 6] };
+    else if (btnClicked === 9)
+        obj.isTie = true;
+    return obj;
 }
--- 04-Typescript/02-conditions/HomePuzzles/Lee/exercise1.ts
+++ 04-Typescript/02-conditions/HomePuzzles/Lee/exercise1.ts
@@ -1,5 +1,5 @@
 let turn = true // true = x
-
+let btnClicked = 0
 let btns = document.querySelectorAll(".btn")
 
 btns.forEach(b=>{
@@ -8,38 +8,66 @@ btns.forEach(b=>{
 
 function btnClick(){
     if(this.textContent !== "") return
+    btnClicked++
 
     if(turn) this.textContent = "x"
     else this.textContent = "o"
 
-    
-    if(checkWin()){
+    let obj = checkWin()
+    if(obj.win){
+        let btns = document.querySelectorAll(".btn")
+
+        btns[obj.pos[0]].style.color = "red"
+        btns[obj.pos[1]].style.color = "red"
+        btns[obj.pos[2]].style.color = "red"
+
         setTimeout((){
             alert(this.textContent + " is Win")
+            reset()
 
         }, 100)  
-    }
+    } 
+    else if(obj?.isTie) {
+        setTimeout((){
+            alert(" is a Tie")
+            reset()
 
-    turn = ! turn
+        }, 100)  
+   }
+   turn = ! turn
+}
+
+function reset(){
+let btns = document.querySelectorAll(".btn")
+turn = !turn
+btnClicked = 0
+btns.forEach(b=>{
+    b.textContent = ""
+    b.style.color = ""
+})
 }
 
 function checkWin(){
     let btns = document.querySelectorAll(".btn")
+    let obj = {win:false, isTie:false, pos:[]} 
 
     if(btns[0].textContent == btns[1].textContent && btns[1].textContent == btns[2].textContent && btns[2].textContent !== "")
-        return true    
+        obj = {win:true, isTie:false, pos:[0,1,2]}    
     else if(btns[3].textContent == btns[4].textContent && btns[4].textContent == btns[5].textContent && btns[5].textContent !== "")
-        return true 
+    obj = {win:true, isTie:false, pos:[3,4,5]} 
     else if(btns[6].textContent == btns[7].textContent && btns[7].textContent == btns[8].textContent && btns[8].textContent !== "")
-        return true 
+    obj =  {win:true, isTie:false, pos:[6,7,8]} 
     else if(btns[0].textContent == btns[3].textContent && btns[3].textContent == btns[6].textContent && btns[6].textContent !== "")
-        return true
+    obj =  {win:true, isTie:false, pos:[0,3,6]}
     else if(btns[1].textContent == btns[4].textContent && btns[4].textContent == btns[7].textContent && btns[7].textContent !== "")
-        return true 
+    obj =  {win:true, isTie:false, pos:[1,4,7]} 
     else if(btns[2].textContent == btns[5].textContent && btns[5].textContent == btns[8].textContent && btns[8].textContent !== "")
-        return true 
+    obj =  {win:true, isTie:false, pos:[2,5,8]} 
     else if(btns[0].textContent == btns[4].textContent && btns[4].textContent == btns[8].textContent && btns[8].textContent !== "")
-        return true
+    obj =  {win:true, isTie:false, pos:[0,4,8]}
     else if(btns[2].textContent == btns[4].textContent && btns[4].textContent == btns[6].textContent && btns[6].textContent !== "")
-        return true  
+    obj =  {win:true, isTie:false, pos:[2,4,6]}  
+    else if(btnClicked === 9)
+    obj.isTie = true 
+    return obj 
 }
