79a36d8db21ee3941853acee5ea2a5590e1f481e
liran26 <liranav26@gmail.com>
2023-05-13 18:44:04 +0300

    update

--- 04-Typescript/02-conditions/HomePuzzles/Liran/dist/index.js
+++ 04-Typescript/02-conditions/HomePuzzles/Liran/dist/index.js
@@ -2,14 +2,22 @@ var use = confirm("Welcome to Age verefication for byuing alcohol, press ok to c
 while (use) {
     var np = prompt("Enter your name");
     var ap = Number(prompt("Enter your age"));
-    while ((Number.isNaN(ap)) || (ap == null)) {
-        ap = Number(prompt("Enter your age, please use only numbers"));
+    while ((Number.isNaN(ap)) || (ap == null) || (ap <= 0)) {
+        if (ap == 0) {
+            ap = Number(prompt("Don't leave empty, please enter your age"));
+        }
+        else if (ap < 0) {
+            ap = Number(prompt("Enter your age, please use only positive numbers"));
+        }
+        else {
+            ap = Number(prompt("Enter your age, please use only digits"));
+        }
     }
     if (ap >= 18) {
-        alert(np + " You are " + ap + ", therefor you are allowed to buy alcohol");
+        alert(np + " You are " + ap + " years old, therefor you are allowed to buy alcohol");
     }
     else {
-        alert(np + " You are " + ap + ", therefor you are not allowed to buy alcohol");
+        alert(np + " You are " + ap + " years old, therefor you are not allowed to buy alcohol");
     }
     use = confirm("press ok to for another, cancle to exit");
 }
--- 04-Typescript/02-conditions/HomePuzzles/Liran/index.ts
+++ 04-Typescript/02-conditions/HomePuzzles/Liran/index.ts
@@ -3,19 +3,28 @@ while(use)
 {
     const np = prompt("Enter your name")
     let ap = Number(prompt("Enter your age"))
-    while((Number.isNaN(ap)) || (ap == null))
+    while((Number.isNaN(ap)) || (ap == null) || (ap <= 0))
     {
-        ap = Number(prompt("Enter your age, please use only numbers"))
+        if(ap == 0)
+        {
+            ap = Number(prompt("Don't leave empty, please enter your age"))
+        }
+        else if(ap < 0){
+            ap = Number(prompt("Enter your age, please use only positive numbers"))
+        }
+        else{
+            ap = Number(prompt("Enter your age, please use only digits"))
+        }
 
     }
     if(ap >= 18)
     {
 
-        alert(np + " You are " + ap + ", therefor you are allowed to buy alcohol")
+        alert(np + " You are " + ap +  " years old, therefor you are allowed to buy alcohol")
     }
     else
     {
-        alert(np + " You are " + ap + ", therefor you are not allowed to buy alcohol")
+        alert(np + " You are " + ap + " years old, therefor you are not allowed to buy alcohol")
     }
     use = confirm("press ok to for another, cancle to exit")
 }
