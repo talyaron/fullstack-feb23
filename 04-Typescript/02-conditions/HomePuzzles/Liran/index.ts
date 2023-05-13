a80d2c9cb23f910ec787e049bc2770efbac8e902
liran26 <liranav26@gmail.com>
2023-05-13 15:48:21 +0300

    conditions Home puzzle

---
+++ 04-Typescript/02-conditions/HomePuzzles/Liran/index.ts
@@ -0,0 +1,25 @@
+let use = confirm("Welcome to Age verefication for byuing alcohol, press ok to continue")
+while(use)
+{
+    const np = prompt("Enter your name")
+    let ap = Number(prompt("Enter your age"))
+    while((Number.isNaN(ap)) || (ap == null))
+    {
+        ap = Number(prompt("Enter your age, please use only numbers"))
+
+    }
+    if(ap >= 18)
+    {
+
+        alert(np + " You are " + ap + ", therefor you are allowed to buy alcohol")
+    }
+    else
+    {
+        alert(np + " You are " + ap + ", therefor you are not allowed to buy alcohol")
+    }
+    use = confirm("press ok to for another, cancle to exit")
+}
+alert("Bye Bye...")
+
+
+
