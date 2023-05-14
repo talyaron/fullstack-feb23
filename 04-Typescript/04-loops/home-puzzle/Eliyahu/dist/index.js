111f60c8a4f6d5e28c0afa28b51a16099d4fb4e9
Eliyaho basson <basson131@gmail.com>
2023-05-14 23:16:09 +0300

    1

--- 04-Typescript/04-loops/home-puzzle/Eliyahu/dist/index.js
+++ 04-Typescript/04-loops/home-puzzle/Eliyahu/dist/index.js
@@ -3,14 +3,11 @@ var m = prompt("הכנס מספר כפל");
 var sum = Number(n);
 var multiply = Number(m);
 var x = 0;
-var y = 0;
 for (var j = 1; j <= multiply; j++) {
     for (var i = 1; i <= sum; i++) {
         x = x + i;
     }
-    y = y + x;
-    x = 0;
     if (j === multiply) {
-        document.write("(1+2+...+ " + sum + ")x" + multiply + "=" + y);
+        document.write("(1+2+...+ " + sum + ")x" + multiply + "=" + x);
     }
 }
--- 04-Typescript/04-loops/home-puzzle/Eliyahu/index.ts
+++ 04-Typescript/04-loops/home-puzzle/Eliyahu/index.ts
@@ -4,16 +4,13 @@ const m = prompt("הכנס מספר כפל");
 const sum = Number(n);
 const multiply = Number(m);
 let x = 0
-let y = 0
 
 for (let j = 1; j <= multiply; j++) {
     for (let i = 1; i <= sum; i++) {
         x = x + i
     }
-    y = y + x
-    x = 0
     if (j === multiply) {
-        document.write(`(1+2+...+ ${sum})x${multiply}=${y}`)
+        document.write(`(1+2+...+ ${sum})x${multiply}=${x}`)
     }
 }
 
