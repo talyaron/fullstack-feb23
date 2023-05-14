c2a7142bc81fcc545f86c8c5f1aa5972d8e50c92
Eliyaho basson <basson131@gmail.com>
2023-05-14 23:11:12 +0300

    -

--- 04-Typescript/04-loops/home-puzzle/Eliyahu/index.ts
+++ 04-Typescript/04-loops/home-puzzle/Eliyahu/index.ts
@@ -7,20 +7,13 @@ let x = 0
 let y = 0
 
 for (let j = 1; j <= multiply; j++) {
-
-
-
     for (let i = 1; i <= sum; i++) {
-
         x = x + i
-
-
     }
     y = y + x
     x = 0
     if (j === multiply) {
         document.write(`(1+2+...+ ${sum})x${multiply}=${y}`)
-
     }
 }
 
