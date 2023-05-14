f54798fc5f980045f934fe7d66611cb04cf8bbe2
Tal Yaron <tal.yaron@gmail.com>
2023-05-14 18:54:32 +0300

    loops

--- 04-Typescript/03-mvc/example/dist/index.js
+++ 04-Typescript/03-mvc/example/dist/index.js
@@ -1,4 +1,22 @@
+//Model (data) View (GUI) Controler(process)
 var ageAsString = prompt("what is your age?");
-if (ageAsString !== null) {
+debugger;
+console.log(ageAsString);
+//scope
+if (ageAsString) {
     var ageAsNumber = parseInt(ageAsString);
+    if (!isNaN(ageAsNumber)) {
+        console.log(ageAsNumber);
+        if (ageAsNumber >= 18) {
+            document.write("which alchol beverage woulf you like?");
+        }
+        else {
+            document.write("You are too young to some drink");
+        }
+    }
 }
+var z = !true;
+//false value
+// values which afre equal to flase
+// 0, null, undefined, NaN, ""
+console.log(z);
---
+++ 04-Typescript/04-loops/exampe/dist/index.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// let x = 1;
+// console.log(x);
+// x++ 
+// console.log(x);
+//loop
+for (var x = 1; x < 100; x = x * 2) {
+  console.log(x);
+}
