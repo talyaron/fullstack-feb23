557287112352eea9de92a081b9f016fe68ac673b
ShaharShaul <128305593+ShaharShaul@users.noreply.github.com>
2023-05-15 20:05:08 +0300

    1

---
+++ 04-Typescript/04-loops/class-puzzle/Yuval Lee Shahar/dist/index.js
@@ -0,0 +1,15 @@
+// for(let i = 1; i<100; i++){
+//     console.log(i);
+//     if(!(i%7)){ 
+//         let j = "Boom!";
+//         let i = j;
+//         console.log(j);
+//     }
+// }
+for (var i = 1; i <= 100; i++) {
+    if (i % 7 === 0) {
+        console.log("BOOM");
+    }
+    else
+        console.log(i);
+}
