9bcc355782274ffa0b9d6dd3df6b50dba62680aa
Bar <beerya.yaron@gmail.com>
2023-05-15 10:47:41 +0300

    home puzzel

---
+++ 04-Typescript/04-loops/home-puzzle/Bar/index.ts
@@ -0,0 +1,5 @@
+for (let i = 99; i >= 0; i--) {
+    console.log(i + "Bottles of Beer on the wall. take one down pass it around, and" + i--2 + "bootle of beer on the wall");
+    // console.log(i-- +"bootle of beer on the wall");
+    // console.log(i + "bootle of beer on the wall");
+} 
