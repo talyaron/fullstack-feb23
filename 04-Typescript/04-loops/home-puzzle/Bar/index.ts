67db74044e4fa01498e693a10126a92cadd03505
ziv leloneck <119840698+zivll1991@users.noreply.github.com>
2023-05-15 14:25:20 +0300

    ver1.1

---
+++ 04-Typescript/04-loops/home-puzzle/Bar/index.ts
@@ -0,0 +1,6 @@
+for (let i = 99; i >= 0;) {
+  console.log(i-- + "Bottles of Beer on the wall. take one down pass it around, and");
+  console.log(i + "bootle of beer on the wall")
+  // console.log(i-- +"bootle of beer on the wall");
+    // console.log(i + "bootle of beer on the wall");
+} 
