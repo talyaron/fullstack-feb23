d1cca2055c58a64ccae082cc6291c34ab9072a41
tomsobia <tom162sobia@gmail.com>
2023-05-16 09:37:17 +0300

    update

--- 04-Typescript/04-loops/home-puzzle/tomsobia/dist/index.js
+++ 04-Typescript/04-loops/home-puzzle/tomsobia/dist/index.js
@@ -1,6 +1,7 @@
 // תרגיל 1
-// for (let i= 99; i > 0 ; i--) {
-//     const x = i -1;
-//       document.write(`${i} Bottles of Beer on the wall. Take one down pass it around, and ${x} bottles of beer on the wall.` + "<br>");
-//   };
+for (var i = 99; i > 0; i--) {
+    var x = i - 1;
+    document.write(i + " Bottles of Beer on the wall. Take one down pass it around, and " + x + " bottles of beer on the wall." + "<br>");
+}
+;
 // תרגיל 2
--- 04-Typescript/04-loops/home-puzzle/tomsobia/index.ts
+++ 04-Typescript/04-loops/home-puzzle/tomsobia/index.ts
@@ -1,7 +1,7 @@
 // תרגיל 1
-// for (let i= 99; i > 0 ; i--) {
-//     const x = i -1;
-//       document.write(`${i} Bottles of Beer on the wall. Take one down pass it around, and ${x} bottles of beer on the wall.` + "<br>");
-//   };
+for (let i= 99; i > 0 ; i--) {
+    const x = i -1;
+      document.write(`${i} Bottles of Beer on the wall. Take one down pass it around, and ${x} bottles of beer on the wall.` + "<br>");
+  };
 
 // תרגיל 2
