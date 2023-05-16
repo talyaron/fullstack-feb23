7dbc3a1be069bcafbb17514b0ed9e2529b759646
Lee_D <leegreat21@gmail.com>
2023-05-16 13:59:57 +0300

    loops

---
+++ 04-Typescript/04-loops/home-puzzle/Lee/Lindex-1.ts
@@ -0,0 +1,11 @@
+// Task No. 1
+
+// for (let i = 1; i < 10; i--){
+    
+//     console.log('Bottoles of beer ', i); 
+//     }
+    
+    
+ for (let i = 100; i >= 1; i--){
+    console.log(i, "Bottles of Beer on the wall...");
+}
