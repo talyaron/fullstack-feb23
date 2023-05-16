091d540ee47a4843b11c4ce510296e0c43ef5171
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2023-05-16 09:18:29 +0300

    .

--- 04-Typescript/04-loops/home-puzzle/hadar/1/index.ts
+++ 04-Typescript/04-loops/home-puzzle/hadar/1/index.ts
@@ -1,5 +1,5 @@
 
-for(let x = 100; x>0;x--){
+for(let x = 99; x>0;x--){
     let y = x-1
     console.log(`${x} Bottles of Beer on the wall. take one down pass it around, and ${y} bootle of beer on the wall`);
 }
