df806e212f6ed51c5cd53faa395d6fcf4dcbd9f7
tomsobia <tom162sobia@gmail.com>
2023-05-16 09:36:04 +0300

    update

--- 04-Typescript/04-loops/home-puzzle/tomsobia/dist/index.js
+++ 04-Typescript/04-loops/home-puzzle/tomsobia/dist/index.js
@@ -1,5 +1,6 @@
-for (var i = 99; i > 0; i--) {
-    var x = i - 1;
-    console.log(i + " Bottles of Beer on the wall. Take one down pass it around, and " + x + " bottles of beer on the wall.");
-}
-;
+// תרגיל 1
+// for (let i= 99; i > 0 ; i--) {
+//     const x = i -1;
+//       document.write(`${i} Bottles of Beer on the wall. Take one down pass it around, and ${x} bottles of beer on the wall.` + "<br>");
+//   };
+// תרגיל 2
