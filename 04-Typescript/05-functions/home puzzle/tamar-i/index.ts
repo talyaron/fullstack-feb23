c7fb97d80d80d665f7e7c303af214bebe65927f1
itahTamar <tamar.verber@gmail.com>
2023-05-17 21:31:20 +0300

    lesson5-ex2 complet

--- 04-Typescript/05-functions/home puzzle/tamar-i/index.ts
+++ 04-Typescript/05-functions/home puzzle/tamar-i/index.ts
@@ -2,14 +2,14 @@
 
 function hello (jander:string | null):string{
     if (jander=='mail'){
-        return ("Hello sir")}
+        return ("Hello sir, ")}
 
     else{
      if (jander=='female'){
-        return ("Hello miss")}
+        return ("Hello miss, ")}
 
         else
-        return ("hello")
+        return ("hello, ")
     }
 }
 
@@ -17,4 +17,16 @@ const jander:string | null = prompt("what is your jander?");
 
 document.write(hello(jander));
 
+//2
 
+function max (num1:number, num2:number):number {
+    if (num1 > num2) return num1
+    else return num2
+}
+
+const num1:number = Number(prompt("please enter the first number"));
+const num2:number = Number(prompt("please enter the srcond number"));
+
+document.write(`the bigger number is ${max(num1,num2)} `);
+
+//3
