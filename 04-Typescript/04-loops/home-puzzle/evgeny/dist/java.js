da0c8409528f4d6ec964c6da639b06b2452c70fe
Evgeny <zakman336@gmail.com>
2023-05-17 12:47:23 +0300

    h

--- 04-Typescript/04-loops/home-puzzle/evgeny/dist/java.js
+++ 04-Typescript/04-loops/home-puzzle/evgeny/dist/java.js
@@ -1,6 +1,5 @@
-number = 2;
 var y = 0;
-for (var x = 1; x <= $number; x++) {
+for (var x = 1; x <= 5; x++) {
     y = x + y;
 }
-console.log(y, "ok");
+console.log(y);
--- 04-Typescript/04-loops/home-puzzle/evgeny/java.ts
+++ 04-Typescript/04-loops/home-puzzle/evgeny/java.ts
@@ -1,11 +1,12 @@
 
 
-number = 2
+
 
 let y = 0
 
-for(let x=1;x<=$number;x++){
-     y=x+y
-    }
+for(let x=1;x<=5;x++){
+    y=x+y
+    
+}
 
-console.log(y,"ok")
+console.log(y)
