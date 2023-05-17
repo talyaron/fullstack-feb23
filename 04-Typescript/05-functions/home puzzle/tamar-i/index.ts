bcd991ddcd4c81eb53f584f1368795ea0cc951a0
itahTamar <tamar.verber@gmail.com>
2023-05-17 23:08:08 +0300

    lessen5-ex3 almost finish

--- 04-Typescript/05-functions/home puzzle/tamar-i/index.ts
+++ 04-Typescript/05-functions/home puzzle/tamar-i/index.ts
@@ -27,6 +27,30 @@ function max (num1:number, num2:number):number {
 const num1:number = Number(prompt("please enter the first number"));
 const num2:number = Number(prompt("please enter the srcond number"));
 
-document.write(`the bigger number is ${max(num1,num2)} `);
+document.write(`the bigger number is ${max(num1,num2)}. `);
 
 //3
+
+function sortArray (arr:number[]):number[]{
+    let big:number[] = [0];
+    for (let i=1 ; i<(arr.length) ; i++){
+        big[i-1] = max(arr[i-1], arr[i])
+    }
+    return big;
+}
+
+function getArray ():number[]{
+    let arr:number[] = [0]
+    let i:number = 0
+    let num:number = Number(prompt("enter a number diffrent then 0"));
+    while (num!=0){
+        arr[i] = num;
+        i++;
+        num = Number(prompt("enter the next number"));
+    }
+    return arr;
+}
+
+let arr:number[] = getArray();
+
+document.write(`the sort array is ${sortArray(arr)}`);
