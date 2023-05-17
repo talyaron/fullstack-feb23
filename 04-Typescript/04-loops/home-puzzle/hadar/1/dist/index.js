1d419dcdd89c0aedf735468eadf8aebd169be1a3
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2023-05-17 16:46:36 +0300

    2

--- 04-Typescript/04-loops/home-puzzle/hadar/1/dist/index.js
+++ 04-Typescript/04-loops/home-puzzle/hadar/1/dist/index.js
@@ -2,3 +2,9 @@ for (var x = 99; x > 0; x--) {
     var y = x - 1;
     console.log(x + " Bottles of Beer on the wall. take one down pass it around, and " + y + " bootle of beer on the wall");
 }
+var num = prompt("choose a number:");
+var sum = 0;
+for (var y = 0; y <= num; y++) {
+    sum = sum + y;
+}
+console.log("1+2+3...+" + num + "=" + sum);
