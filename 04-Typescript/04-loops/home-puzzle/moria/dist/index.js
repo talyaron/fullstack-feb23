a2d7ba691db4fc303198a2b40cd9f9d7f6f321b6
moria-avraham <128312303+moria-avraham@users.noreply.github.com>
2023-05-17 15:47:26 +0300

    .

--- 04-Typescript/04-loops/home-puzzle/moria/dist/index.js
+++ 04-Typescript/04-loops/home-puzzle/moria/dist/index.js
@@ -1,10 +1,13 @@
 var number = 5;
 var j = 3;
 var sum = 0;
+var s = 1;
 for (var n = 0; n < number; n++) {
-    console.log(sum + " = " + number + " + " + n);
     sum = number + n;
-    while (n < number) {
-        console.log("* " + j);
+    s--;
+    while (s < 1) {
+        console.log(sum + " = " + number + " + " + n + " * " + j);
+        sum = (number + n) * j;
+        s++;
     }
 }
