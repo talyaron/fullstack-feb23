c418442decccc67da262bf4e33ebb9b693df508a
Evgeny <zakman336@gmail.com>
2023-05-17 12:26:34 +0300

    r

---
+++ 04-Typescript/04-loops/home-puzzle/evgeny/dist/java.js
@@ -0,0 +1,6 @@
+number = 2;
+var y = 0;
+for (var x = 1; x <= $number; x++) {
+    y = x + y;
+}
+console.log(y, "ok");
---
+++ 04-Typescript/04-loops/home-puzzle/evgeny/java.ts
@@ -0,0 +1,11 @@
+
+
+number = 2
+
+let y = 0
+
+for(let x=1;x<=$number;x++){
+     y=x+y
+    }
+
+console.log(y,"ok")
