35aab937f918fd15ffd98c72ef5bd1ca32fca844
Eliyaho basson <basson131@gmail.com>
2023-05-18 23:28:56 +0300

    ץ

--- 04-Typescript/05-functions/home puzzle/Eliyahu/dist/index.js
+++ 04-Typescript/05-functions/home puzzle/Eliyahu/dist/index.js
@@ -1,4 +1,3 @@
-// // 1) write a prompt that ask the user if he is male or female. if the answer is "male" return "hello sir" if it is "female" return "hello Miss". If the user write another answer, it should return "hello".
 var type = prompt("what is your gender?");
 function Hello(sug) {
     var gender = "";
@@ -10,12 +9,11 @@ function Hello(sug) {
             gender = "Miss";
         }
     }
-    return "Hello " + gender;
+    return "Hello " + gender + " <br/>";
 }
 document.write(Hello(type));
-// // 2) write a function that gets 2 numbers, and return the bigger. (max)
-var num1 = Number(prompt("הכנס מספר"));
-var num2 = Number(prompt("הכנס מספר נוסף"));
+var num1 = Number(prompt("הכנס מספר להשוואה"));
+var num2 = Number(prompt("הכנס מספר נוסף להשוואה"));
 function max(a, b) {
     if (a > b) {
         return a;
@@ -24,23 +22,26 @@ function max(a, b) {
         return b;
     }
 }
-console.log(max(num1, num2));
-// 3) write a function that gets an array of numbers, and return the number ordered from the smallest to the largest. dont use .sort. dont use chatGPT. you can only search of array in javascript. ([9,4,5,7] ->[4,5,7,9] )
+document.write("The big number is " + (max(num1, num2)).toString() + "<br/>");
 var nums = [];
 var lng = Number(prompt("כמה מספרים יש במערך?"));
 for (var z = 0; z < lng; z++) {
     var n = Number(prompt("הכנס מספר"));
     nums.push(n);
 }
-var orderNums = [];
-for (var j = 0; j < nums.length; j++) {
-    var x = 0;
-    for (var i = 0; i < nums.length; i++) {
-        if (nums[i] > x) {
-            x = nums[i];
+function order(arr) {
+    var orderNums = [];
+    for (var j = 0; j < arr.length; j++) {
+        var x = 0;
+        for (var i = 0; i < arr.length; i++) {
+            if (x < arr[i]) {
+                x = arr[i];
+            }
         }
+        arr[arr.indexOf(x)] = 0;
+        orderNums.unshift(x);
     }
-    nums[nums.indexOf(x)] = 0;
-    orderNums.unshift(x);
+    return orderNums;
 }
-document.write(orderNums);
+var result = (order(nums));
+document.write("Here your array sort by order: " + (result.toString()));
