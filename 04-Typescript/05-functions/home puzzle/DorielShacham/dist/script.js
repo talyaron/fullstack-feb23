6085a9eb82b09ff76d9c32029ab60c0ed7dd3a19
Doriel Shacham <dorielshacham@gmail.com>
2023-05-20 22:12:03 +0300

    prettifi

--- 04-Typescript/05-functions/home puzzle/DorielShacham/dist/script.js
+++ 04-Typescript/05-functions/home puzzle/DorielShacham/dist/script.js
@@ -30,12 +30,12 @@ var bigger = biggerNum(num1, num2);
 document.write(bigger.toString());
 // 3) write a function that gets an array of numbers, and return the number ordered from the smallest to the largest. dont use .sort. dont use chatGPT. you can only search of array in javascript. ([9,4,5,7] ->[4,5,7,9] )
 var arr = [5, 2, 3, 4, 1];
-console.log("[" + arr, ']', 'this is the first array');
+console.log('%c[' + arr + '] this is the first array', 'background: blue; color: white; display: block; font-family:monospace; font-weight: bold; font-size: 20px; text-shadow: 3px 3px 0 rgb(0,0,0)');
 var OOO = arr.splice(1, 4);
-console.log("" + OOO, 'using .splice to remove the first(0) number');
+console.log('%c[' + OOO + '] using .splice to remove the first(0) number', 'background: teal; color: white; display: block; font-family:monospace; font-weight: bold; font-size: 20px; text-shadow: 3px 3px 0 rgb(0,0,0)');
 OOO.unshift(0, 1);
-console.log("" + OOO, 'Using .unshift to add values(0, 1) to the start of the array');
+console.log('%c[' + OOO + '] Using .unshift to add values(0, 1) to the start of the array', 'background: orange; color: white; display: block; font-family:monospace; font-weight: bold; font-size: 20px; text-shadow: 3px 3px 0 rgb(0,0,0)');
 var newNewOOO = OOO.splice(0, 5);
-console.log("" + newNewOOO, 'Using .splice again to cut the last number of the array(1)');
+console.log('%c[' + newNewOOO + '] Using .splice again to cut the last number of the array(1)', 'background: red; color: white; display: block; font-family:monospace; font-weight: bold; font-size: 20px; text-shadow: 3px 3px 0 rgb(0,0,0)');
 newNewOOO.push(5);
-console.log("" + newNewOOO, 'Using .push to add the number (5) to the end of the array');
+console.log('%c[' + newNewOOO + '] Using .push to add the number (5) to the end of the array', 'background: lime; color: white; display: block; font-family:monospace; font-weight: bold; font-size: 20px; text-shadow: 3px 3px 0 rgb(0,0,0)');
