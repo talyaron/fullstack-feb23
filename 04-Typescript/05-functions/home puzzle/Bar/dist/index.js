fa52b0e23f157cfee1bee3241627216d5fab0e94
Bar <beerya.yaron@gmail.com>
2023-05-21 20:31:53 +0300

    hp

--- 04-Typescript/05-functions/home puzzle/Bar/dist/index.js
+++ 04-Typescript/05-functions/home puzzle/Bar/dist/index.js
@@ -12,10 +12,3 @@ function ensWer(theGennder) {
 }
 var greeting = ensWer(gennder);
 document.write("hello " + greeting);
-// const gennDer = prompt("are you a male or female?");
-// let theGennder = Gennder
-// function sayHello(theGennder:string|null):string{
-//     return `Hello ${theGennder}`;
-// } 
-// const greeting = sayHello(gennDer)
-// document.write(greeting);
--- 04-Typescript/05-functions/home puzzle/Bar/index.ts
+++ 04-Typescript/05-functions/home puzzle/Bar/index.ts
@@ -15,14 +15,3 @@ function ensWer(theGennder: string | null): string {
 
 const greeting = ensWer(gennder)
 document.write(`hello ${greeting}`)
-
-// const gennDer = prompt("are you a male or female?");
-// let theGennder = Gennder
-
-// function sayHello(theGennder:string|null):string{
-//     return `Hello ${theGennder}`;
-// } 
-
-
-// const greeting = sayHello(gennDer)
-// document.write(greeting);
