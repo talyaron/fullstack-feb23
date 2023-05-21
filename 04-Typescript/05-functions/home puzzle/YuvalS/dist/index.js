c97170103bae96c3dfc8491c813257c874f2b4c0
Shtig7 <128290647+Shtig7@users.noreply.github.com>
2023-05-21 12:38:25 +0300

    1

--- 04-Typescript/05-functions/home puzzle/YuvalS/dist/index.js
+++ 04-Typescript/05-functions/home puzzle/YuvalS/dist/index.js
@@ -1,14 +1,28 @@
-var x = Number(prompt("Enter a number"));
-var y = Number(prompt("Enter a second number"));
-function whoIsBigger(x, y) {
-    if (x > y) {
-        return x;
-    }
-    else if (x === y) {
-        return x = y;
-    }
-    else {
-        return y;
+// const x = Number(prompt("Enter a number"))
+// const y = Number(prompt("Enter a second number"))
+// function whoIsBigger (x: Number, y: Number) {
+//     if(x>y){
+//        return x 
+//     }
+//     else if(x===y){
+//         return x=y
+//     }
+//     else{
+//         return y
+//     }
+// }
+// console.log(whoIsBigger(x, y))
+var numbers2 = ["3, 1, 4, 2"];
+function fromStoB(numbers2) {
+    for (var i = 0; i < numbers2.length; i++) {
+        console.log(Math.min.apply(Math, numbers2));
     }
 }
-console.log(whoIsBigger(x, y));
+console.log(fromStoB(numbers2));
+// if(numbers[i] = Math.min(...numbers))
+// {
+// numbers[i] = result.length ; 
+// }
+// }
+// return result
+// }
