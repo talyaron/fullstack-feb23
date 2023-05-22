7686ffa24f9359c2eb96f2ec295329334ced3b12
inalexa81 <athegreat5@hotmail.com>
2023-05-22 13:33:42 +0300

    Alex reverse

---
+++ 04-Typescript/07-functions-pure-functions/Home puzzles/Alex/dist/index.js
@@ -0,0 +1,19 @@
+// # Level 3
+//  Write a function to reverse a number. For example, the function gets 123 and returns 321
+var num = Number(prompt("enter number"));
+function reversedNum(num) {
+    try {
+        if (isNaN(num))
+            throw new Error("Not a number! Enter NUMBER type input only!");
+        return (parseFloat(num
+            .toString()
+            .split('')
+            .reverse()
+            .join('')) * Math.sign(num));
+    }
+    catch (error) {
+        console.error(error);
+        return undefined;
+    }
+}
+console.log(reversedNum(num));
