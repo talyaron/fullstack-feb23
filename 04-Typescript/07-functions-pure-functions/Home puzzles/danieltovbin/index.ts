dccb5cd2e68ec4bfd7911539ae9b10c2e725ddc1
danieltovbin <111869103+danieltovbin@users.noreply.github.com>
2023-05-23 18:01:36 +0300

    .

---
+++ 04-Typescript/07-functions-pure-functions/Home puzzles/danieltovbin/index.ts
@@ -0,0 +1,66 @@
+// # Level 1
+
+// 1. Write a JavaScript function that returns the negative number of a number.
+//    Example:
+//    x = 32243;
+//    Expected Output: -32243;
+
+
+// task 1(1)
+// function positiv (numPosToNeg :number) :number {
+//     return numPosToNeg * (-1);
+// } 
+// console.log(positiv(300));
+
+
+
+// 2. Write a function to convert Celsius to Fahrenheit
+
+// function addName (another :string = "Celsius"):string {
+//     let firstName :string = another + " Fahrenheit" ;
+//     return firstName;
+    
+// }
+
+// console.log(addName())
+
+
+
+// function popElement(arr) :string{
+//     arr.pop()
+//     return arr;
+//   }
+//   console.log(popElement())
+
+
+//   const person = {
+//     name: "Doriel",
+//     age: 29,
+//     date: "11.1.1994"
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// # Level 2
+
+// 1. Create a function that gets the gender of the user and height of the user.
+//    The function returns the height of the user relative to his gender. For example, the user's height is 170cm, and the gender is male.
+//    The average height of males is 174cm, and the function return -4
+
+// # Level 3
+
+// 1. Write a function to reverse a number. For example, the function gets 123 and returns 321
+
+// Find exercises for js functions and complete them.
+// https://www.w3resource.com/javascript-exercises/javascript-functions-exercises.php
