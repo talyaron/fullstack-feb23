d1c8684cf23b21eed12af1ac4eecff21d6e9eea9
inalexa81 <athegreat5@hotmail.com>
2023-05-24 19:42:44 +0300

    room 1

---
+++ 04-Typescript/08-objects/class-puzzle/room 1/dist/index.js
@@ -0,0 +1,27 @@
+var shiransCar = {
+    company: "Audi",
+    year: 2020,
+    model: "Q3",
+    millage: 10000,
+    color: "yellow",
+    fuelConsamption: 11.9
+};
+var alexsCar = {
+    company: "MG",
+    year: 2019,
+    model: "HMS",
+    millage: 30687,
+    color: "metalic",
+    fuelConsamption: 10.3
+};
+function tripConsamption(car, trip) {
+    try {
+        return trip / car.fuelConsamption;
+    }
+    catch (error) {
+        console.error(error);
+        return undefined;
+    }
+}
+console.log(tripConsamption(alexsCar, 123));
+console.log("you need " + tripConsamption(shiransCar, 1000) + " Liters");
