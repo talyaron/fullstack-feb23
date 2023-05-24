a1cad94a7ad86fb0d813c53404fd700bf2dc6318
Tal Yaron <tal.yaron@gmail.com>
2023-05-24 18:01:36 +0300

    Objects

---
+++ 04-Typescript/08-objects/example/index.ts
@@ -0,0 +1,30 @@
+const userName = "Moshe";
+const password = "1234";
+const profileImage = "....";
+
+const userName2 = "Miriam";
+const password2 = "123456";
+const profileImage2 = "....";
+
+//object
+const moshe = {
+  //key:value,
+  userName: "Moshe",
+  password: "1234",
+  profileImage: "...",
+  age: 3500,
+};
+
+console.log(moshe.userName, moshe.password);
+console.log(moshe);
+
+const miriam = {
+  userName: "Miriam",
+  password: "123456",
+  profileImage: "...",
+  age: 3507,
+};
+
+console.log(miriam.userName);
+console.log(miriam["userName"]);
+
