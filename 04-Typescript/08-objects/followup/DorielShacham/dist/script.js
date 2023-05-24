fba23e1acde67e3d69e0a49f965bdb547c7bf2d6
Doriel Shacham <dorielshacham@gmail.com>
2023-05-24 18:11:16 +0300

    objects

---
+++ 04-Typescript/08-objects/followup/DorielShacham/dist/script.js
@@ -0,0 +1,15 @@
+var personOne = {
+    firstName: "Doriel",
+    lastName: "Shacham",
+    age: 29,
+    isTeacher: false
+};
+var personTwo = {
+    firstName: "Tal",
+    lastName: "Yaron",
+    age: 25,
+    isTeacher: true
+};
+console.log(personOne.firstName);
+console.log("Is " + personTwo.firstName + " a teacher? \n" + "", personTwo.isTeacher);
+console.log("Is " + personOne.firstName + " a teacher? \n" + "", personOne["isTeacher"]);
