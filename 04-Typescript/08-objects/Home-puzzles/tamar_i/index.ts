69a478c97b985d83aa35ee8383e32d33c20f75b7
itahTamar <tamar.verber@gmail.com>
2023-05-26 17:47:42 +0300

    object hw start 3

--- 04-Typescript/08-objects/Home-puzzles/tamar_i/index.ts
+++ 04-Typescript/08-objects/Home-puzzles/tamar_i/index.ts
@@ -1,29 +1,60 @@
 //1
 
 interface Student {
-    name: string, 
-    gender: string, 
-    mathMidTermGrade: number, 
-    historyMidTermGrade:number, 
-    mathFinalTermGrade:number, 
-    historyFinalTermGrade:number
-    averageGrade:Function,
+    name: string,
+    gender: string,
+    mathMidTermGrade: number,
+    historyMidTermGrade: number,
+    mathFinalTermGrade: number,
+    historyFinalTermGrade: number
+    averageGrade: Function,
 }
 
 //2
-const student1:Student = {
-    name: "bob", 
-    gender: "mail", 
-    mathMidTermGrade: 85, 
-    historyMidTermGrade:90, 
-    mathFinalTermGrade:75, 
-    historyFinalTermGrade:90,
-    averageGrade:function():number{
-        return (this.mathMidTermGradw+this.mathFinalTermGrade
-            +this.mathFinalTermGrade+this.historyFinalTermGrade)/4
+const studentA: Student = {
+    name: "bob",
+    gender: "mail",
+    mathMidTermGrade: 85,
+    historyMidTermGrade: 90,
+    mathFinalTermGrade: 75,
+    historyFinalTermGrade: 90,
+    averageGrade: function (): number|undefined {
+        try {
+            debugger;
+            console.log(this.mathMidTermGrade + this.historyMidTermGrade + this.mathFinalTermGrade + this.historyFinalTermGrade / 4);
+            return ((this.mathMidTermGrade + this.historyMidTermGrade + this.mathFinalTermGrade + this.historyFinalTermGrade) / 4);
+        } catch (error) {
+            console.log(error)
+            return undefined
+        }
+
     }
 }
 
-console.log (`${student1.name} average grade is ${student1.averageGrade()}`)
+console.log(`${studentA.name} average grade is ${studentA.averageGrade()}`)
 
+//3
 
+interface CourseGrade {
+    courseName: string,
+    courseGrade: number,
+}
+
+let listofgrade: CourseGrade[] = [
+    {
+        courseName: "phizics",
+        courseGrade: 85,
+    },
+    {
+        courseName: "math",
+        courseGrade: 80,
+    },
+    {
+        courseName: "englsh",
+        courseGrade: 96,
+    },
+    {
+        courseName: "sport",
+        courseGrade: 70,
+    },
+]
