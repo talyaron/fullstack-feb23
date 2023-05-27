63a5da7f2c790035e48de3b4927c979ef55abe88
ziv leloneck <119840698+zivll1991@users.noreply.github.com>
2023-05-28 00:42:21 +0300

    ver 1.0

---
+++ 04-Typescript/08-objects/Home-puzzles/zivll/index.ts
@@ -0,0 +1,43 @@
+interface StudentGrades {
+  name: string;
+  grades: number[];
+  averageGrades: Function;
+}
+
+const ziv: StudentGrades = {
+  name: `ziv`,
+  grades: [100, 90, 76],
+  // Number(prompt(`please insert your grades`)),
+  averageGrades() {
+    const result =
+      this.grades.reduce((a: number, b: number) => a + b, 0) /
+      this.grades.length;
+    return result;
+  },
+};
+const dana: StudentGrades = {
+  name: `dana`,
+  grades: [100, 90, 80, 70, 60, 50],
+  // Number(prompt(`please insert your grades`)),
+  averageGrades: averageGrades,
+};
+
+function averageGrades(grade: []): number | undefined {
+  try {
+    const result =
+      this.grades.reduce((a: number, b: number) => a + b, 0) /
+      this.grades.length;
+    return result;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+}
+console.log(
+  `hello ${ziv.name}, your average grades is: ${ziv.averageGrades()}`
+);
+console.log(
+  `hello ${dana.name}, your average grades is: ${dana.averageGrades(
+    dana.grades
+  )}`
+);
