4fd4685ce57ba202e16c92f8c97d9a536d38adcb
moria-avraham <128312303+moria-avraham@users.noreply.github.com>
2023-05-28 16:45:26 +0300

    .

---
+++ 04-Typescript/08-objects/Home-puzzles/moria/dist/index.js
@@ -0,0 +1,22 @@
+var moriaGrade = {
+    name: "moria",
+    math: 65,
+    english: 70,
+    geography: 80,
+    history: 75
+};
+var talGrade = {
+    name: "tal",
+    math: 95,
+    english: 80,
+    geography: 90,
+    history: 100
+};
+var shirGrade = {
+    name: "shir",
+    math: 85,
+    english: 100,
+    geography: 80,
+    history: 95
+};
+// function average
---
+++ 04-Typescript/08-objects/Home-puzzles/moria/index.ts
@@ -0,0 +1,40 @@
+interface grade {
+    name: string,
+    math: number,
+    english: number,
+    geography: number,
+    history: number,
+    // calculatGrade: Function,
+    // // average: Function,
+}
+
+const moriaGrade: grade = {
+    name: "moria",
+    math: 65,
+    english: 70,
+    geography: 80,
+    history: 75,
+    // calculatGrade: average
+
+}
+
+const talGrade: grade = {
+    name: "tal",
+    math: 95,
+    english: 80,
+    geography: 90,
+    history: 100,
+    // calculatGrade: average
+
+}
+
+const shirGrade: grade = {
+    name: "shir",
+    math: 85,
+    english: 100,
+    geography: 80,
+    history: 95,
+    // calculatGrade: average
+
+}
+// function average
