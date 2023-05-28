f090ae26422735434f659cbc891a3201656f962e
moria-avraham <128312303+moria-avraham@users.noreply.github.com>
2023-05-28 16:54:15 +0300

    .

--- 04-Typescript/08-objects/Home-puzzles/moria/dist/index.js
+++ 04-Typescript/08-objects/Home-puzzles/moria/dist/index.js
@@ -1,22 +1,25 @@
 var moriaGrade = {
     name: "moria",
-    math: 65,
-    english: 70,
-    geography: 80,
-    history: 75
+    gender: "female",
+    mathMidTermGrade: 65,
+    historyMidTermGrade: 70,
+    mathFinalTermGrade: 80,
+    historyFinalTermGrade: 75
 };
 var talGrade = {
     name: "tal",
-    math: 95,
-    english: 80,
-    geography: 90,
-    history: 100
+    gender: "female",
+    mathMidTermGrade: 95,
+    historyMidTermGrade: 80,
+    mathFinalTermGrade: 90,
+    historyFinalTermGrade: 100
 };
 var shirGrade = {
     name: "shir",
-    math: 85,
-    english: 100,
-    geography: 80,
-    history: 95
+    gender: "female",
+    mathMidTermGrade: 85,
+    historyMidTermGrade: 100,
+    mathFinalTermGrade: 80,
+    historyFinalTermGrade: 95
 };
 // function average
--- 04-Typescript/08-objects/Home-puzzles/moria/index.ts
+++ 04-Typescript/08-objects/Home-puzzles/moria/index.ts
@@ -1,39 +1,44 @@
 interface grade {
     name: string,
-    math: number,
-    english: number,
-    geography: number,
-    history: number,
+    gender: string,
+    mathMidTermGrade: number,
+    historyMidTermGrade: number,
+    mathFinalTermGrade: number,
+    historyFinalTermGrade: number,
     // calculatGrade: Function,
     // // average: Function,
 }
 
 const moriaGrade: grade = {
     name: "moria",
-    math: 65,
-    english: 70,
-    geography: 80,
-    history: 75,
+    gender: "female",
+    mathMidTermGrade: 65,
+    historyMidTermGrade: 70,
+    mathFinalTermGrade: 80,
+    historyFinalTermGrade: 75,
     // calculatGrade: average
 
 }
 
 const talGrade: grade = {
     name: "tal",
-    math: 95,
-    english: 80,
-    geography: 90,
-    history: 100,
+    gender: "female",
+    mathMidTermGrade: 95,
+    historyMidTermGrade: 80,
+    mathFinalTermGrade: 90,
+    historyFinalTermGrade: 100,
     // calculatGrade: average
+
 
 }
 
 const shirGrade: grade = {
     name: "shir",
-    math: 85,
-    english: 100,
-    geography: 80,
-    history: 95,
+    gender: "female",
+    mathMidTermGrade: 85,
+    historyMidTermGrade: 100,
+    mathFinalTermGrade: 80,
+    historyFinalTermGrade: 95,
     // calculatGrade: average
 
 }
