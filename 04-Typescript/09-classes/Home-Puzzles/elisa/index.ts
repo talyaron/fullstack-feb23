c41db7284be49b5ddee16bba3de9291a62791a44
elisaelg <83836208+elisaelg@users.noreply.github.com>
2023-05-30 08:12:33 +0300

    123456

---
+++ 04-Typescript/09-classes/Home-Puzzles/elisa/index.ts
@@ -0,0 +1,65 @@
+// ## Celebs
+// 1. create a class of celebs (influencers) with properties: name, genre, account in TikTok,
+//  account in Instagram, and the number of followers.
+class Celeb {
+  name: string;
+  genre: string;
+  tiktokAccount: string;
+  instagramAccount: string;
+  numberOfFollowers: number;
+
+  constructor(
+    name: string,
+    genre: string,
+    tiktokAccount: string,
+    instagramAccount: string,
+    numberOfFollowers: number
+  ) {
+    this.name = name;
+    this.genre = genre;
+    this.tiktokAccount = tiktokAccount;
+    this.instagramAccount = instagramAccount;
+    this.numberOfFollowers = numberOfFollowers;
+  }
+
+  get followersCount() {
+    return this.numberOfFollowers;
+  }
+
+  set followersCount(numberOfFollowers: number) {
+    this.numberOfFollowers = numberOfFollowers;
+  }
+
+  toString() {
+    return `${this.name} is a ${this.genre} influencer with ${this.numberOfFollowers} followers on TikTok and ${this.numberOfFollowers} followers on Instagram.`;
+  }
+}
+const galGdot = new Celeb(
+  'Gal Gadot',
+  'Cinema',
+  'Gal Gadot.tiktok',
+  'Gal Gadot.instegram',
+  1212121212
+);
+const jlo = new Celeb(
+  'Jennifer Lopez',
+  'Music',
+  'Jennifer Lopez.tiktok',
+  'Jennifer Lopez.instegram',
+  212121212
+);
+console.log(galGdot.toString());
+console.log(jlo.toString());
+
+// 2. based on question number one, create a method to set the number of followers and another method to get the number of followers.
+//    Create a few celebs-objects, and set the number of followers. Write to the console each celeb TikTok account.
+
+// 3. create a function that gets all the celebs and returns the celeb with the most followers. Even better, return a list of celebs, ordered by their number of followers. (use array)
+
+// ## Moment Class
+// 4. Create a class for dates. it gets Date format (new Date()). the instnce(object) gets a date and one method returns the date in the format: "dd/mm/yyyy, hh:mm:ss" for instance, gets a date (new Date ())returns "12/1/2023 17:56:12". And another method return in the following format "2 seconds past from that date" (getSimpleDate(), getPasedDate())
+
+// const myDate = new Moment(new Date()).
+// myDate.getSimpleDate() -> dd/mm/yyyy, hh:mm:ss
+// use "setTimeout()" to postpone the time...
+// myDate.getPasedDate() -> 2 seconds past from that date
