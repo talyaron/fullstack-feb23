0564a42b4c63a6d805b5e5e9dea1dd65df44238a
ziv leloneck <119840698+zivll1991@users.noreply.github.com>
2023-05-31 20:18:04 +0300

    ver11

--- 04-Typescript/10-arrays/followup/room3/mission2/index.ts
+++ 04-Typescript/10-arrays/followup/room3/mission2/index.ts
@@ -1,20 +1,24 @@
 const array = [1, 43, 56, 76, 33, 455, 6, 6];
-const found = array.find((element) => element % 2 == 0);
-const found1 = array.find((element) => aa(element) == 14);
+const found = array.find((element, index) => element % 2 == 0 && index > 3);
+const foundIndex = array.findIndex(
+  (element, index) => element % 2 == 0 && index > 3
+);
+// const found1 = array.find((element) => aa(element) == 14);
 console.log(found);
-debugger;
-console.log(found1);
+console.log(foundIndex);
+// debugger;
+// console.log(found1);
 
-function aa(element) {
-  try {
-    let sum = 0;
-    for (let i = 0; i < element.toString().length; i++) {
-      sum += element % 10;
-      element = Math.floor(element / 10);
-    }
-    return sum;
-  } catch (error) {
-    console.error(error);
-    return undefined;
-  }
-}
+// function aa(element) {
+//   try {
+//     let sum = 0;
+//     for (let i = 0; i < element.toString().length; i++) {
+//       sum += element % 10;
+//       element = Math.floor(element / 10);
+//     }
+//     return sum;
+//   } catch (error) {
+//     console.error(error);
+//     return undefined;
+//   }
+// }
