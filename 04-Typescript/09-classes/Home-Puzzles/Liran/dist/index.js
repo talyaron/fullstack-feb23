1e2792f78c3fbb68c79dfa67fb3364f7fd0af8d6
liran26 <liranav26@gmail.com>
2023-05-31 16:44:12 +0300

    update

---
+++ 04-Typescript/09-classes/Home-Puzzles/Liran/dist/index.js
@@ -0,0 +1,31 @@
+var MyDate = /** @class */ (function () {
+    function MyDate(date) {
+        this.day = date.getDate();
+        this.month = date.getMonth() + 1;
+        this.year = date.getFullYear();
+        this.second = date.getSeconds();
+        this.minutes = date.getMinutes();
+        this.hours = date.getHours();
+        this.timePast = 0;
+    }
+    MyDate.prototype.getSimpleDate = function () {
+        // return (`${this.day}/${this.month}/${this.year}, ${this.hours}:${this.minutes}:${this.second}`)
+        return (this.day + "/" + this.month + "/" + this.year + ", " + this.hours + ":" + ((this.minutes > 9 || this.minutes < (-9)) ? Math.abs(this.minutes) : "0" + Math.abs(this.minutes))
+            + ":" + ((this.second > 9 || this.second < (-9)) ? Math.abs(this.second) : "0" + Math.abs(this.second)));
+    };
+    MyDate.prototype.getPassedDate = function () {
+        debugger;
+        var current = new Date();
+        console.log("getPassed() on at: " + current);
+        if (current.getSeconds() < this.second) {
+            return (60 - this.second + current.getSeconds()) + " second passed since MyDay created";
+        }
+        return (current.getSeconds() - this.second) + " second passed since MyDay created";
+    };
+    return MyDate;
+}());
+var newDate = new MyDate(new Date());
+console.log(newDate.getSimpleDate());
+setTimeout(function () {
+    console.log(newDate.getPassedDate());
+}, 1000);
