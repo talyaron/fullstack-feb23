7ee5f74d673c73b7df93ccb3cda82db0d6c38e5a
moria-avraham <128312303+moria-avraham@users.noreply.github.com>
2023-06-01 22:28:27 +0300

    .

---
+++ 04-Typescript/09-classes/Home-Puzzles/moria/dist/index.js
@@ -0,0 +1,10 @@
+var celebrity = /** @class */ (function () {
+    function celebrity(name, followers, genre, account, Instagram) {
+        this.name = name;
+        this.followers = followers;
+        this.genre = genre;
+        this.account = account;
+        this.Instagram = Instagram;
+    }
+    return celebrity;
+}());
---
+++ 04-Typescript/09-classes/Home-Puzzles/moria/index.ts
@@ -0,0 +1,24 @@
+
+class celebrity {
+    name: string;
+    followers: number;
+    genre: string;
+    account: string;
+    Instagram: string;
+
+
+    constructor(
+        name: string,
+        followers: number,
+        genre: string,
+        account: string,
+        Instagram: string,
+
+    ) {
+        this.name = name;
+        this.followers = followers;
+        this.genre = genre;
+        this.account = account;
+        this.Instagram = Instagram;
+    }
+}
