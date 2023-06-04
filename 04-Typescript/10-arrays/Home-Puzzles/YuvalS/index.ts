6b8e04f68c953b303a1a6004074b1fade36bff65
Shtig7 <128290647+Shtig7@users.noreply.github.com>
2023-06-04 20:13:21 +0300

    1

--- 04-Typescript/10-arrays/Home-Puzzles/YuvalS/index.ts
+++ 04-Typescript/10-arrays/Home-Puzzles/YuvalS/index.ts
@@ -5,14 +5,22 @@
 // const ages1: number[] = [5, 8, 3, 25, 100, 15, 10]
 // ages1.sort((a, b) => a-b);
 // console.log(ages1)
-const numbers1: number[] = [5, 8, 3, 25, 100, 15, 10]
-const evenNumberss: number[] = new Array ();
-for(let i = 0; i < numbers1.length; i++ ){
-    if(numbers1[i] % 2 == 0){
-        evenNumberss[i] += numbers1[1]
+// const numbers1: number[] = [5, 8, 3, 25, 100, 15, 10]
+// const evenNumberss: number[] = [];
+// for(let i = 0; i < numbers1.length; i++ ){
+//     if(numbers1[i] % 2 == 0){
+//         evenNumberss.push(numbers1[i])
         
 
-    }
-}
+//     }
+// }
+// console.log(evenNumberss)
+
+const numbers1: number[] = [5, 8, 3, 25, 100, 15, 10, 2, 4, 6]
+// const evenNumberss: number[] = [];
+const evenNumberss: number[] = numbers1.filter(elm=> elm % 2 == 0)
 console.log(evenNumberss)
+
+
+
 
