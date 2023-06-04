c204819424fb391ccb71068fe833fd77c41abacd
Eliyaho basson <basson131@gmail.com>
2023-06-04 19:47:50 +0300

    1

--- 04-Typescript/10-Arrays/followup/room11/dist/index.js
+++ 04-Typescript/10-Arrays/followup/room11/dist/index.js
@@ -1,9 +1,9 @@
-// const arr = [21,`k`,4,6767,4,342,4,6,1,8,88,6]
-// const arrStr = [`ee`,`el`,`bjh`,`gg`]
-// const map= arr.map(elm => elm+`t`)
-// console.log(map);
-// const some = arrStr.some(elm => elm.length==1)
-// console.log(some);
+var arr = [21, "k", 4, 6767, 4, 342, 4, 6, 1, 8, 88, 6];
+var arrStr = ["ee", "el", "bjh", "gg"];
+var map = arr.map(function (elm) { return elm + "t"; });
+console.log(map);
+var some = arrStr.some(function (elm) { return elm.length == 1; });
+console.log(some);
 var array1 = [1, 2, 3, 4];
 var initialValue = 0;
 debugger;
--- 04-Typescript/10-Arrays/followup/room11/index.ts
+++ 04-Typescript/10-Arrays/followup/room11/index.ts
@@ -1,11 +1,11 @@
 
-// const arr = [21,`k`,4,6767,4,342,4,6,1,8,88,6]
-// const arrStr = [`ee`,`el`,`bjh`,`gg`]
-// const map= arr.map(elm => elm+`t`)
-// console.log(map);
+const arr = [21,`k`,4,6767,4,342,4,6,1,8,88,6]
+const arrStr = [`ee`,`el`,`bjh`,`gg`]
+const map= arr.map(elm => elm+`t`)
+console.log(map);
 
-// const some = arrStr.some(elm => elm.length==1)
-// console.log(some);
+const some = arrStr.some(elm => elm.length==1)
+console.log(some);
 
 const array1 = [1, 2, 3, 4];
 
