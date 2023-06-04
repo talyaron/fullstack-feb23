a52ef19f44adc2dd95e5a3f6409307183c7052e5
Shtig7 <128290647+Shtig7@users.noreply.github.com>
2023-06-04 17:51:45 +0300

    1

---
+++ 04-Typescript/10-arrays/Home-Puzzles/YuvalS/dist/index.js
@@ -0,0 +1,14 @@
+// const fruits1: string[] = ["banana", "apple", "peach", "cherry", "mango"]
+// fruits1.splice(2, 1)
+// console.log(fruits1)
+// const ages1: number[] = [5, 8, 3, 25, 100, 15, 10]
+// ages1.sort((a, b) => a-b);
+// console.log(ages1)
+var numbers1 = [5, 8, 3, 25, 100, 15, 10];
+var evenNumberss = new Array();
+for (var i = 0; i < numbers1.length; i++) {
+    if (numbers1[i] % 2 == 0) {
+        evenNumberss[i] += numbers1[1];
+    }
+}
+console.log(evenNumberss);
