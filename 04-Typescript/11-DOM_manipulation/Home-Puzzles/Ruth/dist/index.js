12e32e4415d01011901f9bf1681ed356a97d35f0
ruttur66 <112100490+RuthBenTov@users.noreply.github.com>
2023-06-06 18:19:05 +0300

    ok

--- 04-Typescript/11-DOM_manipulation/Home-Puzzles/Ruth/dist/index.js
+++ 04-Typescript/11-DOM_manipulation/Home-Puzzles/Ruth/dist/index.js
@@ -1,2 +1,10 @@
 var body = document.body;
-var ;
+var Post = /** @class */ (function () {
+    function Post(_author, _imgSrc, _text) {
+        this.author = _author;
+        this.imgSrc = _imgSrc;
+        this.text = _text;
+        this.likes = 0;
+    }
+    return Post;
+}());
--- 04-Typescript/11-DOM_manipulation/Home-Puzzles/Ruth/index.ts
+++ 04-Typescript/11-DOM_manipulation/Home-Puzzles/Ruth/index.ts
@@ -1,2 +1,15 @@
-const body = document.body
-const 
+const body = document.body;
+
+class Post {
+  author: string;
+  imgSrc: string;
+  text: string;
+  likes: number;
+
+  constructor(_author: string, _imgSrc: string, _text: string) {
+    this.author = _author;
+    this.imgSrc = _imgSrc;
+    this.text = _text;
+    this.likes = 0;
+  }
+}
