07e0f0566cb494d052428754866cf451f732f09d
navered <128045047+navered@users.noreply.github.com>
2023-06-06 18:07:48 +0300

    nave

---
+++ 04-Typescript/10-Arrays/followup/nave&shahar/dist/index.js
@@ -0,0 +1,8 @@
+var ages = [18, 25, 22, 25, 100];
+var index = ages.every(function (age) { return age > 17; });
+console.log(index);
+var fruits = ["banana", "apple", "mango", "melon", "watermelon"];
+var sort = fruits.slice(1, 4);
+console.log(sort);
+var cars, _a = void 0;
+"volvo", ;
