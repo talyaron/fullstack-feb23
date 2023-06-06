6651c152426f4f42dad0413d937eb6facc95219c
moria-avraham <128312303+moria-avraham@users.noreply.github.com>
2023-06-06 17:55:09 +0300

    .

---
+++ 04-Typescript/11-DOM_manipulation/Home-Puzzles/moria/dist/index.js
@@ -0,0 +1,23 @@
+var People = /** @class */ (function () {
+    function People(name, imgg) {
+        this.name = name;
+        this.imgg = imgg;
+    }
+    return People;
+}());
+var people = [
+    new People("mor", ' <img src="dist/img/images.jpg">'),
+    new People("shir", '  <img src="dist/img/הורדה (1).jpg">'),
+    new People("or", ' <img src="dist/img/הורדה.jpg">'),
+    new People("avi", '<img src="dist/img/הורדה (2).jpg">'),
+];
+var div = document.querySelector("#div");
+var imgDiv = "<div class='img'>";
+imgDiv += people.map(function (peoplee) {
+    return "<div class=\"info\">  " + peoplee.imgg + " " + peoplee.name + "</div>";
+})
+    .join(" ");
+imgDiv += "<div>";
+if (div) {
+    div.innerHTML = imgDiv;
+}
