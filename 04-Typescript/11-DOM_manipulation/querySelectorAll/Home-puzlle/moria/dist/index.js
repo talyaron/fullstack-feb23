20ec5a50a7d96a0abaa16a0f5c55c64e60369c11
moria-avraham <128312303+moria-avraham@users.noreply.github.com>
2023-06-08 13:54:45 +0300

    .

---
+++ 04-Typescript/11-DOM_manipulation/querySelectorAll/Home-puzlle/moria/dist/index.js
@@ -0,0 +1,6 @@
+// get five image URLs from the user and print five images on the DOM.
+var firstImage = (prompt("Please write the URL of your first image"));
+var secondImage = (prompt("Please write the URL of your second image"));
+var thirdImage = (prompt("Please write the URL of your third image"));
+var fourthImage = (prompt("Please write the URL of your fourth image"));
+var fifthImage = (prompt("Please write the URL of your fifth image"));
---
+++ 04-Typescript/11-DOM_manipulation/querySelectorAll/Home-puzlle/moria/index.ts
@@ -0,0 +1,7 @@
+// get five image URLs from the user and print five images on the DOM.
+
+const firstImage: string | null = (prompt("Please write the URL of your first image"));
+const secondImage: string | null = (prompt("Please write the URL of your second image"));
+const thirdImage: string | null = (prompt("Please write the URL of your third image"));
+const fourthImage: string | null = (prompt("Please write the URL of your fourth image"));
+const fifthImage: string | null = (prompt("Please write the URL of your fifth image"));
