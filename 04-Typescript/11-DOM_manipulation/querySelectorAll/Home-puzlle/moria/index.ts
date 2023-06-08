ca2c409622ece1dbcc6479f58082af6dec10919b
moria-avraham <128312303+moria-avraham@users.noreply.github.com>
2023-06-08 19:18:03 +0300

    .

--- 04-Typescript/11-DOM_manipulation/querySelectorAll/Home-puzlle/moria/index.ts
+++ 04-Typescript/11-DOM_manipulation/querySelectorAll/Home-puzlle/moria/index.ts
@@ -5,3 +5,23 @@ const secondImage: string | null = (prompt("Please write the URL of your second 
 const thirdImage: string | null = (prompt("Please write the URL of your third image"));
 const fourthImage: string | null = (prompt("Please write the URL of your fourth image"));
 const fifthImage: string | null = (prompt("Please write the URL of your fifth image"));
+
+
+const div = document.querySelector("#div")!;
+
+
+// let divImages: string = `<div class='images'>`;
+
+// const images: HTMLDivElement | null = document.querySelector(".images")!;
+// if (firstImage && div) {
+//     // background: url(images / fire.jpg) no - repeat center center / cover;
+//     images.style.background = ii(firstImage);
+// }
+// function ii(elm: string)=> {
+//     return "url(" + `${elm}` + ")";
+// }
+
+// if (div) {
+//     div.innerHTML = images;
+
+// }
