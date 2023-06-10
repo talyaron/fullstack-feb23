d47cd5ecfd2db34ffc993c16a6c2798947c47d25
ziv leloneck <119840698+zivll1991@users.noreply.github.com>
2023-06-11 00:34:06 +0300

    ver 1.1

--- 04-Typescript/11-DOM_manipulation/querySelectorAll/Home-puzlle/zivll/dist/index.js
+++ 04-Typescript/11-DOM_manipulation/querySelectorAll/Home-puzlle/zivll/dist/index.js
@@ -1,9 +1,24 @@
-var imagesByUser = [];
-for (var i = 0; i >= 5; i++) {
-    imagesByUser[i] = prompt("What is your images URL? please");
+var ImagesByUser = /** @class */ (function () {
+    function ImagesByUser(type, url) {
+        this.type = type;
+        this.url = url;
+    }
+    return ImagesByUser;
+}());
+var imagesByUser = [
+    new ImagesByUser(prompt("what is the name of your image?"), prompt("What is your images URL?")),
+    new ImagesByUser(prompt("what is the name of your image?"), prompt("What is your images URL?")),
+    new ImagesByUser(prompt("what is the name of your image?"), prompt("What is your images URL?")),
+    new ImagesByUser(prompt("what is the name of your image?"), prompt("What is your images URL?")),
+    new ImagesByUser(prompt("what is the name of your image?"), prompt("What is your images URL?")),
+];
+// debugger;
+var userImages = document.querySelector("#user-images");
+var imagesToHTML = imagesByUser
+    .map(function (img) {
+    return "<p>image type: " + img.type + "<br><br><img src=\"" + img.url + "\">";
+})
+    .join(" ");
+if (userImages) {
+    userImages.innerHTML = imagesToHTML;
 }
-// for (let i = 0; i >= imagesByUser.length; i++) {
-//   document.querySelectorAll(`#user-images`);
-// }
-var userImages = document.querySelectorAll("#user-images");
-var newArray = imagesByUser.map(function () { return ; });
