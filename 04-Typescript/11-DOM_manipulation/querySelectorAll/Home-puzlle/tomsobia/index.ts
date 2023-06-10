a2dc8e9677727171ed62b60deb41035bdf3d379e
tomsobia <tom162sobia@gmail.com>
2023-06-10 17:12:25 +0300

    new

---
+++ 04-Typescript/11-DOM_manipulation/querySelectorAll/Home-puzlle/tomsobia/index.ts
@@ -0,0 +1,37 @@
+class User {
+    ImgName: string;
+    Img: string;
+
+    constructor(ImgName: string, Img: string) {
+        this.ImgName = ImgName;
+        this.Img = Img;
+    }
+}
+
+const firstImgName = prompt("Enter the name of the first image");
+const firstImgUrl = prompt("Please write the URL of the first image");
+const secondImgName = prompt("Enter the name of the second image");
+const secondImgUrl = prompt("Please write the URL of the second image");
+const thirdImgName = prompt("Enter the name of the third image");
+const thirdImgUrl = prompt("Please write the URL of the third image");
+const fourthImgName = prompt("Enter the name of the fourth image");
+const fourthImgUrl = prompt("Please write the URL of the fourth image");
+const fifthImgName = prompt("Enter the name of the fifth image");
+const fifthImgUrl = prompt("Please write the URL of the fifth image");
+
+const firstUser = new User(firstImgName, firstImgUrl);
+const secondUser = new User(secondImgName, secondImgUrl);
+const thirdUser = new User(thirdImgName, thirdImgUrl);
+const fourthUser = new User(fourthImgName, fourthImgUrl);
+const fifthUser = new User(fifthImgName, fifthImgUrl);
+
+const div = document.getElementById("box1");
+
+div.innerHTML += `<p>First User: ${firstUser.ImgName}, <img src="${firstUser.Img}"></img></p><hr>`;
+div.innerHTML += `<p>Second User: ${secondUser.ImgName}, <img src="${firstUser.Img}"></img></p><hr>`;
+div.innerHTML += `<p>Third User: ${thirdUser.ImgName}, <img src="${firstUser.Img}"></img></p><hr>`;
+div.innerHTML += `<p>Fourth User: ${fourthUser.ImgName},<img src="${firstUser.Img}"></img></p><hr>`;
+div.innerHTML += `<p>Fifth User: ${fifthUser.ImgName}, <img src="${firstUser.Img}"></img></p><hr>`;
+
+
+
