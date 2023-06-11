0322f07238a5eb6a77fc9dbdbbf2032f07e04cde
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2023-06-11 20:03:51 +0300

    .

---
+++ 04-Typescript/11-DOM_manipulation/Home-Puzzles/Ruth/dist/index.js
@@ -0,0 +1,54 @@
+var body = document.body;
+var Post = /** @class */ (function () {
+    function Post(_author, _imgSrc, _text) {
+        this.author = _author;
+        this.imgSrc = _imgSrc;
+        this.text = _text;
+        this.likes = 0;
+        this.id = String(Date.now().toString(32) + Math.random().toString(16)).replace(/\./g, "");
+    }
+    Post.prototype.addLike = function () {
+        this.likes++;
+    };
+    Post.prototype.getLikes = function () {
+        return this.likes;
+    };
+    Post.prototype.NewPostHtml = function () {
+        return "<div class=\"gallery__post\" id = \"" + this.id + "\">\n    <h3 class=\"author\">" + this.author + "</h3>\n    <h3 class=\"author\">" + this.text + "</h3>\n    <img src=" + this.imgSrc + " alt=\"\">\n    <div class=\"likesDiv\">\n      <span onclick=\"doingLike(event)\" class=\"material-symbols-outlined\">favorite</span>\n      <p>" + this.getLikes() + "</p>\n    </div>\n    </div>";
+    };
+    Post.prototype.renderNewPost = function () {
+        var gallery = document.querySelector(".gallery");
+        gallery.innerHTML += this.NewPostHtml();
+    };
+    return Post;
+}());
+var postsArray = [
+    new Post("ruthbentov 1", "https://cdn.pixabay.com/photo/2023/06/01/06/22/british-shorthair-8032816_1280.jpg", "hello world"),
+    new Post("ruthbentov 2", "https://cdn.pixabay.com/photo/2023/05/01/11/48/bird-7962790_1280.jpg", "hello world"),
+    new Post("ruthBentov 3", "https://cdn.pixabay.com/photo/2023/05/31/18/15/st-stephens-basilica-8031985_1280.jpg", "hello world"),
+    new Post("ruthbentov 4", "https://cdn.pixabay.com/photo/2023/01/16/19/13/laptop-7723139_1280.jpg", "hello world"),
+    new Post("ruthbentov 5", "https://cdn.pixabay.com/photo/2023/01/16/19/13/laptop-7723139_1280.jpg", "hello world"),
+    new Post("ruthbentov 6", "https://cdn.pixabay.com/photo/2023/01/16/19/13/laptop-7723139_1280.jpg", "hello world"),
+];
+postsArray.forEach(function (element) {
+    element.renderNewPost();
+});
+var doingLike = function (event) {
+    var elem = event.target;
+    var idElem = event.target.parentNode.parentNode.id;
+    var LikeNum = event.target.parentNode.querySelector("p");
+    LikeNum.innerHTML++;
+    updateInObject(idElem);
+};
+function updateInObject(idElem) {
+    var postById = postsArray.find(function (elem) { return elem.id == idElem; });
+    try {
+        if (!postById)
+            throw new Error();
+        postById === null || postById === void 0 ? void 0 : postById.addLike();
+        console.log(postById);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
