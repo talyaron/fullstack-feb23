a1db36159307b7f48e0e2cad5abf61871ad4513f
itahTamar <125893355+itahTamar@users.noreply.github.com>
2023-06-13 12:01:15 +0300

    hw13-finish level 1

--- 04-Typescript/13-dom-events/home-puzzles/tamar_i/dist/index.js
+++ 04-Typescript/13-dom-events/home-puzzles/tamar_i/dist/index.js
@@ -1,7 +1,9 @@
+//1
 var audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
 var box = document.querySelectorAll(".box");
 box.forEach(function (box) {
-    box.addEventListener("click", function () {
+    box.addEventListener("click", function (ev) {
+        ev.target.style.backgroundColor = "red";
         audio.play();
     });
 });
--- 04-Typescript/13-dom-events/home-puzzles/tamar_i/index.ts
+++ 04-Typescript/13-dom-events/home-puzzles/tamar_i/index.ts
@@ -1,8 +1,11 @@
+//1
+
 const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
 const box = document.querySelectorAll(".box");
 
 box.forEach(box => {
-  box.addEventListener("click", () => {
+  box.addEventListener("click", (ev:any) => {
+    ev.target.style.backgroundColor = "red";
     audio.play();
   });
 });
