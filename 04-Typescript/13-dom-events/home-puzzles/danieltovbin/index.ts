44c53f24cfce22fd94894246574d6c5cbfac54cb
danieltovbin <111869103+danieltovbin@users.noreply.github.com>
2023-06-14 10:12:11 +0300

    /

--- 04-Typescript/13-dom-events/home-puzzles/danieltovbin/index.ts
+++ 04-Typescript/13-dom-events/home-puzzles/danieltovbin/index.ts
@@ -4,4 +4,45 @@
 
 
 
-const box 
+const div = document.createElement('div') as HTMLDivElement;
+const box = document.createElement('div') as HTMLDivElement;
+div.classList.add('box')
+document.body.append(div)
+
+const div1 = document.createElement('div') as HTMLDivElement;
+div1.classList.add('box')
+document.body.append(div1)
+
+
+const mySound = document.querySelector('#mySound') as HTMLAudioElement;
+const mySoundTwo = document.querySelector('#mySoundTwo') as HTMLAudioElement;
+
+div.onclick = playSound;
+div1.onclick = playSoundTwo;
+
+
+function playSound() {
+    mySound.play();
+}
+
+function playSoundTwo () {
+    mySoundTwo.play();
+}
+
+const stopIcon = document.createElement('i');
+stopIcon.classList.add('stop-icon', 'fa', 'fa-pause');
+box.append(stopIcon)
+
+const playIcon = document.createElement('i');
+playIcon.classList.add('play-icon', 'fa', 'fa-play');
+box.append(playIcon)
+
+function toggleOne(){
+    mySound.paused ? mySound.play() : mySound.pause();
+    box.classList.toggle('playing')
+}
+
+function toggleTwo() {
+    mySoundTwo.paused ? mySoundTwo.play() : mySoundTwo.pause();
+    box.classList.toggle('playing')
+}
