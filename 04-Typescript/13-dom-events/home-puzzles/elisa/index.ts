a55f4d518052806711df84222401f831915f6f47
elisaelg <83836208+elisaelg@users.noreply.github.com>
2023-06-14 18:52:47 +0300

    111

--- 04-Typescript/13-dom-events/home-puzzles/elisa/index.ts
+++ 04-Typescript/13-dom-events/home-puzzles/elisa/index.ts
@@ -5,25 +5,37 @@ function playSound() {
   const audio = new Audio('./dist/audio/coldplay_clocks.mp3');
   audio.play();
 }
-
-// Get the play button element
 const playButton = document.getElementById('playButton');
-
-// Add an event listener to the button to play the sound when clicked
 playButton?.addEventListener('click', playSound);
 
 // 2) Create images of calm faces on the screen. When clicked, change the face into to a frighting clown. when the mouse leaves, change back to the nice face.
 // Get the image element
 const image = document.getElementById('myImage') as HTMLImageElement;
-
 function changeImage() {
   image.src = './dist/audio/angryface.jpg';
 }
-
 function revertImage() {
   image.src = './dist/audio/calmface.jpg';
 }
-
 image.addEventListener('click', changeImage);
 image.addEventListener('mousemove', revertImage);
+
 // 3) Create images of dogs on the screen. when the mouse leave the dog, the dog follows the mouse.
+// const dogImage = document.querySelector<HTMLImageElement>('#dog-image');
+
+// if (dogImage) {
+//   dogImage.addEventListener('mouseenter', (event: MouseEvent) => {
+//     if (dogImage) {
+//       dogImage.style.cursor = 'default';
+//     }
+//   });
+
+//   dogImage.addEventListener('mouseleave', (event: MouseEvent) => {
+//     if (dogImage) {
+//       dogImage.style.cursor = 'pointer';
+//       const mousePosition = event.clientX;
+//       dogImage.style.left = mousePosition + 'px';
+//     }
+//   });
+// }
+
