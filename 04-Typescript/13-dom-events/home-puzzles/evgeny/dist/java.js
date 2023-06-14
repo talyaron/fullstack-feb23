0cbdc391a4087dc32d2d76952d21d32b76c3dc25
EvgenyUsh94 <97188387+EvgenyUsh94@users.noreply.github.com>
2023-06-14 03:43:09 +0300

    s

--- 04-Typescript/13-dom-events/home-puzzles/evgeny/dist/java.js
+++ 04-Typescript/13-dom-events/home-puzzles/evgeny/dist/java.js
@@ -1,13 +1,40 @@
 var dog = document.querySelector(".box");
-function test(event) {
-    // console.log(event.clientY)
-    debugger;
-    var Xline = dog.style.left = event.clientX + "px";
-    var Yline = dog.style.top = event.clientY + "px";
-    console.log(event.clientX);
-    console.log(Xline);
-    dog.style.transition = "all 3s";
-    if (Xline > event.clientX) {
+var boneDrop = document.querySelector(".boneDrop");
+var wrapper = document.querySelector(".wrapper");
+var bonesEaten = document.querySelector(".boneEaten");
+var count = 0;
+wrapper.addEventListener("transitionend", function () {
+    boneDrop.innerHTML = "";
+    removeBone();
+    countCounter();
+});
+function clicked(event) {
+    if (dog.offsetLeft < event.clientX) {
+        dog.style.transform = "scale( 1)";
+    }
+    if (dog.offsetLeft > event.clientX) {
         dog.style.transform = "scale(-1, 1)";
     }
+    return dogMove(event), addBone(event), count++;
 }
+;
+function dogMove(event) {
+    dog.style.left = event.clientX + "px";
+    dog.style.top = event.clientY + "px";
+    dog.style.transition = " 3s";
+}
+;
+function countCounter() {
+    bonesEaten.innerHTML = "your dog has eaten " + count + " bones";
+}
+;
+function addBone(event) {
+    boneDrop.innerHTML = "<img src=\"https://i.etsystatic.com/15536434/r/il/f478ff/2462002336/il_fullxfull.2462002336_iimi.jpg\" height=\"50em\" width=\"50rem\" alt=\"\">";
+    boneDrop.style.left = event.clientX + "px";
+    boneDrop.style.top = event.clientY + "px";
+}
+;
+function removeBone() {
+    boneDrop.innerHTML = "";
+}
+;
--- 04-Typescript/13-dom-events/home-puzzles/evgeny/java.ts
+++ 04-Typescript/13-dom-events/home-puzzles/evgeny/java.ts
@@ -1,17 +1,47 @@
-let dog:any= document.querySelector(".box");
 
+const dog:HTMLDivElement|null= document.querySelector(".box");
+const boneDrop:HTMLDivElement|null=document.querySelector(".boneDrop");
+const wrapper:HTMLDivElement|null= document.querySelector(".wrapper");
+const bonesEaten:HTMLDivElement|null=document.querySelector(".boneEaten");
+let count = 0;
 
-function test (event){
-    // console.log(event.clientY)
-    debugger
-    let Xline= dog.style.left=`${event.clientX}px`
-    let Yline=dog.style.top=`${event.clientY}px`
+wrapper!.addEventListener("transitionend", () => {
+    boneDrop!.innerHTML = "";
+    removeBone()
+    countCounter()
+    
+});
 
-    console.log(event.clientX)
-    console.log(Xline)
-    dog.style.transition="all 3s"
-    if( Xline>event.clientX){
-        dog.style.transform= "scale(-1, 1)";
+function clicked (event){
+    
+    if( dog!.offsetLeft<event.clientX){
+        
+        dog!.style.transform= "scale( 1)";
     }
-}
+    if ( dog!.offsetLeft>event.clientX){
+        
+        dog!.style.transform= "scale(-1, 1)";
+    }
+    return dogMove(event),addBone(event),count++;
+};
 
+function dogMove(event){
+    dog!.style.left=`${event.clientX}px`
+    dog!.style.top=`${event.clientY}px`
+    dog!.style.transition=" 3s"
+};
+
+function countCounter(){
+    bonesEaten!.innerHTML=`your dog has eaten ${count} bones`
+   
+};
+
+function addBone(event){
+    boneDrop!.innerHTML=`<img src="https://i.etsystatic.com/15536434/r/il/f478ff/2462002336/il_fullxfull.2462002336_iimi.jpg" height="50em" width="50rem" alt="">`
+    boneDrop!.style.left=`${event.clientX}px`
+    boneDrop!.style.top=`${event.clientY}px`
+};
+
+function removeBone(){
+    boneDrop!.innerHTML=""
+};
