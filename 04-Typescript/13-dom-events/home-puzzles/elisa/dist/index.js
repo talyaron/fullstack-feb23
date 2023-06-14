4a8701ae2204a8b50ff2c81dda770b99c30d7b2e
elisaelg <83836208+elisaelg@users.noreply.github.com>
2023-06-14 08:39:02 +0300

    events

--- 04-Typescript/13-dom-events/home-puzzles/elisa/dist/index.js
+++ 04-Typescript/13-dom-events/home-puzzles/elisa/dist/index.js
@@ -1,3 +1,29 @@
 // 1) create boxes on the screen. when clicking on a button, play sound.
+// Create a function to play the sound when the button is clicked
+function playSound() {
+    var audio = new Audio('./dist/audio/coldplay_clocks.mp3');
+    audio.play();
+}
+// Get the play button element
+var playButton = document.getElementById('playButton');
+// Add an event listener to the button to play the sound when clicked
+playButton === null || playButton === void 0 ? void 0 : playButton.addEventListener('click', playSound);
 // 2) Create images of calm faces on the screen. When clicked, change the face into to a frighting clown. when the mouse leaves, change back to the nice face.
+// Get the image element
+var image = document.getElementById('image');
+// Store the paths for the two images
+var firstImagePath = './dist/audio/calmface.jpg';
+var secondImagePath = './dist/audio/angryface.jpg';
+// Function to change the image source
+function changeImage() {
+    if (image.src.endsWith(firstImagePath)) {
+        image.src = secondImagePath;
+    }
+    else {
+        image.src = firstImagePath;
+    }
+}
+// Add event listeners for click and mouseleave
+image.addEventListener('click', changeImage);
+image.addEventListener('mouseleave', changeImage);
 // 3) Create images of dogs on the screen. when the mouse leave the dog, the dog follows the mouse.
--- 04-Typescript/13-dom-events/home-puzzles/elisa/index.ts
+++ 04-Typescript/13-dom-events/home-puzzles/elisa/index.ts
@@ -1,4 +1,36 @@
 // 1) create boxes on the screen. when clicking on a button, play sound.
 
+// Create a function to play the sound when the button is clicked
+function playSound() {
+  const audio = new Audio('./dist/audio/coldplay_clocks.mp3');
+  audio.play();
+}
+
+// Get the play button element
+const playButton = document.getElementById('playButton');
+
+// Add an event listener to the button to play the sound when clicked
+playButton?.addEventListener('click', playSound);
+
 // 2) Create images of calm faces on the screen. When clicked, change the face into to a frighting clown. when the mouse leaves, change back to the nice face.
+// Get the image element
+const image = document.getElementById('image') as HTMLImageElement;
+
+// Store the paths for the two images
+const firstImagePath = './dist/audio/calmface.jpg';
+const secondImagePath = './dist/audio/angryface.jpg';
+
+// Function to change the image source
+function changeImage() {
+  if (image.src.endsWith(firstImagePath)) {
+    image.src = secondImagePath;
+  } else {
+    image.src = firstImagePath;
+  }
+}
+
+// Add event listeners for click and mouseleave
+image.addEventListener('click', changeImage);
+image.addEventListener('mouseleave', changeImage);
+
 // 3) Create images of dogs on the screen. when the mouse leave the dog, the dog follows the mouse.
