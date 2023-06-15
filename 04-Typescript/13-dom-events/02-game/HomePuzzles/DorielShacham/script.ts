e5f418cce47fd5ea581f0a2797d9cef591f8f53b
Doriel Shacham <dorielshacham@gmail.com>
2023-06-15 17:05:38 +0300

    done

---
+++ 04-Typescript/13-dom-events/02-game/HomePuzzles/DorielShacham/script.ts
@@ -0,0 +1,54 @@
+class Player {
+    width: number;
+    position: any;
+    height: number;
+    offset: { x: number; y: number; };
+    constructor({ position, offset = { x: 0, y: 0 } }) {
+        this.position = position;
+        this.width = 80;
+        this.height = 80;
+        this.offset = offset; 
+    }
+}
+
+const player = document.querySelector('#player') as HTMLElement;
+const player1 = new Player({ position: { x: 0, y: 0 } });
+
+function updatePlayer() {
+    player.style.top = `${player1.position.y}px`;
+    player.style.left = `${player1.position.x}px`;
+    player.style.width = `${player1.width}px`;
+    player.style.height = `${player1.height}px`;
+    //console.log(player1.position);
+    
+}
+
+function update() {
+    updatePlayer();
+    requestAnimationFrame(update);
+}
+update();
+
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'ArrowUp':
+            player1.position.y -= 50;
+            break;
+        case 'ArrowDown':
+            player1.position.y += 50;
+            break;
+        case 'ArrowLeft':
+            player1.position.x -= 50;
+            break;
+        case 'ArrowRight':
+            player1.position.x += 50;
+            break;
+    }
+    updatePlayer();
+});
+
+function red(){
+player.addEventListener('click', () => {
+    player.style.backgroundColor = 'red';
+})};
+
