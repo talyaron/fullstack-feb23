e2ecadb4a8b6b628c74bac3be67842b1d0fc0b63
ziv leloneck <119840698+zivll1991@users.noreply.github.com>
2023-06-15 16:20:15 +0300

    ver 1.1

--- 04-Typescript/13-dom-events/02-game/HomePuzzles/zivll/dist/script.js
+++ 04-Typescript/13-dom-events/02-game/HomePuzzles/zivll/dist/script.js
@@ -1,50 +1,58 @@
 // 2) create checkers board (דמקה)
 // create a pawn that moves accross the board. (using the keyboard)
 // 3) create many pawns that can move (press one of them and move it using the keyboard)
-// caching pawns
-var movement = 112.5;
 var Pawns = document.querySelectorAll(".pawn");
 Pawns.forEach(function (pawn) {
+    pawn.addEventListener("click", function (event) {
+        addingClassByClick(pawn);
+        // console.dir(pawn);
+    });
+});
+Pawns.forEach(function (pawn) {
     pawn.onclick = function (ev) {
+        console.dir(ev);
+        document.addEventListener("keyup", function (event) {
+            for (var i = 0; i < Pawns.length; i++) {
+                debugger;
+                // for (let n = 0; n < Pawns[i].classList.length; n++)
+                if (Pawns[i].classList[2] === "clicked") {
+                    switch (event.key) {
+                        case "ArrowUp":
+                            Pawns[i].style.top = Pawns[i].offsetTop - 112.5 + "px";
+                            break;
+                        case "ArrowDown":
+                            Pawns[i].style.top = Pawns[i].offsetTop + 112.5 + "px";
+                            break;
+                        case "ArrowLeft":
+                            Pawns[i].style.left = Pawns[i].offsetLeft - 112.5 + "px";
+                            break;
+                        case "ArrowRight":
+                            Pawns[i].style.left = Pawns[i].offsetLeft + 112.5 + "px";
+                            break;
+                    }
+                }
+            }
+        });
+    };
+    // debugger;
+});
+function addingClassByClick(pawn) {
+    try {
+        pawn.classList.add("clicked");
         for (var i = 0; i < Pawns.length; i++) {
             for (var n = 0; n < Pawns[i].classList.length; n++)
                 if (Pawns[i].classList[n] === "clicked") {
                     Pawns[i].classList.remove("clicked");
                 }
-            // let n = Pawns[i].classList.length;
+            pawn.classList.add("clicked");
         }
-        pawn.classList.add("clicked");
-        console.dir(pawn);
-    };
-});
-Pawns.forEach(function (pawn) {
-    pawn.addEventListener("click", function (ev) {
-        console.dir(ev);
-        document.addEventListener("keyup", function (event) {
-            for (var i = 0; i < Pawns.length; i++) {
-                for (var n = 0; n < Pawns[i].classList.length; n++)
-                    if (Pawns[i].classList[n] === "clicked") {
-                        // debugger;
-                        switch (event.key) {
-                            case "ArrowUp":
-                                Pawns[i].style.top = pawn.offsetTop - 112.5 + "px";
-                                break;
-                            case "ArrowDown":
-                                Pawns[i].style.top = pawn.offsetTop + 112.5 + "px";
-                                break;
-                            case "ArrowLeft":
-                                Pawns[i].style.left = pawn.offsetLeft - 112.5 + "px";
-                                break;
-                            case "ArrowRight":
-                                Pawns[i].style.left = pawn.offsetLeft + 112.5 + "px";
-                                break;
-                        }
-                    }
-            }
-        });
-    });
-    // debugger;
-});
+        return pawn;
+    }
+    catch (error) {
+        console.error(error);
+        return;
+    }
+}
 // debugger;
 // Pawns.forEach((pawn: HTMLDivElement) => {
 //   pawn.onclick = (ev) => {
