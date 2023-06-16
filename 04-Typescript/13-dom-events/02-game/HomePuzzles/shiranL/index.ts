ca7776da0a0a69667399accbb6ce1c697ac1f833
shiranlasry <128288093+shiranlasry@users.noreply.github.com>
2023-06-16 15:11:11 +0300

    shiran

--- 04-Typescript/13-dom-events/02-game/HomePuzzles/shiranL/index.ts
+++ 04-Typescript/13-dom-events/02-game/HomePuzzles/shiranL/index.ts
@@ -7,22 +7,17 @@ class clown {
     
         try {
             if (!clowns) throw new Error("missing root element")
-            const html: string = `<div  onclick="move(this.id)" class='card' id="${this.id}" ><img src="${this.imgUrl}"></div>`;
+            const html: string = `<div  onclick="getClickedID(this.id as id)" class='card' id="${this.id}" ><img src="${this.imgUrl}"></div>`;
             clowns.innerHTML += html;
         } catch (error) {
             console.error(error)
         }
     }
+    
 }
-function move(clicked_id :string)
-{ debugger;
-    const moveclown: HTMLElement | null = document.querySelector(`#${clicked_id}`);
-    document.addEventListener('keyup', (event: KeyboardEvent) => {
-        //if arrow up go up. if arrow down go down...
-        console.log(event);
-   
-        
-    });
+let clickedID =function getClickedID(id:string):string|null{
+    debugger;
+    return id;
 }
 //get root of clowns class
 const clownsHTML= document.querySelector('.clowns') as HTMLElement;
@@ -33,4 +28,45 @@ let newclown =new clown("shiran","./p1.png","A1"); newclown.renderclown(clownsHT
 newclown =new clown("ohad","./p2.png","A2"); newclown.renderclown(clownsHTML); clowns.push(newclown);
 newclown =new clown("Yarden","./p3.png","A3"); newclown.renderclown(clownsHTML); clowns.push(newclown); 
 newclown =new clown("Yarden","./p3.png","A4"); newclown.renderclown(clownsHTML); clowns.push(newclown);
-document.onkeydown = detectKey;
+
+// document.addEventListener('keyup', (event: KeyboardEvent) => {
+//     //if arrow up go up. if arrow down go down...
+  
+//     console.log(event);
+//     if(clickedID!=null)
+//     {
+//         const selectedclownsHTML= document.querySelector(`#${clickedID}`) as HTMLElement;
+
+//     }
+//     // switch (event.key) {
+        
+//     //     case 'ArrowUp':
+
+//     //         box.style.top = `${box.offsetTop - 10}px`;
+//     //         break;
+//     //     case 'ArrowDown':
+//     //         box.style.top = `${box.offsetTop + 10}px`;
+//     //         break;
+//     //     case 'ArrowLeft':
+//     //         box.style.left = `${box.offsetLeft - 10}px`;
+//     //         break;
+//     //     case 'ArrowRight':
+
+//     //         box.style.left = `${box.offsetLeft + 10}px`;
+//     //         break;
+//     //     case " ":
+//     //         const urlMonster = 'url("./dist/packman-monster.png")'
+//     //         const urlPackman = 'url("./dist/packman.png")'
+
+//     //         if (box.style.backgroundImage === urlMonster) {
+//     //             box.style.backgroundImage = urlPackman;
+//     //         } else {
+//     //             box.style.backgroundImage = urlMonster;
+//     //         }
+
+//     //         break;
+
+
+
+//     // }
+// });
