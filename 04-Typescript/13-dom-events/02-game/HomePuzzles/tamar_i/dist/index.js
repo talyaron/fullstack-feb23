9cb7e1b2478bd805086c106583d2002003c5c331
itahTamar <tamar.verber@gmail.com>
2023-06-17 19:08:07 +0300

    hw13.game-fallowup class exampel

--- 04-Typescript/13-dom-events/02-game/HomePuzzles/tamar_i/dist/index.js
+++ 04-Typescript/13-dom-events/02-game/HomePuzzles/tamar_i/dist/index.js
@@ -2,27 +2,59 @@
 //css- put a bord pic 
 //css- put on the bord a pawn pic
 //catch the pawn using queryselector
+//catch the pawn using mousedown
+//chack if the pawn move, if not - move it, if yes - release it by mouseup
 var pawn = document.querySelector("#pawn");
+document.addEventListener('keyup', function (event) {
+    console.log(event);
+    switch (event.key) {
+        case 'ArrowUp':
+            pawn.style.top = pawn.offsetTop - 90 + "px";
+            break;
+        case 'ArrowDown':
+            pawn.style.top = pawn.offsetTop + 90 + "px";
+            break;
+        case 'ArrowLeft':
+            pawn.style.left = pawn.offsetLeft - 90 + "px";
+            break;
+        case 'ArrowRight':
+            pawn.style.left = pawn.offsetLeft + 90 + "px";
+            break;
+    }
+});
+// let isMoved: boolean = false;
 //make function that mouse catch the pawn (initial click)
-if (pawn) {
-    document.addEventListener("click", initiaAtMouselClick);
-}
-function initiaAtMouselClick(ev) {
-    console.log(ev);
-    if ()
-        switch (ev.key) {
-            case 'ArrowUp':
-                pawn.style.top = pawn.offsetTop - 92 + "px";
-                break;
-            case 'ArrowDown':
-                pawn.style.top = pawn.offsetTop + 92 + "px";
-                break;
-            case 'ArrowLeft':
-                pawn.style.left = pawn.offsetLeft - 93 + "px";
-                break;
-            case 'ArrowRight':
-                pawn.style.left = pawn.offsetLeft + 93 + "px";
-                break;
-        }
-}
-//make function that nove the pawn using keybourd
+// if (pawn) {
+//     pawn.addEventListener("click", moveByKeybourd); 
+// }
+// //bild the chatch and release by mouse function
+// function isClicked (pawn: any){
+//     console.log(pawn); 
+//     if (isMoved){
+//         pawn.removeEventListener("click", moveByKeybourd);
+//         isMoved = !isMoved;
+//         return;
+//       }
+//       isMoved = !isMoved;
+//       pawn = this;
+//       pawn.addEventListener("click", moveByKeybourd, false);    
+// }
+//bild the moveByKeybourd function 
+// function moveByKeybourd(ev: any) {
+//     console.log(ev);
+//     document.addEventListener('keyup', ev: KeyboardEvent) => {
+//         switch (ev.key) {
+//             case 'ArrowUp':
+//                 pawn.style.top = `${pawn.offsetTop - 92}px`;
+//                 break;
+//             case 'ArrowDown':
+//                 pawn.style.top = `${pawn.offsetTop + 92}px`;
+//                 break;
+//             case 'ArrowLeft':
+//                 pawn.style.left = `${pawn.offsetLeft - 93}px`;
+//                 break;
+//             case 'ArrowRight':
+//                 pawn.style.left = `${pawn.offsetLeft + 93}px`;
+//                 break;
+//         }
+// }}
--- 04-Typescript/13-dom-events/02-game/HomePuzzles/tamar_i/index.ts
+++ 04-Typescript/13-dom-events/02-game/HomePuzzles/tamar_i/index.ts
@@ -2,34 +2,71 @@
 //css- put a bord pic 
 //css- put on the bord a pawn pic
 //catch the pawn using queryselector
+//catch the pawn using mousedown
+//chack if the pawn move, if not - move it, if yes - release it by mouseup
 
 const pawn = document.querySelector("#pawn") as HTMLDivElement;
 
+document.addEventListener('keyup', (event: KeyboardEvent)=>{
+    console.log(event);
+
+    switch (event.key){
+        case 'ArrowUp':
+            pawn.style.top = `${pawn.offsetTop - 90}px`;
+            break;
+        case 'ArrowDown':
+                pawn.style.top = `${pawn.offsetTop + 90}px`;
+                break;
+        case 'ArrowLeft':
+                pawn.style.left = `${pawn.offsetLeft - 90}px`;
+                break;
+        case 'ArrowRight':
+                pawn.style.left = `${pawn.offsetLeft + 90}px`;
+                break;
+    }
+})
+
+// let isMoved: boolean = false;
+
 //make function that mouse catch the pawn (initial click)
-if(pawn){
-document.addEventListener("click",initiaAtMouselClick);
-}
-function initiaAtMouselClick(ev: any){
-    console.log(ev);
-    if()
-switch (ev.key) {
-    case 'ArrowUp':
-        pawn.style.top = `${pawn.offsetTop - 92}px`;
-        break;
-    case 'ArrowDown':
-        pawn.style.top = `${pawn.offsetTop + 92}px`;
-        break;
-    case 'ArrowLeft':
-        pawn.style.left = `${pawn.offsetLeft - 93}px`;
-        break;
-    case 'ArrowRight':
-        pawn.style.left = `${pawn.offsetLeft + 93}px`;
-        break;
-}}
+// if (pawn) {
+//     pawn.addEventListener("click", moveByKeybourd); 
 
+// }
 
+// //bild the chatch and release by mouse function
 
+// function isClicked (pawn: any){
+//     console.log(pawn); 
+//     if (isMoved){
+//         pawn.removeEventListener("click", moveByKeybourd);
+//         isMoved = !isMoved;
+//         return;
+//       }
+      
+//       isMoved = !isMoved;
+//       pawn = this;
+    
+//       pawn.addEventListener("click", moveByKeybourd, false);    
+// }
 
-//make function that nove the pawn using keybourd
+//bild the moveByKeybourd function 
 
-
+// function moveByKeybourd(ev: any) {
+//     console.log(ev);
+//     document.addEventListener('keyup', ev: KeyboardEvent) => {
+//         switch (ev.key) {
+//             case 'ArrowUp':
+//                 pawn.style.top = `${pawn.offsetTop - 92}px`;
+//                 break;
+//             case 'ArrowDown':
+//                 pawn.style.top = `${pawn.offsetTop + 92}px`;
+//                 break;
+//             case 'ArrowLeft':
+//                 pawn.style.left = `${pawn.offsetLeft - 93}px`;
+//                 break;
+//             case 'ArrowRight':
+//                 pawn.style.left = `${pawn.offsetLeft + 93}px`;
+//                 break;
+//         }
+// }}
