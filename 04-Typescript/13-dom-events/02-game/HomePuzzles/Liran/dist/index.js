f93c6766611904991afe1fa924e5d1fe542ad4a3
liran26 <liranav26@gmail.com>
2023-06-17 21:12:19 +0300

    .

--- 04-Typescript/13-dom-events/02-game/HomePuzzles/Liran/dist/index.js
+++ 04-Typescript/13-dom-events/02-game/HomePuzzles/Liran/dist/index.js
@@ -23,12 +23,13 @@ function renderBord() {
 function renderWhitePlayer() {
     try {
         console.log("renderWhitePlayer");
+        var pawnId = 0;
         // const boxId = document.querySelector(`#box${Math.floor(Math.random()*64)}`);
         var wrapper = document.querySelector("#wrapper");
         if (!wrapper)
             throw new Error("Error in wrapper");
         for (var i = 0; i < numOfWP; i++) {
-            wrapper.innerHTML += "<div class=\"img white\"></div>";
+            wrapper.innerHTML += "<div id=\"white" + pawnId++ + "\" class=\"img white\"></div>";
         }
     }
     catch (error) {
@@ -38,22 +39,36 @@ function renderWhitePlayer() {
 function renderBlackPlayer() {
     try {
         console.log("renderBlackPlayer");
+        var pawnId = 0;
         // const boxId = document.querySelector(`#box${Math.floor(Math.random()*64)}`);
         var wrapper = document.querySelector("#wrapper");
         if (!wrapper)
             throw new Error("Error in wrapper");
         for (var i = 0; i < numOfWP; i++) {
-            wrapper.innerHTML += "<div class=\"img black\"></div>";
+            wrapper.innerHTML += "<div id=\"black" + pawnId++ + "\" class=\"img black\"></div>";
         }
     }
     catch (error) {
         console.error(error);
     }
 }
-// function getPositions()
 var Pawn = /** @class */ (function () {
-    function Pawn() {
+    function Pawn(x, y) {
+        this.x = x;
+        this.y = y;
     }
+    Pawn.prototype.setX = function (num) {
+        this.x = num;
+    };
+    Pawn.prototype.sety = function (num) {
+        this.y = num;
+    };
+    Pawn.prototype.getX = function () {
+        return this.x;
+    };
+    Pawn.prototype.gety = function () {
+        return this.y;
+    };
     return Pawn;
 }());
 ;
@@ -64,50 +79,139 @@ renderWhitePlayer();
 renderBlackPlayer();
 var topIndex = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2];
 var leftIndex = [0, 2, 4, 6, 1, 3, 5, 7, 0, 2, 4, 6];
-var blackPosition = new Array(numOfWP);
-var whitePosition = new Array(numOfWP);
+var blackPosition = new Array();
+var whitePosition = new Array();
 var index = 0;
 var blackPlayers = document.querySelectorAll(".black");
 blackPlayers.forEach(function (player) {
     player.style.top = (topIndex[index] * 80) + 10 + "px";
     player.style.left = (leftIndex[index++] * 80) + 10 + "px";
+    setPosition(player, "black");
 });
 index = 0;
 var whitePlayers = document.querySelectorAll(".white");
 whitePlayers.forEach(function (player) {
     player.style.top = ((topIndex[index] + 5) * 80) + 10 + "px";
     player.style.left = ((7 - leftIndex[index++]) * 80) + 10 + "px";
+    setPosition(player, "white");
 });
-var whitePawns = document.querySelectorAll(".img");
+function setPosition(player, team) {
+    try {
+        if (!player)
+            throw new Error("Missing player");
+        if (team == "white") {
+            whitePosition.push(new Pawn(player.offsetTop, player.offsetLeft));
+        }
+        else {
+            blackPosition.push(new Pawn(player.offsetTop, player.offsetLeft));
+        }
+        return true;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+function meetPlyer(playerTop, playerLeft, secondPlayer) {
+    try {
+        if (!playerTop || !playerLeft || !secondPlayer)
+            throw new Error("Missing Player");
+        if (playerTop === secondPlayer.gety() && playerLeft === secondPlayer.getX())
+            return true;
+        return false;
+    }
+    catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+function canEat(playerTop, playerLeft, secondPlayer) {
+    try {
+        if (!playerTop || !playerLeft || !secondPlayer)
+            throw new Error("Missing Player");
+        if (playerTop === secondPlayer.gety() && playerLeft === secondPlayer.getX())
+            return false;
+        return true;
+    }
+    catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+var Players = document.querySelectorAll(".img");
 var activePawn = null;
-whitePawns.forEach(function (whitePawn) {
-    whitePawn.addEventListener("click", function () {
-        activePawn = whitePawn;
+Players.forEach(function (player) {
+    player.addEventListener("click", function () {
+        activePawn = player;
+        debugger;
     });
     document.addEventListener("keydown", function (ev) {
         try {
-            if (activePawn === whitePawn) {
+            if (activePawn === player) {
+                var step_1 = 80;
+                var nextLocatiomn_1;
+                var eat_1 = false, meet_1 = false;
                 console.log(ev.key);
                 switch (ev.key) {
                     case 'ArrowUp':
-                        if ((whitePawn.offsetTop - 80) < 0)
+                        nextLocatiomn_1 = (player.offsetTop - step_1);
+                        if (nextLocatiomn_1 < 0)
                             throw new Error("Out of Bord");
-                        whitePawn.style.top = whitePawn.offsetTop - 80 + "px";
+                        // debugger;
+                        (player.classList[1] === "white" ? whitePosition : blackPosition).forEach(function (pawn) {
+                            meet_1 = meetPlyer(nextLocatiomn_1, player.offsetLeft, pawn);
+                            eat_1 = canEat(nextLocatiomn_1 + step_1 * 2, player.offsetLeft, pawn);
+                            if (meet_1) {
+                                if (eat_1)
+                                    nextLocatiomn_1 - step_1 * 2;
+                            }
+                        });
+                        player.style.top = nextLocatiomn_1 + "px";
                         break;
                     case 'ArrowDown':
-                        if ((whitePawn.offsetTop + 80) > Limit)
+                        nextLocatiomn_1 = (player.offsetTop + step_1);
+                        if (nextLocatiomn_1 > Limit)
                             throw new Error("Out of Bord");
-                        whitePawn.style.top = whitePawn.offsetTop + 80 + "px";
+                        (player.classList[1] === "white" ? whitePosition : blackPosition).forEach(function (pawn) {
+                            meet_1 = meetPlyer(nextLocatiomn_1, player.offsetLeft, pawn);
+                            eat_1 = canEat(nextLocatiomn_1 + 2 * step_1, player.offsetLeft, pawn);
+                            if (meet_1) {
+                                if (eat_1)
+                                    nextLocatiomn_1 + step_1 * 2;
+                            }
+                        });
+                        player.style.top = nextLocatiomn_1 + "px";
+                        // player.style.top = `${player.offsetTop + step}px`;
                         break;
                     case 'ArrowLeft':
-                        if ((whitePawn.offsetLeft - 80) < 0)
+                        nextLocatiomn_1 = (player.offsetLeft - step_1);
+                        if (nextLocatiomn_1 < 0)
                             throw new Error("Out of Bord");
-                        whitePawn.style.left = whitePawn.offsetLeft - 80 + "px";
+                        // player.style.left = `${player.offsetLeft - step}px`;
+                        (player.classList[1] === "white" ? whitePosition : blackPosition).forEach(function (pawn) {
+                            meet_1 = meetPlyer(player.offsetTop, nextLocatiomn_1, pawn);
+                            eat_1 = canEat(player.offsetTop, nextLocatiomn_1 + step_1 * 2, pawn);
+                            if (meet_1) {
+                                if (eat_1)
+                                    nextLocatiomn_1 - step_1 * 2;
+                            }
+                        });
+                        player.style.left = nextLocatiomn_1 + "px";
                         break;
                     case 'ArrowRight':
-                        if ((whitePawn.offsetLeft + 80) > Limit)
+                        nextLocatiomn_1 = (player.offsetLeft + step_1);
+                        if (nextLocatiomn_1 > Limit)
                             throw new Error("Out of Bord");
-                        whitePawn.style.left = whitePawn.offsetLeft + 80 + "px";
+                        (player.classList[1] === "white" ? whitePosition : blackPosition).forEach(function (pawn) {
+                            meet_1 = meetPlyer(player.offsetTop, player.offsetLeft, pawn);
+                            eat_1 = canEat(player.offsetTop, nextLocatiomn_1 + step_1 * 2, pawn);
+                            debugger;
+                            if (meet_1) {
+                                if (eat_1)
+                                    nextLocatiomn_1 + step_1 * 2;
+                            }
+                        });
+                        player.style.left = nextLocatiomn_1 + "px";
+                        // plyer.style.left = `${plyer.offsetLeft + step}px`;
                         break;
                 }
             }
--- 04-Typescript/13-dom-events/02-game/HomePuzzles/Liran/index.ts
+++ 04-Typescript/13-dom-events/02-game/HomePuzzles/Liran/index.ts
@@ -1,9 +1,8 @@
-function reloadPage()
-{
+function reloadPage() {
     location.reload();
 }
 const restart = document.querySelector("#restart");
-restart?.addEventListener("click",()=>{
+restart?.addEventListener("click", () => {
     reloadPage();
 })
 
@@ -33,14 +32,17 @@ function renderBord() {
 
 }
 
+
 function renderWhitePlayer() {
     try {
         console.log(`renderWhitePlayer`)
+        let pawnId = 0;
+
         // const boxId = document.querySelector(`#box${Math.floor(Math.random()*64)}`);
         const wrapper = document.querySelector("#wrapper");
         if (!wrapper) throw new Error("Error in wrapper")
         for (let i = 0; i < numOfWP; i++) {
-            wrapper.innerHTML += `<div class="img white"></div>`
+            wrapper.innerHTML += `<div id="white${pawnId++}" class="img white"></div>`
         }
     } catch (error) {
         console.error(error);
@@ -50,22 +52,37 @@ function renderWhitePlayer() {
 function renderBlackPlayer() {
     try {
         console.log(`renderBlackPlayer`)
+        let pawnId = 0;
         // const boxId = document.querySelector(`#box${Math.floor(Math.random()*64)}`);
         const wrapper = document.querySelector("#wrapper");
         if (!wrapper) throw new Error("Error in wrapper")
         for (let i = 0; i < numOfWP; i++) {
-            wrapper.innerHTML += `<div class="img black"></div>`
+            wrapper.innerHTML += `<div id="black${pawnId++}" class="img black"></div>`
         }
     } catch (error) {
         console.error(error);
     }
 }
 
-// function getPositions()
 
-class Pawn{
-    x:number;
-    y:number;
+class Pawn {
+    constructor(public x: number, public y: number) { }
+
+    setX(num: number) {
+        this.x = num;
+    }
+
+    sety(num: number) {
+        this.y = num;
+    }
+
+    getX(): number {
+        return this.x;
+    }
+
+    gety(): number {
+        return this.y;
+    }
 };
 
 const numOfWP = 12;
@@ -74,51 +91,138 @@ renderBord();
 renderWhitePlayer();
 renderBlackPlayer();
 
-const topIndex = [0,0,0,0,1,1,1,1,2,2,2,2];
-const leftIndex = [0,2,4,6,1,3,5,7,0,2,4,6];
-const blackPosition:Pawn[] = new Array(numOfWP); 
-const whitePosition:Pawn[] = new Array(numOfWP); 
+const topIndex = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2];
+const leftIndex = [0, 2, 4, 6, 1, 3, 5, 7, 0, 2, 4, 6];
+const blackPosition: Pawn[] = new Array();
+const whitePosition: Pawn[] = new Array();
 
 let index = 0;
 const blackPlayers: NodeListOf<HTMLDivElement> = document.querySelectorAll(".black");
 blackPlayers.forEach(player => {
     player.style.top = `${(topIndex[index] * 80) + 10}px`
     player.style.left = `${(leftIndex[index++] * 80) + 10}px`
+    setPosition(player, "black")
 })
 
-index =0;
+index = 0;
 const whitePlayers: NodeListOf<HTMLDivElement> = document.querySelectorAll(".white");
 whitePlayers.forEach(player => {
-    player.style.top = `${((topIndex[index] + 5)* 80) + 10}px`
-    player.style.left = `${((7-leftIndex[index++]) * 80) + 10}px`
+    player.style.top = `${((topIndex[index] + 5) * 80) + 10}px`
+    player.style.left = `${((7 - leftIndex[index++]) * 80) + 10}px`
+    setPosition(player, "white")
+
 })
 
-const whitePawns: NodeListOf<HTMLDivElement> = document.querySelectorAll(".img");
+function setPosition(player: HTMLDivElement, team: String): Boolean | undefined {
+    try {
+        if (!player) throw new Error("Missing player");
+        if (team == "white") {
+            whitePosition.push(new Pawn(player.offsetTop, player.offsetLeft))
+        }
+        else {
+            blackPosition.push(new Pawn(player.offsetTop, player.offsetLeft))
+        }
+        return true;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function meetPlyer(playerTop: number, playerLeft: number, secondPlayer: Pawn): boolean {
+    try {
+        if (!playerTop || !playerLeft || !secondPlayer) throw new Error("Missing Player");
+        if (playerTop === secondPlayer.gety() && playerLeft === secondPlayer.getX())
+            return true;
+        return false;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+function canEat(playerTop: number, playerLeft: number, secondPlayer: Pawn): boolean {
+    try {
+        if (!playerTop || !playerLeft || !secondPlayer) throw new Error("Missing Player");
+        if (playerTop === secondPlayer.gety() && playerLeft === secondPlayer.getX())
+            return false;
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+const Players: NodeListOf<HTMLDivElement> = document.querySelectorAll(".img");
 let activePawn: HTMLDivElement | null = null;
-whitePawns.forEach(whitePawn => {
-    whitePawn.addEventListener("click", () => {
-        activePawn = whitePawn;
+Players.forEach(player => {
+    player.addEventListener("click", () => {
+        activePawn = player;
+        debugger;
     })
     document.addEventListener("keydown", (ev: any) => {
         try {
-            if (activePawn === whitePawn) {
+            if (activePawn === player) {
+                let step = 80;
+                let nextLocatiomn;
+                let eat = false, meet = false;
                 console.log(ev.key)
                 switch (ev.key) {
                     case 'ArrowUp':
-                        if((whitePawn.offsetTop - 80)<0)throw new Error("Out of Bord")
-                        whitePawn.style.top = `${whitePawn.offsetTop - 80}px`;
+                        nextLocatiomn = (player.offsetTop - step);
+                        if (nextLocatiomn < 0) throw new Error("Out of Bord");
+                        // debugger;
+                        (player.classList[1] === "white" ? whitePosition : blackPosition).forEach(pawn => {
+                            meet = meetPlyer(nextLocatiomn, player.offsetLeft, pawn);
+                            eat = canEat(nextLocatiomn+step*2, player.offsetLeft, pawn);
+                            if (meet) {
+                                if (eat)
+                                    nextLocatiomn - step * 2;
+                            }
+                        })                        
+                        player.style.top = `${nextLocatiomn}px`;
                         break;
                     case 'ArrowDown':
-                        if((whitePawn.offsetTop + 80)>Limit)throw new Error("Out of Bord")
-                        whitePawn.style.top = `${whitePawn.offsetTop + 80}px`;
+                        nextLocatiomn = (player.offsetTop + step);
+                        if (nextLocatiomn > Limit) throw new Error("Out of Bord");
+                        (player.classList[1] === "white" ? whitePosition : blackPosition).forEach(pawn => {
+                            meet = meetPlyer(nextLocatiomn, player.offsetLeft, pawn);
+                            eat = canEat(nextLocatiomn + 2*step, player.offsetLeft, pawn);
+                            if (meet) {
+                                if (eat)
+                                    nextLocatiomn + step * 2;
+                            }
+                        })
+                        player.style.top = `${nextLocatiomn}px`;
+                        // player.style.top = `${player.offsetTop + step}px`;
                         break;
                     case 'ArrowLeft':
-                        if((whitePawn.offsetLeft - 80)<0)throw new Error("Out of Bord")
-                        whitePawn.style.left = `${whitePawn.offsetLeft - 80}px`;
+                        nextLocatiomn = (player.offsetLeft - step);
+                        if (nextLocatiomn < 0) throw new Error("Out of Bord");
+                        // player.style.left = `${player.offsetLeft - step}px`;
+                        (player.classList[1] === "white" ? whitePosition : blackPosition).forEach(pawn => {
+                            meet = meetPlyer(player.offsetTop, nextLocatiomn, pawn);
+                            eat = canEat(player.offsetTop, nextLocatiomn + step * 2, pawn);
+                            if (meet) {
+                                if (eat)
+                                    nextLocatiomn - step * 2;
+                            }
+                        })
+                        player.style.left = `${nextLocatiomn}px`;
                         break;
                     case 'ArrowRight':
-                        if((whitePawn.offsetLeft + 80)>Limit)throw new Error("Out of Bord")
-                        whitePawn.style.left = `${whitePawn.offsetLeft + 80}px`;
+                        nextLocatiomn = (player.offsetLeft + step);
+                        if (nextLocatiomn > Limit) throw new Error("Out of Bord");
+                        (player.classList[1] === "white" ? whitePosition : blackPosition).forEach(pawn => {
+                            meet = meetPlyer(player.offsetTop, player.offsetLeft, pawn);
+                            eat = canEat(player.offsetTop, nextLocatiomn + step * 2, pawn);
+                            debugger;
+                            if (meet) {
+                                if (eat)
+                                    nextLocatiomn + step * 2;
+                            }
+                        })
+                        player.style.left = `${nextLocatiomn}px`;
+                        // plyer.style.left = `${plyer.offsetLeft + step}px`;
                         break;
                 }
             }
