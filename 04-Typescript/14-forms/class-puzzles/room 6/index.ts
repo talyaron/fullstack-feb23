aa9d1a0d72a8a047305f14569830a47e3abc11eb
danieltovbin <111869103+danieltovbin@users.noreply.github.com>
2023-06-18 21:16:03 +0300

    .

---
+++ 04-Typescript/14-forms/class-puzzles/room 6/index.ts
@@ -0,0 +1,44 @@
+function handleInput(event) {
+    console.dir(event)
+    console.log(event.target.value);
+    const root = document.querySelector('#root');
+    if(root) {
+        root.innerHTML = event.target.value;
+    }
+}
+
+function handleSubmit(ev:any){
+    try {
+        ev.preventDefault();
+        console.dir(ev);
+        const username:string = ev.target.username.value;
+        const imageUrl:string = ev.target.imageUrl.value;
+        const yearOfBirth = ev.target.yearOfBirth.valueAsNumber;
+        const age:number = new Date().getFullYear() - yearOfBirth;
+        const color:number = ev.target.color.value;
+        const result = {username, imageUrl, age, color};
+        console.log(result)
+        const arrPlayers = [];
+        arrPlayers.push(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const root = document.querySelectorAll('.root') as NodeListOf<HTMLDivElement> ;
+
+class Player {
+    constructor(public username: string, public imageUrl: string, public age: number, public color: string) {
+    }
+
+renderPlyer(root: HTMLElement | null) {
+    try {
+        if (!root) throw new Error("missing root element")
+        const html: string = `<div class='card' onclick="handleHideCard('${this.id}')" id="${this.id}"><img src="${this.imgUrl}"><h4>${this.title}</h4></div>`;
+        root.innerHTML += html;
+    } catch (error) {
+        console.error(error)
+    }
+}
+}
+
