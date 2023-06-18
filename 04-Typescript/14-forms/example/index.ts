73a36d4fa63eed90e53d114383f1d90d4d383d87
Tal Yaron <tal.yaron@gmail.com>
2023-06-18 19:34:30 +0300

    calss

--- 04-Typescript/14-forms/example/index.ts
+++ 04-Typescript/14-forms/example/index.ts
@@ -6,3 +6,28 @@ function handleInput(event) {
         root.innerHTML = event.target.value;
     }
 }
+
+function handleSubmit(ev:any){
+    try {
+        ev.preventDefault();
+        console.dir(ev);
+       const username = ev.target.username.value;
+        const password = ev.target.password.value;
+        const date = new Date(ev.target.date.value);
+        const age = ev.target.age.valueAsNumber;
+        const result = {age, date, password, username};
+        console.log(result)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function handleColor(ev:any){
+    try {
+       
+    
+        document.body.style.backgroundColor = ev.target.value;  
+    } catch (error) {
+        console.error(error);
+    }
+}
