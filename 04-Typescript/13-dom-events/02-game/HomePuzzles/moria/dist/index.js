a20dbf387a31be40c185ade1d208a518d7356570
Tal Yaron <tal.yaron@gmail.com>
2023-06-18 18:11:56 +0300

    fixed

--- 04-Typescript/13-dom-events/02-game/HomePuzzles/moria/dist/index.js
+++ 04-Typescript/13-dom-events/02-game/HomePuzzles/moria/dist/index.js
@@ -1,9 +1,16 @@
-var player = document.querySelector('.images');
-var onClick = function (event) {
-    console.log(player = document.querySelector("#" + event.srcElement.id));
-};
-window.addEventListener('click', onClick);
+// var player = document.querySelector('.images') as HTMLDivElement;
+var player;
+var players = document.querySelectorAll('.player');
+players.forEach(function (pl) {
+    pl.addEventListener('click', function (ev) {
+        player = ev.target;
+        console.dir(player);
+    });
+});
 document.addEventListener('keyup', function (event) {
+    event.stopPropagation();
+    console.dir(player);
+    console.log(event.key);
     switch (event.key) {
         case 'ArrowUp':
             player.style.top = player.offsetTop - 45 + "px";
--- 04-Typescript/13-dom-events/02-game/HomePuzzles/moria/index.ts
+++ 04-Typescript/13-dom-events/02-game/HomePuzzles/moria/index.ts
@@ -1,18 +1,26 @@
 
-var player = document.querySelector('.images') as HTMLDivElement;
+// var player = document.querySelector('.images') as HTMLDivElement;
+
+let player;
 
 
-const onClick = (event) => {
-    console.log(player = document.querySelector(`#${event.srcElement.id}`)!);
 
-}
 
-window.addEventListener('click', onClick);
+const players = document.querySelectorAll('.player') as NodeListOf<HTMLDivElement>;
+
+players.forEach(pl => {
+    pl.addEventListener('click', (ev) => {
+        player = ev.target as HTMLDivElement;
+        console.dir(player)
+    });
+});
 
 
 
 document.addEventListener('keyup', (event: KeyboardEvent) => {
-
+    event.stopPropagation();
+    console.dir(player)
+    console.log(event.key)
     switch (event.key) {
         case 'ArrowUp':
             player.style.top = `${player.offsetTop - 45}px`;
