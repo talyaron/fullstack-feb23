5519a359df65a3ee2fa3d50c1cb3d669930215b1
tomsobia <tom162sobia@gmail.com>
2023-06-20 21:16:32 +0300

    new

---
+++ 04-Typescript/07-functions-pure-functions/Home puzzles/tom,hadar/index.ts
@@ -0,0 +1,56 @@
+// # Level 1
+
+// 1. Write a JavaScript function that returns the negative number of a number.
+//    Example:
+//    x = 32243;
+//    Expected Output: -32243;
+
+// 2. Write a function to convert Celsius to Fahrenheit
+
+// # Level 2
+
+// 1. Create a function that gets the gender of the user and height of the user.
+//    The function returns the height of the user relative to his gender. For example, the user's height is 170cm, and the gender is male.
+//    The average height of males is 174cm, and the function return -4
+
+// # Level 3
+
+// 1. Write a function to reverse a number. For example, the function gets 123 and returns 321
+
+// Find exercises for js functions and complete them.
+// https://www.w3resource.com/javascript-exercises/javascript-functions-exercises.php
+
+// משימה 1
+// const x:any = prompt("תביאאאא מספר!!!?")
+// function tom(a:number){
+//     return a*(-1)
+// }
+// console.log(tom(x))
+
+// משימה 1++
+
+// const x:any=prompt("give me a Celsius")
+// function tom (a:number){
+//     const fahrenheit = (x * 9/5) + 32;
+//     return fahrenheit 
+// }
+// console.log(tom(x))
+
+// // משימה 2
+// const ge:any=prompt("מה אתה????")
+// const he:any=prompt("?תביא תגובהה")
+// function tom(a:string,b:number):any{
+//     try {
+//         if(a!="man"&&a!="woman")throw new Error("what are you?")
+//         if(a==="man"){
+//             return (176-b)
+//         }
+//         if(a==="woman"){
+//             return (161-b)
+//         }
+//     } catch (error) {
+//         console.error(error)
+//     }
+// }
+// console.log( tom(ge,he))
+
