ed99b3b8befe4ef10d3b3680d3659175c02ac457
tomsobia <tom162sobia@gmail.com>
2023-06-20 20:04:55 +0300

    new

--- 04-Typescript/06-functions-error/followup/tomsobia/dist/iindex.js
+++ 04-Typescript/06-functions-error/followup/tomsobia/dist/iindex.js
@@ -1,13 +1,11 @@
-var x = prompt("give me number?");
-var y = prompt("give me a second number");
-function number(x, y) {
-    try {
-        if (isNaN(x) || isNaN(y))
-            throw new Error("not a number");
-        return (x + "," + y);
-    }
-    catch (error) {
-        console.error(error);
-    }
-}
-console.log(number(x, y));
+// const x:any= prompt("give me number?")
+// const y:any = prompt("give me a second number")
+// function number(x:number,y:number){
+//     try {
+//         if (isNaN(x)||isNaN(y))throw new Error("not a number")
+//         return (`${x},${y}`)
+//     } catch (error) {
+//         console.error(error)
+//     }
+// }
+// console.log(number(x,y))
--- 04-Typescript/06-functions-error/followup/tomsobia/iindex.ts
+++ 04-Typescript/06-functions-error/followup/tomsobia/iindex.ts
@@ -1,15 +1,15 @@
-const x:any= prompt("give me number?")
+// const x:any= prompt("give me number?")
 
-const y:any = prompt("give me a second number")
+// const y:any = prompt("give me a second number")
 
-function number(x:number,y:number){
-    try {
-        if (isNaN(x)||isNaN(y))throw new Error("not a number")
-        return (`${x},${y}`)
+// function number(x:number,y:number){
+//     try {
+//         if (isNaN(x)||isNaN(y))throw new Error("not a number")
+//         return (`${x},${y}`)
         
-    } catch (error) {
-        console.error(error)
-    }
+//     } catch (error) {
+//         console.error(error)
+//     }
 
-}
-console.log(number(x,y))
+// }
+// console.log(number(x,y))
