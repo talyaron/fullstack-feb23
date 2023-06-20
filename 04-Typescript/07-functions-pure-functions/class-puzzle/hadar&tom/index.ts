16cab0172607b41fb979dc5de959a248f22c48bd
tomsobia <tom162sobia@gmail.com>
2023-06-20 20:51:40 +0300

    new

--- 04-Typescript/07-functions-pure-functions/class-puzzle/hadar&tom/index.ts
+++ 04-Typescript/07-functions-pure-functions/class-puzzle/hadar&tom/index.ts
@@ -1,23 +1,54 @@
-const num1:any= prompt("give me a number")
-const sign:any= prompt("give a sign")
-const num2:any= prompt("give me a number")
 
-function tom(a:number, b:string, c:number): any{
+// hadar
 
-    try {
-        if(isNaN(a)|| isNaN(c)) throw new Error("input is not a number")
 
-        if(b!= "/" && b!= "*") throw new Error ("input is not good")
+// const num1:any= prompt("give me a number")
+// const sign:any= prompt("give a sign")
+// const num2:any= prompt("give me a number")
 
-        if( b=== "/"){
-            return a/c
-        } else{
-            return a*c
-        }
+// function tom(a:number, b:string, c:number): any{
 
-    } catch (error) {
-        console.error(error)
+//     try {
+//         if(isNaN(a)|| isNaN(c)) throw new Error("input is not a number")
+
+//         if(b!= "/" && b!= "*") throw new Error ("input is not good")
+
+//         if( b=== "/"){
+//             return a/c
+//         } else{
+//             return a*c
+//         }
+
+//     } catch (error) {
+//         console.error(error)
+//     }
+// }
+
+// console.log(tom(num1,sign,num2))
+
+
+
+
+tom
+
+const num1:any=prompt("give me number")
+const sign:any=prompt("give me sign")
+const num2:any=prompt("give me number two")
+
+function tom(a:number,b:string,c:number):any {
+try {
+    if(isNaN(a)||isNaN(c))throw new Error("NaN")
+    if(b!="/"&&b!="*")throw new Error("לך קיבינימט")
+    if(b==="/"){
+        return a/c
+    }else{
+        return a*c
+        
     }
+    
+    
+} catch (error) {
+    console.error(error)
 }
-
+}
 console.log(tom(num1,sign,num2))
