d58353b6c9ae1e857457f18df216b68b1d4cbce4
shiranlasry <128288093+shiranlasry@users.noreply.github.com>
2023-06-23 19:31:52 +0300

    shiranL

--- 04-Typescript/15-Project-desing/02-HomePuzzle/shiranL/dist/index.js
+++ 04-Typescript/15-Project-desing/02-HomePuzzle/shiranL/dist/index.js
@@ -43,9 +43,12 @@ var employeesTimeClock = [];
 var LogINDiv = document.querySelector("#LogIN");
 var AddNewWorkerDiv = document.querySelector("#AddNewWorker");
 var clockiDiv = document.querySelector("#clocki");
+var ShowDetailsbtns = document.querySelector("#ShowDetailsbtns");
+var ShowDetails = document.querySelector("#ShowDetails");
+var setionID = null;
 function renderEmployeeLogIN(rootElement) {
     try {
-        var html = " <form onsubmit=\"handleLogIN(event)\">\n        <label for=\"employeeID\">ID: </label>\n        <input type=\"text\" name=\"employeeID\" id='employeeID' placeholder=\"ENERT ID\" required>\n        <input type=\"submit\" value=\"ENTER\">\n    </form>";
+        var html = " <form class=\"LoginDiv\" onsubmit=\"handleLogIN(event)\">\n        <label>LogIn</label>\n        <input type=\"text\" name=\"employeeID\" id='employeeID' placeholder=\"ENERT ID\" required>\n        <input type=\"submit\" value=\"ENTER\">\n    </form>";
         if (!rootElement)
             throw new Error("No root element");
         rootElement.innerHTML = html;
@@ -54,9 +57,9 @@ function renderEmployeeLogIN(rootElement) {
         console.error(error);
     }
 }
-function renderClock(rootElement) {
+function RenderClock(rootElement) {
     try {
-        var html = "<form>\n        <button type=\"button\" onclick=\"saveDateTime('enter'," + setionID + ")\">Enter</button>\n        <button type=\"button\" onclick=\"saveDateTime('exit'," + setionID + ")\">Exit</button>\n      </form>";
+        var html = "<form>\n        <button type=\"button\" onclick=\"saveDateTime('enter')\">Enter</button>\n        <button type=\"button\" onclick=\"saveDateTime('exit')\">Exit</button>\n      </form>";
         if (!rootElement)
             throw new Error("No root element");
         rootElement.innerHTML = html;
@@ -64,16 +67,114 @@ function renderClock(rootElement) {
     catch (error) {
         console.error(error);
     }
+}
+function RenderBtnsDetails(rootElement) {
+    try {
+        var html = "<form>\n        <button type=\"button\" onclick=\"RenderIdForSerchEmployee()\">serach ID</button>\n        <button type=\"button\" onclick=\"ShowByDate()\">serach Date</button>\n        </form>";
+        if (!rootElement)
+            throw new Error("No root element");
+        rootElement.innerHTML = html;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+function RenderIdForSerchEmployee() {
+    try {
+        if (LogINDiv) {
+            LogINDiv.innerHTML = "";
+        } // Remove all HTML content
+        if (AddNewWorkerDiv) {
+            AddNewWorkerDiv.innerHTML = "";
+        }
+        var html = "<form  onsubmit=\"ShowByID(event)\">\n        <input type=\"number\" name=\"SearchID\" id='SearchID' placeholder=\"Search ID\" required>\n        <input type=\"submit\" value=\"Search\">\n        <button type=\"button\" onclick=\"GoHome()\">Back</button>\n    </form>";
+        if (!ShowDetailsbtns)
+            throw new Error("No root element");
+        ShowDetailsbtns.innerHTML = html;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+function GoHome() {
+    if (ShowDetailsbtns)
+        ShowDetailsbtns.innerHTML = "";
+    RenderHomePage();
+}
+function RenderHomePage() {
+    if (ShowDetailsbtns)
+        RenderBtnsDetails(ShowDetailsbtns);
+    if (AddNewWorkerDiv)
+        renderAddEmployee(AddNewWorkerDiv);
+    if (LogINDiv)
+        renderEmployeeLogIN(LogINDiv);
+    if (ShowDetails) {
+        ShowDetails.innerHTML = "";
+    } // Remove all HTML content
 }
 function renderAddEmployee(rootElement) {
     try {
-        var html = "<form onsubmit=\"handAddEmployee(event)\">\n        <label for=\"firstName\">First Name: </label>\n        <input type=\"text\" name=\"firstName\" id='firstName' placeholder=\"First Name:\" required>\n        <label for=\"lastName\">Last Name: </label>\n        <input type=\"text\" name=\"lastName\" id='lastName' placeholder=\"Last Name:\" required>\n        <label for=\"newId\">ID: </label>\n        <input type=\"number\" name=\"newId\" id='newId' placeholder=\"ID:\" required>\n        <input type=\"submit\" value=\"ADD\">\n    </form>";
+        var html = "<form class=\"AddNewDiv\" onsubmit=\"handAddEmployee(event)\">\n        <label>Add New Employee</label>\n        <input type=\"text\" name=\"firstName\" id='firstName' placeholder=\"First Name:\" required>\n        <input type=\"text\" name=\"lastName\" id='lastName' placeholder=\"Last Name:\" required>\n        <input type=\"number\" name=\"newId\" id='newId' placeholder=\"ID:\" required>\n        <input type=\"submit\" value=\"ADD\">\n    </form>";
         if (!rootElement)
             throw new Error("No root element");
         rootElement.innerHTML = html;
     }
     catch (error) {
         console.error(error);
+    }
+}
+function ShowByID(event) {
+    event.preventDefault();
+    var id = event.target.SearchID.valueAsNumber;
+    var employeeTimesClockByID = employeesTimeClock.filter(function (employeeTime) { return employeeTime.employee.employeeID === id; });
+    if (ShowDetails) {
+        ShowDetails.innerHTML = "";
+    }
+    // Generate HTML elements and append to ShowDetails element
+    var ul = document.createElement("ul");
+    // Add full name as a title
+    if (employeeTimesClockByID.length > 0) {
+        var fullName = employeeTimesClockByID[0].employee.firsName + " " + employeeTimesClockByID[0].employee.lastName;
+        var fullNameElement = document.createElement("h2");
+        fullNameElement.textContent = "" + fullName;
+        ul.appendChild(fullNameElement);
+        // Create a table for displaying details
+        var table = document.createElement("table");
+        table.innerHTML = "\n        <tr>\n          <th>Date</th>\n          <th>Enter Time</th>\n          <th>Exit Time</th>\n          <th>Hours per Day</th>\n        </tr>\n      ";
+        // Iterate over the employee's time clock entries
+        for (var _i = 0, employeeTimesClockByID_1 = employeeTimesClockByID; _i < employeeTimesClockByID_1.length; _i++) {
+            var employeeTime = employeeTimesClockByID_1[_i];
+            var enterTime = employeeTime.enterTime, exitTime = employeeTime.exitTime;
+            var row = document.createElement("tr");
+            // Date
+            var dateCell = document.createElement("td");
+            dateCell.textContent = enterTime.toDateString();
+            row.appendChild(dateCell);
+            // Enter Time
+            var enterTimeCell = document.createElement("td");
+            enterTimeCell.textContent = enterTime.toLocaleTimeString();
+            row.appendChild(enterTimeCell);
+            // Exit Time
+            var exitTimeCell = document.createElement("td");
+            exitTimeCell.textContent = exitTime ? exitTime.toLocaleTimeString() : "-";
+            row.appendChild(exitTimeCell);
+            // Hours per Day
+            var hoursPerDayCell = document.createElement("td");
+            hoursPerDayCell.textContent = exitTime
+                ? employeeTime.calkWorkTime() + " hours"
+                : "-";
+            row.appendChild(hoursPerDayCell);
+            table.appendChild(row);
+        }
+        ul.appendChild(table);
+    }
+    else {
+        var errorMessage = document.createElement("p");
+        errorMessage.textContent = "No records found for the given employee ID.";
+        ul.appendChild(errorMessage);
+    }
+    if (ShowDetails) {
+        ShowDetails.appendChild(ul);
     }
 }
 function handAddEmployee(event) {
@@ -92,84 +193,95 @@ function handAddEmployee(event) {
         else {
             employees.push(newEmployee);
             alert('Add Employee sucsess!');
-            console.log(employees);
         }
         if (AddNewWorkerDiv) {
-            AddNewWorkerDiv.innerHTML = ""; // Remove all HTML content
-        }
-        renderEmployeeLogIN(LogINDiv);
-        renderAddEmployee(AddNewWorkerDiv);
+            AddNewWorkerDiv.innerHTML = "";
+        } // Remove all HTML content
+        RenderHomePage();
     }
     catch (error) {
         console.error(error);
     }
 }
-var setionID;
 function handleLogIN(event) {
-    event.preventDefault();
-    setionID = event.target.employeeID.value;
-    var employeeLG = employees.find(function (employee) {
-        return Number(employee.employeeID) === Number(setionID);
-    });
-    if (!employeeLG) {
-        throw new Error("Employee NOt Exists");
+    try {
+        event.preventDefault();
+        setionID = event.target.employeeID.value;
+        var employeeLG = employees.find(function (employee) {
+            return Number(employee.employeeID) === Number(setionID);
+        });
+        if (!employeeLG) {
+            alert('Employee NOt Exists');
+            throw new Error("Employee NOt Exists");
+        }
+        if (LogINDiv) {
+            LogINDiv.innerHTML = ""; // Remove all HTML content
+        }
+        if (AddNewWorkerDiv) {
+            AddNewWorkerDiv.innerHTML = ""; // Remove all HTML content
+        }
+        if (ShowDetailsbtns) {
+            ShowDetailsbtns.innerHTML = "";
+        }
+        RenderClock(clockiDiv);
     }
-    renderClock(clockiDiv);
-    if (LogINDiv) {
-        LogINDiv.innerHTML = ""; // Remove all HTML content
-    }
-    if (AddNewWorkerDiv) {
-        AddNewWorkerDiv.innerHTML = ""; // Remove all HTML content
+    catch (error) {
+        console.error(error);
     }
 }
-function saveDateTime(action, id) {
-    var currentDateTime = new Date();
-    var dateTime = currentDateTime.toLocaleString();
-    if (action === 'enter') {
-        var time = localStorage.setItem('enterDateTime', dateTime);
-        if (setionID) {
-            var emploeeybyID = employees.find(function (emploeey) { return emploeey.employeeID === id; });
-            if (!emploeeybyID)
-                throw new Error("cant find Employee with id " + id);
-            var checkStatus = employeesTimeClock.find(function (employeeTime) { return employeeTime.employee.employeeID === id &&
-                employeeTime.status == true; });
-            if (checkStatus)
-                alert('Need To Exit First ');
-            else {
-                if (!emploeeybyID)
-                    throw new Error("cant find Employee with id " + id);
-                var newtimeclock = new TimeClock(emploeeybyID, true, currentDateTime, null);
-                employeesTimeClock.push(newtimeclock);
-                alert('Enter date and time saved: ' + dateTime);
-                setionID = null;
+function saveDateTime(action) {
+    try {
+        var currentDateTime = new Date();
+        var dateTime = currentDateTime.toLocaleString();
+        if (action === 'enter') {
+            var time = localStorage.setItem('enterDateTime', dateTime);
+            if (setionID) {
+                //get employee
+                var emploeeybyID = employees.find(function (emploeey) { return emploeey.employeeID === Number(setionID); });
+                if (!emploeeybyID) {
+                    throw new Error("cant find Employee with id " + setionID);
+                }
+                // check if employee alardy sing enter => if he have line in the array with statuse true
+                var checkStatus = employeesTimeClock.find(function (employeeTime) { return employeeTime.employee.employeeID
+                    === Number(setionID) && employeeTime.status == true; });
+                if (checkStatus)
+                    alert('Need To Exit First ');
+                else {
+                    // if not - push new line to array
+                    var newtimeclock = new TimeClock(emploeeybyID, true, currentDateTime, null);
+                    employeesTimeClock.push(newtimeclock);
+                    alert('Enter date and time saved: ' + dateTime);
+                    // reset sestion
+                    setionID = null;
+                }
+                if (clockiDiv) {
+                    clockiDiv.innerHTML = "";
+                } // Remove all HTML content for enter and exit btns
+                RenderHomePage();
+            }
+        }
+        else if (action === 'exit') {
+            // find the line of the employee with status true
+            var employeeIndex = employeesTimeClock.findIndex(function (employeeTime) {
+                return employeeTime.employee.employeeID === Number(setionID) && employeeTime.status == true;
+            });
+            if (employeeIndex == -1) {
+                alert('Enter Time Not Declare:');
+            } //if there is no index=> the emlpoyee not sing enter
+            else // update exit time and status
+             {
+                employeesTimeClock[employeeIndex].exitTime = currentDateTime;
+                employeesTimeClock[employeeIndex].status = false;
+                alert('Exit date and time saved: ' + dateTime);
             }
             if (clockiDiv) {
-                clockiDiv.innerHTML = ""; // Remove all HTML content
-            }
-            renderEmployeeLogIN(LogINDiv);
-            renderAddEmployee(AddNewWorkerDiv);
+                clockiDiv.innerHTML = "";
+            } // Remove all HTML content
+            RenderHomePage();
         }
     }
-    else if (action === 'exit') {
-        console.log(employeesTimeClock);
-        var employeeIndex = employeesTimeClock.findIndex(function (employeeTime) {
-            return employeeTime.employee.employeeID === Number(id) && employeeTime.status == true;
-        });
-        if (employeeIndex == -1) {
-            alert('Enter  time not declare:');
-        }
-        else {
-            employeesTimeClock[employeeIndex].exitTime = currentDateTime;
-            employeesTimeClock[employeeIndex].status = false;
-            console.log(employeesTimeClock);
-            alert('Exit date and time saved: ' + dateTime);
-        }
-        if (clockiDiv) {
-            clockiDiv.innerHTML = ""; // Remove all HTML content
-        }
-        renderEmployeeLogIN(LogINDiv);
-        renderAddEmployee(AddNewWorkerDiv);
+    catch (error) {
+        console.error(error);
     }
 }
-renderEmployeeLogIN(LogINDiv);
-renderAddEmployee(AddNewWorkerDiv);
+RenderHomePage();
--- 04-Typescript/15-Project-desing/02-HomePuzzle/shiranL/index.ts
+++ 04-Typescript/15-Project-desing/02-HomePuzzle/shiranL/index.ts
@@ -42,17 +42,18 @@ const employeesTimeClock:TimeClock[]= [];
 const LogINDiv: HTMLElement | null = document.querySelector("#LogIN")
 const AddNewWorkerDiv: HTMLElement | null = document.querySelector("#AddNewWorker")
 const clockiDiv: HTMLElement | null = document.querySelector("#clocki")
+const ShowDetailsbtns: HTMLElement | null = document.querySelector("#ShowDetailsbtns")
+const ShowDetails: HTMLElement | null = document.querySelector("#ShowDetails")
+let setionID :number|null=null;
 
 function renderEmployeeLogIN(rootElement: HTMLElement | null) {
     try {
-        const html = ` <form onsubmit="handleLogIN(event)">
-        <label for="employeeID">ID: </label>
+        const html = ` <form class="LoginDiv" onsubmit="handleLogIN(event)">
+        <label>LogIn</label>
         <input type="text" name="employeeID" id='employeeID' placeholder="ENERT ID" required>
         <input type="submit" value="ENTER">
     </form>`
        
-
-
         if (!rootElement) throw new Error("No root element")
 
         rootElement.innerHTML = html;
@@ -60,11 +61,11 @@ function renderEmployeeLogIN(rootElement: HTMLElement | null) {
         console.error(error)
     }
 }
-function renderClock(rootElement: HTMLElement | null){
+function RenderClock(rootElement: HTMLElement | null){
     try {
         const html = `<form>
-        <button type="button" onclick="saveDateTime('enter',${setionID})">Enter</button>
-        <button type="button" onclick="saveDateTime('exit',${setionID})">Exit</button>
+        <button type="button" onclick="saveDateTime('enter')">Enter</button>
+        <button type="button" onclick="saveDateTime('exit')">Exit</button>
       </form>`
        
         if (!rootElement) throw new Error("No root element")
@@ -75,14 +76,59 @@ function renderClock(rootElement: HTMLElement | null){
     }
 
 }
-function renderAddEmployee(rootElement: HTMLElement | null){
+function RenderBtnsDetails(rootElement: HTMLElement | null){
     try {
-        const html = `<form onsubmit="handAddEmployee(event)">
-        <label for="firstName">First Name: </label>
+        const html = `<form>
+        <button type="button" onclick="RenderIdForSerchEmployee()">serach ID</button>
+        <button type="button" onclick="ShowByDate()">serach Date</button>
+        </form>`
+       
+        if (!rootElement) throw new Error("No root element")
+
+        rootElement.innerHTML = html;
+    } catch (error) {
+        console.error(error)
+    }
+
+}
+function RenderIdForSerchEmployee(){
+    try {
+        if (LogINDiv) {  LogINDiv.innerHTML = ""; }               // Remove all HTML content
+        if (AddNewWorkerDiv) {  AddNewWorkerDiv.innerHTML = ""; }
+    
+        const html = `<form  onsubmit="ShowByID(event)">
+        <input type="number" name="SearchID" id='SearchID' placeholder="Search ID" required>
+        <input type="submit" value="Search">
+        <button type="button" onclick="GoHome()">Back</button>
+    </form>`
+    if (!ShowDetailsbtns) throw new Error("No root element")
+
+    ShowDetailsbtns.innerHTML = html;
+
+        
+    } catch (error) {
+        console.error(error);}
+}
+
+function GoHome(){
+    if (ShowDetailsbtns) ShowDetailsbtns.innerHTML = ``;
+    RenderHomePage();
+}
+function RenderHomePage(){
+    if (ShowDetailsbtns) RenderBtnsDetails(ShowDetailsbtns);
+    if (AddNewWorkerDiv) renderAddEmployee(AddNewWorkerDiv);
+    if (LogINDiv) renderEmployeeLogIN(LogINDiv);
+    if (ShowDetails) {  ShowDetails.innerHTML = ""; }               // Remove all HTML content
+   
+}
+
+function renderAddEmployee(rootElement: HTMLElement | null){
+    
+    try {
+        const html = `<form class="AddNewDiv" onsubmit="handAddEmployee(event)">
+        <label>Add New Employee</label>
         <input type="text" name="firstName" id='firstName' placeholder="First Name:" required>
-        <label for="lastName">Last Name: </label>
         <input type="text" name="lastName" id='lastName' placeholder="Last Name:" required>
-        <label for="newId">ID: </label>
         <input type="number" name="newId" id='newId' placeholder="ID:" required>
         <input type="submit" value="ADD">
     </form>`
@@ -95,9 +141,82 @@ function renderAddEmployee(rootElement: HTMLElement | null){
     }
 
 }
+function ShowByID(event: any) {
+    event.preventDefault();
+    const id = event.target.SearchID.valueAsNumber;
+    const employeeTimesClockByID = employeesTimeClock.filter(
+      (employeeTime) => employeeTime.employee.employeeID === id
+    );
+    if (ShowDetails) {
+      ShowDetails.innerHTML = "";
+    }
+  
+    // Generate HTML elements and append to ShowDetails element
+    const ul = document.createElement("ul");
+  
+    // Add full name as a title
+    if (employeeTimesClockByID.length > 0) {
+      const fullName = `${employeeTimesClockByID[0].employee.firsName} ${employeeTimesClockByID[0].employee.lastName}`;
+      const fullNameElement = document.createElement("h2");
+      fullNameElement.textContent = `${fullName}`;
+      ul.appendChild(fullNameElement);
+  
+      // Create a table for displaying details
+      const table = document.createElement("table");
+      table.innerHTML = `
+        <tr>
+          <th>Date</th>
+          <th>Enter Time</th>
+          <th>Exit Time</th>
+          <th>Hours per Day</th>
+        </tr>
+      `;
+  
+      // Iterate over the employee's time clock entries
+      for (const employeeTime of employeeTimesClockByID) {
+        const { enterTime, exitTime } = employeeTime;
+        const row = document.createElement("tr");
+  
+        // Date
+        const dateCell = document.createElement("td");
+        dateCell.textContent = enterTime.toDateString();
+        row.appendChild(dateCell);
+  
+        // Enter Time
+        const enterTimeCell = document.createElement("td");
+        enterTimeCell.textContent = enterTime.toLocaleTimeString();
+        row.appendChild(enterTimeCell);
+  
+        // Exit Time
+        const exitTimeCell = document.createElement("td");
+        exitTimeCell.textContent = exitTime ? exitTime.toLocaleTimeString() : "-";
+        row.appendChild(exitTimeCell);
+  
+        // Hours per Day
+        const hoursPerDayCell = document.createElement("td");
+        hoursPerDayCell.textContent = exitTime
+          ? `${employeeTime.calkWorkTime()} hours`
+          : "-";
+        row.appendChild(hoursPerDayCell);
+  
+        table.appendChild(row);
+      }
+  
+      ul.appendChild(table);
+    } else {
+      const errorMessage = document.createElement("p");
+      errorMessage.textContent = "No records found for the given employee ID.";
+      ul.appendChild(errorMessage);
+    }
+  
+    if (ShowDetails) {
+      ShowDetails.appendChild(ul);
+    }
+  }
+  
 function handAddEmployee(event :any){
 try {
-    
+   
     event.preventDefault();
     const firstName= event.target.firstName.value;
     const lastName= event.target.lastName.value;
@@ -113,108 +232,98 @@ if (employeeExists) {
 } else {
     employees.push(newEmployee);
     alert('Add Employee sucsess!');
-    console.log(employees);
-    
 }
 if (AddNewWorkerDiv) {
-    AddNewWorkerDiv.innerHTML = ""; // Remove all HTML content
-  }
-  renderEmployeeLogIN(LogINDiv);
-  renderAddEmployee(AddNewWorkerDiv);
-
-    
+    AddNewWorkerDiv.innerHTML = "";  }// Remove all HTML content
+    RenderHomePage();
 } catch (error) {
     console.error(error);
-  
 }
-   
 }
-let setionID;
-function handleLogIN(event :any){
 
+function handleLogIN(event :any){
+    try {
     event.preventDefault();
     setionID= event.target.employeeID.value;
  
     const employeeLG :Employee|undefined= employees.find(function (employee) {
         return Number(employee.employeeID) === Number(setionID);  });
-    if (!employeeLG){throw new Error("Employee NOt Exists")}
-
-    renderClock(clockiDiv)
-
-
+    if (!employeeLG){
+        alert('Employee NOt Exists');
+        throw new Error("Employee NOt Exists")
+    }
     if (LogINDiv) {
         LogINDiv.innerHTML = ""; // Remove all HTML content
       }
       if (AddNewWorkerDiv) {
         AddNewWorkerDiv.innerHTML = ""; // Remove all HTML content
       }
+      if (ShowDetailsbtns) {   ShowDetailsbtns.innerHTML = "";   }
+      
+      RenderClock(clockiDiv)
+    } 
+    catch (error) {
+        console.error(error);
+    }
+    
 }
 
-function saveDateTime(action, id ) {
-    
+function saveDateTime(action) {
+    try {
     const currentDateTime = new Date();
     const dateTime = currentDateTime.toLocaleString();
 
     if (action === 'enter') {
-     
       const time= localStorage.setItem('enterDateTime', dateTime);
-      
       if(setionID){
       
-        const emploeeybyID= employees.find(emploeey=>emploeey.employeeID===id)
-        if(!emploeeybyID) throw new Error(`cant find Employee with id ${id}`)
+        //get employee
+        const emploeeybyID= employees.find(emploeey=>emploeey.employeeID===Number(setionID))
+        if(!emploeeybyID) {throw new Error(`cant find Employee with id ${setionID}`); }
+       
+        // check if employee alardy sing enter => if he have line in the array with statuse true
 
-        const checkStatus = employeesTimeClock.find(employeeTime=>employeeTime.employee.employeeID===id &&
-            employeeTime.status == true  )
+       const checkStatus = employeesTimeClock.find(employeeTime=>employeeTime.employee.employeeID
+            ===Number(setionID) && employeeTime.status == true  )
         if (checkStatus) alert('Need To Exit First ');
         else{
-            if(!emploeeybyID) throw new Error(`cant find Employee with id ${id}`)
-
+            // if not - push new line to array
             const newtimeclock = new TimeClock(emploeeybyID,true,currentDateTime,null);
             employeesTimeClock.push(newtimeclock);
             alert('Enter date and time saved: ' + dateTime);
+            // reset sestion
             setionID=null;
 
         }
-        if (clockiDiv) {
-            clockiDiv.innerHTML = ""; // Remove all HTML content
-          }
-          renderEmployeeLogIN(LogINDiv);
-          renderAddEmployee(AddNewWorkerDiv);
-
-       
+        if (clockiDiv) { clockiDiv.innerHTML = ""; }// Remove all HTML content for enter and exit btns
+        RenderHomePage();
       }
       
     } else if (action === 'exit') {
         
-        console.log(employeesTimeClock);
-        
+        // find the line of the employee with status true
         const employeeIndex = employeesTimeClock.findIndex((employeeTime) =>
-        employeeTime.employee.employeeID === Number(id) && employeeTime.status == true
+        employeeTime.employee.employeeID === Number(setionID) && employeeTime.status == true
       );
       
-        if (employeeIndex == -1) {
-            alert('Enter  time not declare:'); 
-        }
-        else
+        if (employeeIndex == -1) {alert('Enter Time Not Declare:'); }  //if there is no index=> the emlpoyee not sing enter
+        else // update exit time and status
         {
             employeesTimeClock[employeeIndex].exitTime = currentDateTime;
             employeesTimeClock[employeeIndex].status = false;
-            console.log(employeesTimeClock);
             alert('Exit date and time saved: ' + dateTime);
 
         }
-        if (clockiDiv) {
-            clockiDiv.innerHTML = ""; // Remove all HTML content
-          }
-          renderEmployeeLogIN(LogINDiv);
-          renderAddEmployee(AddNewWorkerDiv);
-
+        if (clockiDiv) { clockiDiv.innerHTML = "";} // Remove all HTML content
+        RenderHomePage();
+    }} 
+    catch (error) {
+        console.error(error); 
     }
+    
   }
 
-renderEmployeeLogIN (LogINDiv);
-renderAddEmployee(AddNewWorkerDiv);
+  RenderHomePage();
 
 
 
