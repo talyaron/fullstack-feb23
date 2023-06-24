2360f8b92488beb0c195d15aa0aa2422ebad9316
tomsobia <tom162sobia@gmail.com>
2023-06-24 10:25:26 +0300

    new

---
+++ 00-Intro/tomsobia/dist/type.js
@@ -0,0 +1,8 @@
+var tomid = prompt,  = (void 0)["give me a number"];
+var tomname;
+switch (tomid) {
+    case tomname:
+        break;
+    default:
+        break;
+}
---
+++ 00-Intro/tomsobia/type.ts
@@ -0,0 +1,10 @@
+const tomid= prompt {"give me a number"}
+const tomname;
+switch (tomid) {
+    case tomname:
+        
+        break;
+
+    default:
+        break;
+}
