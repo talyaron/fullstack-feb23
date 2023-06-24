ff1181f0435a1cfc1713c90c7b5d00a9115d4f32
tomsobia <tom162sobia@gmail.com>
2023-06-24 11:34:32 +0300

    new

--- 04-Typescript/08-objects/Home-puzzles/tomhadar/dist/type.js
+++ 04-Typescript/08-objects/Home-puzzles/tomhadar/dist/type.js
@@ -1,5 +1,13 @@
-var tom = {
-    name: "tom",
-    age: 28,
-    id: 5435464
+// 1) Create a student object with the following properties: name, gender, mathMidTermGrade, historyMidTermGrade, mathFinalTermGrade, and historyFinalTermGrade.
+var tomcar = {
+    name: "cx5",
+    company: "mazda",
+    year: 2014,
+    color: "red",
+    mil: 100000,
+    carage: function () {
+        return new Date().getFullYear() - this.year;
+    }
 };
+console.log(tomcar);
+console.log(tomcar.carage());
--- 04-Typescript/08-objects/Home-puzzles/tomhadar/type.ts
+++ 04-Typescript/08-objects/Home-puzzles/tomhadar/type.ts
@@ -1,11 +1,28 @@
-interface Person{
-    name: string;
-    age: number;
-    id: number;
-}
 
-const tom:Person= {
-    name: "tom",
-    age: 28,
-    id: 5435464,
+// 1) Create a student object with the following properties: name, gender, mathMidTermGrade, historyMidTermGrade, mathFinalTermGrade, and historyFinalTermGrade.
+
+// 2) Implement a method within the object that calculates the average grade of the student by utilizing the available grades (make use of the 'this' keyword).
+
+// 3) Additionally, create a list of grades for multiple courses and include a method that calculates the average grade across all courses. You can utilize arrays and arrays of grades for this purpose. Also, implement a method that provides the grade for a specific subject based on the provided information (consider using arrays and indexing). 
+
+
+interface Car{
+    name:string;
+    company:string;
+    year:number;
+    color:string;
+    mil:number;
+    carage:Function
 }
+const tomcar:Car={
+    name:"cx5",
+    company:"mazda",
+    year:2014,
+    color:"red",
+    mil:100000,
+    carage:function():number{
+        return new Date().getFullYear()-this.year;
+    }
+}
+console.log(tomcar)
+console.log(tomcar.carage())
