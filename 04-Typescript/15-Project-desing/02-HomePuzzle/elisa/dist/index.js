b808f4687a38d8724f272db9b594d5d56bf9469c
elisaelg <83836208+elisaelg@users.noreply.github.com>
2023-06-24 19:32:07 +0300

    with tablein ts

--- 04-Typescript/15-Project-desing/02-HomePuzzle/elisa/dist/index.js
+++ 04-Typescript/15-Project-desing/02-HomePuzzle/elisa/dist/index.js
@@ -1,3 +1,22 @@
+//בניית הטבלה
+var tableMain = document.createElement("table"); // Create table element
+var headerRow = document.createElement("tr"); // Create table header row
+var headers = ["Entry Time", "Exit Time", "Hours Worked"]; // Create table header cells
+for (var _i = 0, headers_1 = headers; _i < headers_1.length; _i++) {
+    var headerText = headers_1[_i];
+    var headerCell = document.createElement("th");
+    headerCell.textContent = headerText;
+    headerRow.appendChild(headerCell);
+}
+tableMain.appendChild(headerRow); // Append the header row to the table
+var dataRow = document.createElement("tr"); // Create data row
+for (var i = 0; i < headers.length; i++) { // Create empty data cells
+    var dataCell = document.createElement("td");
+    dataRow.appendChild(dataCell);
+}
+tableMain.appendChild(dataRow);
+document.body.appendChild(tableMain);
+// יצירת הקלאס ושעות הכניסה והיציאה
 var TimeClock = /** @class */ (function () {
     function TimeClock(entryTime, exitTime) {
         this.entryTime = entryTime;
@@ -18,14 +37,14 @@ var TimeClock = /** @class */ (function () {
 var timeClock = new TimeClock(null, null);
 var entryTimeButton = document.querySelector("#entry-time");
 var exitTimeButton = document.querySelector("#exit-time");
-var table = document.querySelector("table");
+// const table1 = document.querySelector("table") as HTMLTableElement;
 entryTimeButton.addEventListener("click", function () {
     timeClock.entryTime = new Date();
-    table.rows[1].cells[0].innerHTML = timeClock.entryTime.toLocaleString();
+    tableMain.rows[1].cells[0].innerHTML = timeClock.entryTime.toLocaleString();
 });
 exitTimeButton.addEventListener("click", function () {
     var _a;
     timeClock.exitTime = new Date();
-    table.rows[1].cells[1].innerHTML = timeClock.exitTime.toLocaleString();
-    table.rows[1].cells[2].innerHTML = (_a = timeClock.getHoursWorked()) === null || _a === void 0 ? void 0 : _a.toFixed(2);
+    tableMain.rows[1].cells[1].innerHTML = timeClock.exitTime.toLocaleString();
+    tableMain.rows[1].cells[2].innerHTML = (_a = timeClock.getHoursWorked()) === null || _a === void 0 ? void 0 : _a.toFixed(2);
 });
