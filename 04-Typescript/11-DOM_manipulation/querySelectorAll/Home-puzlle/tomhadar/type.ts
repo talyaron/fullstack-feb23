dc0e73ccf4206e00ed2889622812961df82b1d65
tomsobia <tom162sobia@gmail.com>
2023-06-25 18:29:20 +0300

    new

---
+++ 04-Typescript/11-DOM_manipulation/querySelectorAll/Home-puzlle/tomhadar/type.ts
@@ -0,0 +1,28 @@
+// 1) get an image URL from the user, and add the image to the DOM.
+// 2) get five image URLs from the user and print five images on the DOM.
+// 3) do task number 2. Store items using array of a class. create a method for rendering an image to the dom. , render all of them to the DOM
+class img{
+  imgname:string
+  img:string
+  constructor(imgname:string,img:string){
+    this.imgname
+    this.img
+  }
+}
+
+const imgname
+
+
+
+
+
+
+// const box = document.querySelector("#box");
+// function fivetime() {
+//   for (let i = 0; i < 5; i++) {
+//     const url = prompt("תביא קישור?!");
+//     box.innerHTML += `<p><br><img src="${url}"></img></p><hr>`;
+//   }
+// }
+
+// fivetime() 
