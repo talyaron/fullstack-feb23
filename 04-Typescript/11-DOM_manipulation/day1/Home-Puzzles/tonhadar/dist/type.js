e24a52dc53bc04b32b7efaff3fda7d08fc698c79
Doriel Shacham <dorielshacham@gmail.com>
2023-06-25 20:19:03 +0300

    ץץ

--- 04-Typescript/11-DOM_manipulation/day1/Home-Puzzles/tonhadar/dist/type.js
+++ 04-Typescript/11-DOM_manipulation/day1/Home-Puzzles/tonhadar/dist/type.js
@@ -1,3 +1,20 @@
+// class Car {
+//   name: string;
+//   urlimg: string;
+//   constructor(name: string, urlimg: string) {
+//     this.name= name;
+//     this.urlimg= urlimg;
+//   }
+// }
+// const arr:Car[]= [
+//     new Car("mazda","https://www.topgear.com/sites/default/files/cars-car/carousel/2019/09/ttsc0048.jpg?w=976&h=549"),
+//     new Car("audi","https://www.topgear.com/sites/default/files/cars-car/carousel/2019/09/ttsc0048.jpg?w=976&h=549"),
+//     new Car("ford","https://www.topgear.com/sites/default/files/cars-car/carousel/2019/09/ttsc0048.jpg?w=976&h=549")]
+// const hadar:any= document.querySelector(".tom")
+// const inr= arr.map(Car =>`<div> ${Car.name} </br><p> <img src =${Car.urlimg}</p> </div>`).join(" ");
+// hadar.innerHTML=inr
+//--- NEW 
+var cars = document.querySelector("body > div.car");
 var Car = /** @class */ (function () {
     function Car(name, urlimg) {
         this.name = name;
@@ -5,11 +22,29 @@ var Car = /** @class */ (function () {
     }
     return Car;
 }());
-var arr = [
-    new Car("mazda", "https://www.topgear.com/sites/default/files/cars-car/carousel/2019/09/ttsc0048.jpg?w=976&h=549"),
-    new Car("audi", "https://www.topgear.com/sites/default/files/cars-car/carousel/2019/09/ttsc0048.jpg?w=976&h=549"),
-    new Car("ford", "https://www.topgear.com/sites/default/files/cars-car/carousel/2019/09/ttsc0048.jpg?w=976&h=549")
-];
-var hadar = document.querySelector(".tom");
-var inr = arr.map(function (Car) { return "<div> " + Car.name + " </br><p> <img src =" + Car.urlimg + "</p> </div>"; }).join(" ");
-hadar.innerHTML = inr;
+var arr = [];
+function handleSubmit(event) {
+    event.preventDefault();
+    var nameInput = document.getElementById("car-name");
+    var urlInput = document.getElementById("car-url");
+    var name = nameInput.value;
+    var url = urlInput.value;
+    var newCar = new Car(name, url);
+    arr.push(newCar);
+    nameInput.value = "";
+    urlInput.value = "";
+    renderCars();
+}
+function renderCars() {
+    cars.innerHTML = "";
+    arr.forEach(function (car) {
+        var carDiv = document.createElement("div");
+        carDiv.textContent = "Car Name: " + car.name;
+        var imgElement = document.createElement("img");
+        imgElement.src = car.urlimg;
+        carDiv.appendChild(imgElement);
+        cars.appendChild(carDiv);
+    });
+}
+var form = document.getElementById("car-form");
+form.addEventListener("submit", handleSubmit);
--- 04-Typescript/15-Project-desing/02-HomePuzzle/moria/dist/index2.js
+++ 04-Typescript/15-Project-desing/02-HomePuzzle/moria/dist/index2.js
@@ -3,20 +3,53 @@ function handleSubmit(event) {
     var EmployeeName = event.target.name.value;
     var EmployeeEnter = event.target.EnterTime.value;
     var EmployeeExit = event.target.ExitTime.value;
-    renderImages(EmployeeName, EmployeeEnter, EmployeeExit);
+    // renderImages(EmployeeName, EmployeeEnter, EmployeeExit)
+    var worker = new s(EmployeeName, EmployeeEnter, EmployeeExit, Result);
+    renderLoggedWorker(worker, document.getElementById('root'));
 }
+//-----------
 var s = /** @class */ (function () {
-    function s() {
+    function s(EmployeeName, EmployeeEnter, EmployeeExit) {
+        this.EmployeeName = EmployeeName,
+            this.EmployeeEnter = EmployeeEnter,
+            this.EmployeeExit = EmployeeExit;
     }
     return s;
 }());
 var sArr = [];
-function renderImages(EmployeeName, EmployeeEnter, EmployeeExit) {
-    var root = document.querySelector("#root");
-    if (root) {
-        var html = "\n        <table>\n        <tr>\n            <td>First name</td>\n            <td>entrance time</td>\n            <td>leaving time </td>\n            </tr>\n            <td>" + EmployeeName + " </td>\n            <td>" + EmployeeEnter + "</td>\n            <td>" + EmployeeExit + " </td>\n            </tr>\n        </table>";
-        // console.log(EmployeeEnter)
-        // console.log(EmployeeExit)
-        root.innerHTML += html;
+function renderLoggedWorker(workers, rootElement) {
+    try {
+        var html = "<h2>Hello " + workers.EmployeeName + " you worked from " + workers.EmployeeEnter + " until " + workers.EmployeeExit + "</h2>";
+        if (!rootElement)
+            throw new Error("no root element");
+        rootElement.innerHTML += html;
+    }
+    catch (error) {
+        console.error(error);
     }
 }
+//----------
+// function renderImages(
+//     EmployeeName: string,
+//     EmployeeEnter: number,
+//     EmployeeExit: number
+// ) {
+//     const root = document.querySelector(`#root`) as HTMLElement;
+//     if (root) {
+//         const html = `
+//         <table>
+//         <tr>
+//             <td>First name</td>
+//             <td>entrance time</td>
+//             <td>leaving time </td>
+//             </tr>
+//             <td>${EmployeeName} </td>
+//             <td>${EmployeeEnter}</td>
+//             <td>${EmployeeExit} </td>
+//             </tr>
+//         </table>`;
+//         // console.log(EmployeeEnter)
+//         // console.log(EmployeeExit)
+//         root.innerHTML += html;
+//     }
+// }
