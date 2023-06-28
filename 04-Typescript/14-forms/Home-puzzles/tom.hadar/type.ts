7fb29ecd88f5ce8222918237745648bdb90b91e2
tomsobia <tom162sobia@gmail.com>
2023-06-28 13:02:20 +0300

    new

--- 04-Typescript/14-forms/Home-puzzles/tom.hadar/type.ts
+++ 04-Typescript/14-forms/Home-puzzles/tom.hadar/type.ts
@@ -1,23 +1,67 @@
 // class
-const root=document.querySelector("#root") as HTMLDivElement
-class Uzer{
-    constructor(public imgUrl:string,public width:number){}
+class Uzer {
+  constructor(public imgUrl, public width) {}
 }
 
-const Imgarr:Uzer[]=new Array();
-function UrlWidth(event:any){
-try {
+const Imgarr = [];
+
+function UrlWidth(event) {
+  try {
     event.preventDefault();
-    const imgUrl=event.target.imgUrl.value
-    const width =event.target.width.value
-    const data = new Uzer (imgUrl,width);
-    console.log(data)
-    Imgarr.push(data)
-    console.log(Imgarr)
-    event.target.reset()
-} catch (error) {
+    const imgUrl = event.target.elements.imgUrl.value;
+    const width = event.target.elements.width.value;
+    const data = new Uzer(imgUrl, width);
+    Imgarr.push(data);
+
+    const root = document.querySelector("#root") as HTMLDivElement;
+    event.target.reset();
+  } catch (error) {
     console.log(error);
+  }
 }
+function render(uzer,root) {
+  
+  const html = uzer.map((uzer) => {
+      return `<div>
+      <img src="${uzer.imgUrl}"style="width:${uzer.width}px;">
+      </div>`;
+    }).join("");
+   
+
+     root.innerHTML = html;
 }
 
-    
+
+// class Uzer {
+//     constructor(public imgUrl:any,public width:number) {}
+// }
+
+// const Imgarr = [];
+
+// function UrlWidth(event) {
+//     try {
+//         event.preventDefault();
+//         const imgUrl = event.target.elements.imgUrl.value;
+//         const width = event.target.elements.width.value;
+//         const data = new Uzer(imgUrl, width);
+//         console.log(data);
+//         Imgarr.push(data);
+
+//         const root = document.querySelector("#root");
+//         event.target.reset();
+//         render(Imgarr, root);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// function render(uzer, root) {
+//     const html = uzer.map((uzer) => {
+//         return `<div>
+//             <img src="${uzer.imgUrl}" style="width: ${uzer.width}px;">
+//         </div>`;
+//     }).join("");
+
+//         root.innerHTML = html;
+
+// }
