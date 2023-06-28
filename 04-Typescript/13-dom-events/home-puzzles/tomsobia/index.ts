c6ff43c9d7ab578b816253209c86fc3f3bd6c4a4
tomsobia <tom162sobia@gmail.com>
2023-06-28 11:19:35 +0300

    new

--- 04-Typescript/13-dom-events/home-puzzles/tomsobia/index.ts
+++ 04-Typescript/13-dom-events/home-puzzles/tomsobia/index.ts
@@ -1,13 +1,13 @@
-const clun = document.querySelector("body > div > div.box1 > img");
-const originalSrc = clun.src; // שמירת הכתובת המקורית של התמונה
+const clown:any = document.querySelector("body > div > div.box1 > img");
+const originalSrc = clown.src;
 
-clun.addEventListener("click", () => {
-  if (clun.src === originalSrc) {
-    // אם התמונה הנוכחית היא התמונה המקורית, אז נחזיר לתמונה השניה
-    clun.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBSWYOtots3xA-Sw2ofN_Z5b6OUFdFIbNJ7VKX2FB4ghATDvupEQS5dac1UGDkr7MxxCk&usqp=CAU";
+clown.addEventListener("click", () => {
+  if (clown.src === originalSrc) {
+
+    clown.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBSWYOtots3xA-Sw2ofN_Z5b6OUFdFIbNJ7VKX2FB4ghATDvupEQS5dac1UGDkr7MxxCk&usqp=CAU";
   } else {
-    // אחרת, נחזיר לתמונה המקורית
-    clun.src = originalSrc;
+
+    clown.src = originalSrc;
   }
 });
-console.log(clun);
+console.log(clown);
