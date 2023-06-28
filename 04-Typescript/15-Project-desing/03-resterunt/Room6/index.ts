36046048fff937ca77a143424773bd00ba630f7a
liran26 <liranav26@gmail.com>
2023-06-28 20:33:44 +0300

    .

---
+++ 04-Typescript/15-Project-desing/03-resterunt/Room6/index.ts
@@ -0,0 +1,62 @@
+
+class Table{
+    id:number;
+    occupide:boolean  = false;
+    constructor(public order:Order )
+    {
+        this.id =Math.random() * 10; 
+    }
+    
+    setOccupide(){
+        this.occupide = !this.occupide;
+    }
+}
+const tables:Table[] = [];
+
+class Order{
+    id:number;
+    totalPayment:number = 0;
+    constructor(public tableOrder:Dish[], public phoneNum:string ){
+        this.id = Math.random()*10000;
+    }
+
+    calculatePayment(){
+        try {
+            if(this.tableOrder === null)throw new Error();
+            this.tableOrder.forEach(dish =>{
+                this.totalPayment += dish.price;
+            } )
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
+const orders:Order[] = [];
+
+class Dish{
+    constructor(public name:string, public price:number, public img:string, public description:string){}
+    setPrice(price:number){
+        this.price = price;
+    }
+}
+const dishes:Dish[] = [];
+
+
+function renderMain(rootElement: HTMLElement | null) {
+    try {
+        const html = `<h2>${new Date().getDate()}.${new Date().getMonth() + 1}.${new Date().getFullYear()}</h2>
+        <form onclick="handleFormSelect(event)">
+        <input type="button" id="newOrder" value="New Order">
+        <input type="button" id=""OpensOrders" value="Opens Orders">
+        <input type="button" id="DishManaging" value="Dish Managing">
+        </form>`;
+        if (!rootElement) throw new Error("no root element");
+
+        rootElement.innerHTML = html;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+renderMain(document.querySelector("#main"));
