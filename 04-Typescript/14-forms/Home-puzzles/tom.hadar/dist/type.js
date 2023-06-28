92a1473d0bae6e1c5238c21797cf8dfd480dafb4
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2023-06-28 14:14:07 +0300

    .

--- 04-Typescript/14-forms/Home-puzzles/tom.hadar/dist/type.js
+++ 04-Typescript/14-forms/Home-puzzles/tom.hadar/dist/type.js
@@ -1,30 +1,35 @@
-// class Img{
-//     constructor(public imgUrl, public width){}
-// }
-// const imgs= []
-// function UrlWidth(event){
-//     try {
-//     event.preventDefault();
-//     const imgURl= event.target.elements.imgUrl.value;
-//     const width= event.target.elements.width.value;
-//     const data= new Img(imgURl,width);
-//     imgs.push(data);
-//     console.log(imgs);
-//     event.target.reset()
-//     const root= document.querySelector("#root")
-//     hadar(imgs,root)
-//     } catch (error) {
-//         console.error(error)
-//     }
-// }
-// function hadar(imgs,root){
-//     try {
-//         const html= imgs.map((imgs)=>{
-//           return `<div>
-//           <img src="${imgs.imgUrl}" style="width${imgs.width}px;"></div> `  
-//         }).join("")
-//         root.innerHTML= html
-//     } catch (error) {
-//         console.error(error)
-//     }
-// }
+var Img = /** @class */ (function () {
+    function Img(imgUrl, width) {
+        this.imgUrl = imgUrl;
+        this.width = width;
+    }
+    return Img;
+}());
+var imgs = [];
+function UrlWidth(event) {
+    try {
+        event.preventDefault();
+        var imgURl = event.target.elements.imgUrl.value;
+        var width = event.target.elements.width.value;
+        var data = new Img(imgURl, width);
+        imgs.push(data);
+        console.log(imgs);
+        event.target.reset();
+        var root = document.querySelector("#root");
+        hadar(imgs, root);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+function hadar(imgs, root) {
+    try {
+        var html = imgs.map(function (imgs) {
+            return "<div>\n          <img src=\"" + imgs.imgUrl + "\" style=\"width" + imgs.width + "px;\"></div> ";
+        }).join("");
+        root.innerHTML = html;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
--- 04-Typescript/14-forms/Home-puzzles/tom.hadar/type.ts
+++ 04-Typescript/14-forms/Home-puzzles/tom.hadar/type.ts
@@ -1,34 +1,34 @@
-// class Img{
-//     constructor(public imgUrl, public width){}
-// }
+class Img{
+    constructor(public imgUrl, public width){}
+}
 
-// const imgs= []
+const imgs= []
 
-// function UrlWidth(event){
-//     try {
-//     event.preventDefault();
-//     const imgURl= event.target.elements.imgUrl.value;
-//     const width= event.target.elements.width.value;
-//     const data= new Img(imgURl,width);
-//     imgs.push(data);
-//     console.log(imgs);
-//     event.target.reset()
-//     const root= document.querySelector("#root")
-//     hadar(imgs,root)
-//     } catch (error) {
-//         console.error(error)
-//     }
-// }
-// function hadar(imgs,root){
-//     try {
-//         const html= imgs.map((imgs)=>{
-//           return `<div>
-//           <img src="${imgs.imgUrl}" style="width${imgs.width}px;"></div> `  
-//         }).join("")
+function UrlWidth(event){
+    try {
+    event.preventDefault();
+    const imgURl= event.target.elements.imgUrl.value;
+    const width= event.target.elements.width.value;
+    const data= new Img(imgURl,width);
+    imgs.push(data);
+    console.log(imgs);
+    event.target.reset()
+    const root= document.querySelector("#root")
+    hadar(imgs,root)
+    } catch (error) {
+        console.error(error)
+    }
+}
+function hadar(imgs,root){
+    try {
+        const html= imgs.map((imgs)=>{
+          return `<div>
+          <img src="${imgs.imgUrl}" style="width${imgs.width}px;"></div> `  
+        }).join("")
 
-//         root.innerHTML= html
-//     } catch (error) {
-//         console.error(error)
-//     }
+        root.innerHTML= html
+    } catch (error) {
+        console.error(error)
+    }
     
-// }
+}
