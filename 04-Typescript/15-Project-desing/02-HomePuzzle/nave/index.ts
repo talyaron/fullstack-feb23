71abd5418956aaf2aba7fd7f3a4dbb81482c2fb4
navered <128045047+navered@users.noreply.github.com>
2023-06-29 09:07:00 +0300

    nave

--- 04-Typescript/15-Project-desing/02-HomePuzzle/nave/index.ts
+++ 04-Typescript/15-Project-desing/02-HomePuzzle/nave/index.ts
@@ -7,99 +7,61 @@
 // 2) The system can log different users (use select input). the system can calculate the user monthly total hours
 // 3) the user can see all workers times, serach for worker, and show each worker total times. the user could edit entrance details
 
+
+
 const uid = function () {
   return Date.now().toString(36) + Math.random().toString(36).substr(2);
 };
 
-//Model
+// Model
 class Employee {
   id: string;
-  constructor(public userName: string, public email: string, public  workerhours: number ) {
+  constructor(public userName: string, public email: string) {
     this.id = uid();
-  }}
+  }
+}
 
-const Employees: Employee[] = [
-    new Employee("nave", "vnavev@gmail.com", 2),
-    new Employee("lior",  "lior@gmail.com", 17)
+const employees: Employee[] = [
+  new Employee("nave", "vnavev@gmail.com"),
+  new Employee("lior", "lior@gmail.com"),
 ];
 
+class HoursDaily {
+  id: string;
+  constructor(public worker: string, public entrance: any, public exit: any) {
+    this.id = uid();
+  }
 
-class LogIn{
-  id:string;
-  constructor(public employee:Employee, public hour:number){
-this.id = uid();
-}}
-
-const logIns: LogIn[] = [
-  new LogIn(Employee.arguments, 12)
-];
-class LogOut{
-  id:string;
-  constructor(public employee:Employee, public hour:number){{
-this.id = uid();
-}}}
-const logOuts: LogOut[] = [
-  new LogOut (Employee.arguments, 14)
-];
-
-function calculateHoursWorked() {
-  const hoursWorked: { [key: string]: number } = {}; // Object to store hours worked for each employee
-
-  // Loop through each login and logout entry and calculate hours worked for each employee
-  for (const login of logIns) {
-    const logout = logOuts.find(logout => logout.employee.id === login.employee.id); // Find the corresponding logout entry
-    if (logout) {
-      const hours = logout.hour - login.hour; // Calculate hours worked
-      if (hoursWorked[login.employee.id]) {
-        hoursWorked[login.employee.id] += hours; // Add hours worked to existing total for the employee
-      } else {
-        hoursWorked[login.employee.id] = hours; // Set hours worked for the employee
-      }
+  calculateDailyHours(): number | undefined {
+    try {
+      const exitHour = new Date(this.exit);
+      const enterHour = new Date(this.entrance);
+      const exitHourMilliS = exitHour.getTime();
+      const enterHourMilliS = enterHour.getTime();
+      console.log(exitHourMilliS, enterHourMilliS);
+      if (!enterHour || !exitHour) throw new Error("Missing entrance or exit");
+      const dailyhours: number = (exitHourMilliS - enterHourMilliS) / 3600000;
+      console.log(dailyhours);
+      return dailyhours;
+    } catch (error) {
+      console.error(error);
+      return undefined;
     }
   }
-
-  return hoursWorked;
 }
 
+const hoursD: HoursDaily[] = [];
 
-
-
-
-class Passanger {
-  id: string;
-  // flights:Flight[] = [];
-  constructor(
-    public firstName: string,
-    public lastName: string,
-    public email: string
-  ) {
-    this.id = uid();
-  }
-}
-
-
-//join classes
-
-class PassangerFlights {
-  constructor(public passenger: Passanger, public flight: Flight) {}
-}
-
-const passengersFlights: PassangerFlights[] = [];
-
-// view controlers
-//register user
 function renderRegisterUser(rootElement: HTMLElement | null) {
   try {
-    const html = `
-        <form onsubmit="handleRegisterUser(event)">
+    const html = ` <div class="registers" id="register">
+        <form class="register" onsubmit="handleRegisterUser(event)">
             <label for="firstName">First name</label>
-            <input type="text" name="firstName" id='firstName' placeholder="first name" required>
-            <label for="lastName">Last name</label>
-            <input type="text" name="lastName" id="'lastName" placeholder="last name" required>
+            <input type="text" name="userName" id='firstName' placeholder="first name" required>
             <label for="email">Email</label>
-            <input type="email" name="email" id="email" required>
+            <input type="email" name="email" id="email" placeholder="email" required>
             <input type="submit" value="Register">
-        </form>`;
+        </form></div>`;
 
     if (!rootElement) throw new Error("No root element");
 
@@ -109,18 +71,20 @@ function renderRegisterUser(rootElement: HTMLElement | null) {
   }
 }
 
-//calculate
-
-renderRegisterUser(document.querySelector("#register"));
-renderCalculateDistance(document.querySelector("#calculate"));
-
-function renderLoggedUser(
-  passenger: Passanger,
-  rootElement: HTMLElement | null
-) {
+function renderLoggedUser(employee: Employee, rootElement: HTMLElement | null) {
   try {
-    const html = `<h2>Hello ${passenger.firstName}</h2>`;
-    if (!rootElement) throw new Error("no root element");
+    const html = ` <div class="LoggedUser" ><h2>Hello ${employee.userName}</h2>
+    <form class="register" onsubmit="handleTimeClock(event)">
+    <label for="firstName">First name</label>
+    <input type="text" name="worker" id='firstName' placeholder="firstname" required>
+    <label for="entrance">Entrance</label>
+    <input type="datetime-local" name="entrance" id="entrance" required>
+    <label for="exit">Exit</label>
+    <input type="datetime-local" name="exit" id="exit" required>
+    <input type="submit" value="Calculate">
+</form>
+    `;
+    if (!rootElement) throw new Error("No root element");
 
     rootElement.innerHTML = html;
   } catch (error) {
@@ -128,48 +92,73 @@ function renderLoggedUser(
   }
 }
 
-// view->model controlers
 function handleRegisterUser(ev: any) {
   try {
     ev.preventDefault();
-    const firstName = ev.target.firstName.value;
-    const lastName = ev.target.lastName.value;
+    const userName = ev.target.userName.value;
     const email = ev.target.email.value;
 
-    const passenger = new Passanger(firstName, lastName, email);
+    const employee = new Employee(userName, email);
 
-    //add to model
-    passengers.push(passenger);
+    // Add to model
+    employees.push(employee);
 
-    //control->view
-    renderLoggedUser(passenger, document.querySelector("#register"));
+    // Control -> View
+    renderLoggedUser(employee, document.querySelector("#calculate"));
 
-    console.log(firstName, lastName, email);
+    console.log(userName, email);
   } catch (error) {
     console.error(error);
+    return undefined;
   }
 }
 
-function renderCalculateDistance(rootElement: HTMLElement | null) {
+function handleTimeClock(ev: any) {
   try {
-    const html = `
-        <form onsubmit="handleCalculate(event)">
-        <h2>From - to</h2>
-        <label for="from">From</label>
-        <select name="from" id="from">
-            ${airports.map((airport) => {
-              return `<option value="${airport.id}">${airport.name}</option>`;
-            })}
-            
-        </select>
-        <label for="to">To</label>
-        <select name="to" id="to">
-        ${airports.map((airport) => {
-          return `<option value="${airport.id}">${airport.name}</option>`;
-        })}
-        </select>
-        <input type="submit" value="Calculate">
-    </form>`;
+    ev.preventDefault();
+    const worker = ev.target.worker.value;
+    const entrance = ev.target.entrance.value;
+    const exit = ev.target.exit.value;
+
+    const timeClockEntry = new HoursDaily(worker, entrance, exit);
+
+    // Add to model
+    hoursD.push(timeClockEntry);
+
+    // Control -> View
+    renderTimeClockToHtml(hoursD, document.querySelector("#renderCalculate"));
+
+    console.log(worker, entrance, exit);
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+}
+
+function renderTimeClockToHtml(timeClockArr: HoursDaily[], rootElement: HTMLElement | null) {
+  try {
+    let html = `
+      <table id="timeTable">
+          <tr>
+              <th>Worker</th>
+              <th>Entrance</th>
+              <th>Exit</th>
+              <th>Total Hours</th>
+          </tr>`;
+
+    timeClockArr.forEach((element) => {
+      console.log(element);
+      const totalHours = element.calculateDailyHours();
+      html += `
+         <tr>
+         <td>${element.worker}</td>
+          <td>${element.entrance}</td>
+          <td>${element.exit}</td>
+          <td>${totalHours}</td>
+         </tr>`;
+    });
+
+    html += `</table>`;
 
     if (!rootElement) throw new Error("No root element");
 
@@ -179,33 +168,12 @@ function renderCalculateDistance(rootElement: HTMLElement | null) {
   }
 }
 
-function handleCalculate(ev: any) {
-  try {
-    ev.preventDefault();
+renderRegisterUser(document.querySelector("#calculate"));
 
-    //get from airport
-    const airportIdFrom = ev.target.from.value;
 
-    //get to airport
-    const airportIdTo = ev.target.to.value;
+   
+  
+  
 
-    console.log(airportIdFrom, airportIdTo);
+    
 
-    const airportFrom = airports.find(
-      (airport) => airport.id === airportIdFrom
-    );
-    const airportTo = airports.find((airport) => airport.id === airportIdTo);
-    if (!airportTo || !airportFrom)
-      throw new Error("Couldnt find one of the airport");
-
-    const flight = new Flight(airportFrom, airportTo);
-
-    flights.push(flight);
-    const distance = flight.calculateDistanceInKm();
-    const rootDistance: any = document.querySelector("#distance");
-    if (rootDistance) rootDistance.innerHTML = distance;
-    console.log(distance);
-  } catch (error) {
-    console.error(error);
-  }
-}
