cfe62523d88135caafb0af4631bfb6c543bf6ea7
itahTamar <tamar.verber@gmail.com>
2023-06-29 21:40:38 +0300

    hw15.3 resturant orders start

--- 04-Typescript/15-Project-desing/03-resterunt/room2-t.d.sh.r/dist/index.js
+++ 04-Typescript/15-Project-desing/03-resterunt/room2-t.d.sh.r/dist/index.js
@@ -1,29 +1,30 @@
 //entities:
 //Tables, Order, Dishes
+//return a random complex number as string
 var uid = function () {
     return Date.now().toString(36) + Math.random().toString(36).substr(2);
 };
+//table number with number of people sitting on it
 var Table = /** @class */ (function () {
-    function Table(capacity) {
+    function Table(tableNumbe, capacity, chatch) {
+        this.tableNumbe = tableNumbe;
         this.capacity = capacity;
-        this.uidTable = Number(uid());
+        this.chatch = chatch;
     }
-    Table.prototype.getTable = function () {
-        return this.capacity;
-    };
     return Table;
 }());
-var Dishe = /** @class */ (function () {
-    function Dishe(name, img, price, status, description) {
-        this.name = name;
+var Dish = /** @class */ (function () {
+    function Dish(disgName, img, price, status, description) {
+        this.disgName = disgName;
         this.img = img;
         this.price = price;
         this.status = status;
         this.description = description;
         this.uidDishe = Number(uid());
     }
-    return Dishe;
+    return Dish;
 }());
+//all the dishes for one table
 var Order = /** @class */ (function () {
     function Order(table, dishes, status) {
         this.table = table;
@@ -31,11 +32,31 @@ var Order = /** @class */ (function () {
         this.status = status;
         this.uidOrder = Number(uid());
     }
-    Order.prototype.addOrder = function () {
+    Order.prototype.openTable = function () {
         //add new order
+        try {
+            if (!this.table.tableNumbe)
+                throw new Error("no table catched");
+            //3 btns -> add, del, close
+            return (this.table.chatch = true);
+        }
+        catch (error) {
+            console.error(error);
+        }
     };
-    Order.prototype.editOrder = function (action, dishe) {
-        //edit dishe
+    Order.prototype.addDish = function (table, dish) {
+        //add dish to order array
+        try {
+            if (!table.chatch)
+                throw new Error("not an open table");
+            order.push(dish);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    };
+    Order.prototype.deletDish = function () {
+        //delet dish from order array
     };
     Order.prototype.closeOrder = function () {
         // calc sum of dises price
--- 04-Typescript/15-Project-desing/03-resterunt/room2-t.d.sh.r/index.ts
+++ 04-Typescript/15-Project-desing/03-resterunt/room2-t.d.sh.r/index.ts
@@ -1,39 +1,56 @@
 //entities:
 //Tables, Order, Dishes
+
+//return a random complex number as string
 const uid = function () {
    return Date.now().toString(36) + Math.random().toString(36).substr(2);
  };
 
+ //table number with number of people sitting on it
 class Table {
-    uidTable: number;
-    constructor(public capacity: number){
-        this.uidTable = Number(uid());
-    }
-
-    getTable(){
-        return this.capacity 
-    }
+    constructor(public tableNumbe :number, public capacity: number, public chatch: boolean){}
 }
 
-class Dishe {
+
+class Dish {
     uidDishe: number;
-    constructor(public name:string,public img:string, public price:number,public status:boolean, public description:string){
+    constructor(public disgName:string, public img:string, public price:number, public status:boolean, public description:string){
         this.uidDishe = Number(uid());
     }
 
 }
 
+//all the dishes for one table
 class Order {
     uidOrder: number;
-    constructor(public table: Table, public dishes: Dishe[],public status:boolean){
+    constructor(public table: Table, public dishes: Dish[],public status:boolean){
         this.uidOrder = Number(uid());
     }
-    addOrder(){
+    openTable(){
         //add new order
+        try {
+            if(!this.table.tableNumbe) throw new Error("no table catched");
+            //3 btns -> add, del, close
+            return (this.table.chatch=true)
+
+        } catch (error) {
+            console.error(error)   
+        }
 
     }
-    editOrder(action:string,dishe:Dishe){
-        //edit dishe
+    addDish(table: Table, dish:Dish){
+        //add dish to order array
+        try {
+           if(!table.chatch) throw new Error("not an open table"); 
+           order.push(dish) 
+
+        } catch (error) {
+               console.error(error)   
+        }
+    }
+
+    deletDish(){
+        //delet dish from order array
     }
     closeOrder(){
 
