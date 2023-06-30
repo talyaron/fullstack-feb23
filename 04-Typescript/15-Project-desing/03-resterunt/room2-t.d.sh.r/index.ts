1ad69dbfa12c8f909788c09ce05034db56d46ef7
itahTamar <tamar.verber@gmail.com>
2023-06-30 11:20:05 +0300

    change the plan and add dishes

--- 04-Typescript/15-Project-desing/03-resterunt/room2-t.d.sh.r/index.ts
+++ 04-Typescript/15-Project-desing/03-resterunt/room2-t.d.sh.r/index.ts
@@ -1,49 +1,38 @@
 //entities:
 //Tables, Order, Dishes
 
-//return a random complex number as string
-const uid = function () {
-   return Date.now().toString(36) + Math.random().toString(36).substr(2);
- };
 
- //table number with number of people sitting on it
+
+//no need for unic ids!!
+
+// the table class will contain the order class
+//the table class will have the option to add or delet an order
+//the table class will have the option to open or closed the table (t-open, f-closed) 
 class Table {
-    constructor(public tableNumbe :number, public capacity: number, public catched: boolean){}
-}
+    constructor(public tableNumber :number, public catched: boolean, order: Order){}
 
-
-class Dish {
-    constructor(public dishName:string, public img:string, public price:number, public status:boolean, public description:string){
-    }
-}
-
-const dishes: Dish[] = []; 
-const order: Order[] = [];
-
-//all the dishes for one table
-class Order {
-    uidOrder: number;
-    constructor(public table: Table, public dishes: Dish[], public status:boolean){
-        this.uidOrder = Number(uid());
-    }
     openTable(){
         //the function return that the table is catched (=true)
         try {
-            if(!this.table.tableNumbe) throw new Error("no table catched");
+            if(!this.tableNumber) throw new Error("no table catched");
             //3 btns -> add, del, close
-            return (this.table.catched=true)
+            this.catched=true;  // the table is now open
 
         } catch (error) {
             console.error(error)   
         }
 
     }
-    addDish(table: Table, dish:Dish, order:Order[]){
+
+    //to add dish to an order, we need the specific order and the dish to add
+    addDish(dish:Dish, order:Order[]){
         //add dish to order array
-        //need to get the table num and to find in it the uid of the order to 
+
         try {
-           if(!table.catched) throw new Error("not an open table"); 
-           order.push(dish) 
+            if(!order) throw new Error("no order picked")
+            if(!dish) throw new Error("not a dish")
+
+            order.push(dish)
 
         } catch (error) {
                console.error(error)   
@@ -58,5 +47,29 @@ class Order {
         this.status=false;
     }
 
-
 }
+
+//the order class will contain the dish class
+class Order {
+    constructor(public dishes: Dish[]){
+    }
+}
+
+const ordersArray: Order[] = []; //contain all the dishes in a spesific order
+
+
+//the Dish class will contain the info of the dish
+class Dish {
+    constructor(public dishName:string, public img:string, public price:number, public description:string){
+    }
+}
+
+//creat some dished
+const pastaRed = new Dish("pastaRed","https://foodhub.scene7.com/is/image/woolworthsltdprod/Easy-chicken-and-bacon-pasta:Desktop-1300x658", 26, "red pasta with bazil")
+const pastaMilk = new Dish("pastaMilk", "https://realhousemoms.com/wp-content/uploads/One-Pot-Alfredo-Pasta-RECIPE-CARD2.jpg", 26, "pasta with milk")
+const pizzaOnion = new Dish("pizzaOnion", "https://pizzatoday.com/wp-content/uploads/2020/05/OnionPizza-1.jpg", 35, "pizza with onions")
+const pizzaOliv = new Dish("pizzaOliv", "https://images.squarespace-cdn.com/content/v1/55370317e4b047f1053ee431/1546891998774-XFFPMCUA4MAQ5251Y5TA/ke17ZwdGBToddI8pDm48kEKeeVwguwXFnn1ZDuv86ikUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8PaoYXhp6HxIwZIk7-Mi3Tsic-L2IOPH3Dwrhl-Ne3Z2sPGQHE4gTOo1QfbaGNmWRT3ltfHJNtU0FQBDPzwxb0xkOpdljO7Z-5qh0zg85Jnj/Loaded+Olive+Pizza?format=original", 30, "pizza with olives")
+
+const dishes: Dish[] = [pastaRed, pastaMilk, pizzaOliv, pizzaOnion]; //contain the information about all dishes
+
+
