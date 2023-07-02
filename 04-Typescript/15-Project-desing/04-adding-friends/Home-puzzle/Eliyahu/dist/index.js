c91e314e66460a5a52706f78fe094615715ff740
Eliyaho basson <basson131@gmail.com>
2023-07-02 22:30:24 +0300

    q

--- 04-Typescript/15-Project-desing/04-adding-friends/Home-puzzle/Eliyahu/dist/index.js
+++ 04-Typescript/15-Project-desing/04-adding-friends/Home-puzzle/Eliyahu/dist/index.js
@@ -5,12 +5,34 @@ var Vegetable = /** @class */ (function () {
         this.amount = amount;
         this.id = "id-" + (new Date().getTime() - Math.random());
     }
+    Vegetable.prototype.ateOne = function () {
+        this.amount--;
+    };
+    Vegetable.prototype.buyOne = function () {
+        this.amount++;
+    };
     return Vegetable;
 }());
-var vegetables = [];
+var vegetables = getVegetablesFromStorage();
+function getVegetablesFromStorage() {
+    try {
+        var vegetablesString = localStorage.getItem("vegetables");
+        if (!vegetablesString)
+            return [];
+        var vegetablesArray = JSON.parse(vegetablesString);
+        var vegetables_1 = vegetablesArray.map(function (vegetable) {
+            return new Vegetable(vegetable.name, vegetable.image, vegetable.amount);
+        });
+        return vegetables_1;
+    }
+    catch (error) {
+        console.error(error);
+        return [];
+    }
+}
 function renderAddVegetable(rootElement) {
     try {
-        var html = "<form onsubmit=\"handleAddVegetable(event)\">\n        <input type=\"text\" name=\"name\" placeholder=\"Name\">\n        <input type=\"url\" name=\"image\" placeholder=\"Image url\">\n        <input type=\"number\" name=\"amount\" placeholder=\"Insert amount\">\n        <input type=\"submit\" value=\"ADD\">\n            </form>";
+        var html = "<form onsubmit=\"handleAddVegetable(event)\">\n        <input type=\"text\" name=\"name\" placeholder=\"Name\" required>\n        <input type=\"url\" name=\"image\" placeholder=\"Image url\">\n        <input type=\"number\" name=\"amount\" placeholder=\"Insert amount\" required>\n        <input type=\"submit\" value=\"ADD\">\n            </form>";
         if (!rootElement)
             throw new Error("can't find rootElement");
         rootElement.innerHTML = html;
@@ -21,6 +43,9 @@ function renderAddVegetable(rootElement) {
 }
 function renderVegetableCard(vegetable) {
     try {
+        if (vegetable.image === '') {
+            vegetable.image = "https://t3.ftcdn.net/jpg/03/11/87/52/360_F_311875255_d57wDCwlZxdtOEwsnmXLHkV1r29i1R2U.jpg";
+        }
         return "<div class=\"card\">\n        <img src=\"" + vegetable.image + "\">\n        <h3>" + vegetable.name + "</h3>\n        <p>" + vegetable.amount + " unit </p>\n        <button onclick=\"handleRemoveVegetableUnit('" + vegetable.id + "')\">I ATE ONE</button>\n        <button onclick=\"handleAddVegetableUnit('" + vegetable.id + "')\">I BUY ONE</button>\n        <button onclick=\"handleEditVegetable('" + vegetable.id + "')\">Edit</button>\n        <button onclick=\"handleDeleteVegetable('" + vegetable.id + "')\">Remove</button>\n    </div>\n";
     }
     catch (error) {
@@ -31,7 +56,7 @@ function renderAllVegetables(vegetables, rootElement) {
     try {
         if (!rootElement)
             throw new Error("can't find rootElement");
-        var html = "<div class=\"cd\">\n        " + vegetables.map(function (vegetable) { return renderVegetableCard(vegetable); }).join(' ') + " </div>";
+        var html = "<div class=\"cards\">\n        " + vegetables.map(function (vegetable) { return renderVegetableCard(vegetable); }).join(' ') + " </div>";
         rootElement.innerHTML = html;
     }
     catch (error) {
@@ -47,7 +72,36 @@ function handleAddVegetable(ev) {
         var newVegetable = new Vegetable(name, image, amount);
         vegetables.push(newVegetable);
         console.log(vegetables);
+        renderAllVegetables(vegetables, document.querySelector("#rootVegetable"));
+        localStorage.setItem("vegetables", JSON.stringify(vegetables));
         ev.target.reset();
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+function handleRemoveVegetableUnit(vegetableId) {
+    try {
+        var vegetable = vegetables.find(function (vegetable) { return vegetable.id === vegetableId; });
+        if (!vegetable)
+            throw new Error("can't find vegetable");
+        if (vegetable.amount !== 0) {
+            vegetable.ateOne();
+        }
+        localStorage.setItem("vegetables", JSON.stringify(vegetables));
+        renderAllVegetables(vegetables, document.querySelector("#rootVegetable"));
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+function handleAddVegetableUnit(vegetableId) {
+    try {
+        var vegetable = vegetables.find(function (vegetable) { return vegetable.id === vegetableId; });
+        if (!vegetable)
+            throw new Error("can't find vegetable");
+        vegetable.buyOne();
+        localStorage.setItem("vegetables", JSON.stringify(vegetables));
         renderAllVegetables(vegetables, document.querySelector("#rootVegetable"));
     }
     catch (error) {
--- 04-Typescript/15-Project-desing/04-adding-friends/Home-puzzle/Eliyahu/index.ts
+++ 04-Typescript/15-Project-desing/04-adding-friends/Home-puzzle/Eliyahu/index.ts
@@ -3,16 +3,38 @@ class Vegetable {
     constructor(public name: string, public image: string, public amount: number) {
         this.id = `id-${new Date().getTime() - Math.random()}`
     }
+    ateOne() {
+        this.amount--
+    }
+    buyOne(){
+        this.amount++
+    }
 }
 
-const vegetables: Vegetable[] = []
+const vegetables: Vegetable[] = getVegetablesFromStorage()
+
+function getVegetablesFromStorage() {
+    try {
+
+        const vegetablesString = localStorage.getItem("vegetables")
+        if (!vegetablesString) return [];
+        const vegetablesArray = JSON.parse(vegetablesString);
+        const vegetables: Vegetable[] = vegetablesArray.map(vegetable => {
+            return new Vegetable(vegetable.name, vegetable.image, vegetable.amount)
+        })
+        return vegetables
+    } catch (error) {
+        console.error(error)
+        return []
+    }
+}
 
 function renderAddVegetable(rootElement: HTMLElement | null) {
     try {
         const html = `<form onsubmit="handleAddVegetable(event)">
-        <input type="text" name="name" placeholder="Name">
+        <input type="text" name="name" placeholder="Name" required>
         <input type="url" name="image" placeholder="Image url">
-        <input type="number" name="amount" placeholder="Insert amount">
+        <input type="number" name="amount" placeholder="Insert amount" required>
         <input type="submit" value="ADD">
             </form>`
         if (!rootElement) throw new Error("can't find rootElement")
@@ -24,6 +46,9 @@ function renderAddVegetable(rootElement: HTMLElement | null) {
 
 function renderVegetableCard(vegetable: Vegetable) {
     try {
+        if(vegetable.image === ''){
+            vegetable.image = "https://t3.ftcdn.net/jpg/03/11/87/52/360_F_311875255_d57wDCwlZxdtOEwsnmXLHkV1r29i1R2U.jpg"
+        }
         return `<div class="card">
         <img src="${vegetable.image}">
         <h3>${vegetable.name}</h3>
@@ -42,8 +67,8 @@ function renderVegetableCard(vegetable: Vegetable) {
 function renderAllVegetables(vegetables: Vegetable[], rootElement: HTMLElement | null) {
     try {
         if (!rootElement) throw new Error("can't find rootElement")
-        const html =`<div class="cd">
-        ${ vegetables.map(vegetable => renderVegetableCard(vegetable)).join(' ')} </div>`
+        const html = `<div class="cards">
+        ${vegetables.map(vegetable => renderVegetableCard(vegetable)).join(' ')} </div>`
         rootElement.innerHTML = html;
     } catch (error) {
         console.error(error)
@@ -60,13 +85,42 @@ function handleAddVegetable(ev: any) {
         const newVegetable = new Vegetable(name, image, amount)
         vegetables.push(newVegetable)
         console.log(vegetables);
-        ev.target.reset()
         renderAllVegetables(vegetables, document.querySelector("#rootVegetable"))
+        localStorage.setItem("vegetables", JSON.stringify(vegetables))
+        ev.target.reset()
     } catch (error) {
         console.error(error)
 
     }
 }
+function handleRemoveVegetableUnit(vegetableId: string) {
+    try {
+        const vegetable = vegetables.find(vegetable => vegetable.id === vegetableId)
+        if (!vegetable) throw new Error("can't find vegetable")
+        if(vegetable.amount!==0){
+            vegetable.ateOne()
+        }
+        localStorage.setItem("vegetables", JSON.stringify(vegetables))  
+        renderAllVegetables(vegetables, document.querySelector("#rootVegetable"))
 
+    } catch (error) {
+        console.error(error)
+
+    }
+}
+function handleAddVegetableUnit(vegetableId: string) {
+    try {
+        const vegetable = vegetables.find(vegetable => vegetable.id === vegetableId)
+        if (!vegetable) throw new Error("can't find vegetable")
+        
+            vegetable.buyOne()
+        localStorage.setItem("vegetables", JSON.stringify(vegetables))  
+        renderAllVegetables(vegetables, document.querySelector("#rootVegetable"))
+
+    } catch (error) {
+        console.error(error)
+
+    }
+}
 renderAddVegetable(document.querySelector("#main"))
 renderAllVegetables(vegetables, document.querySelector("#rootVegetable"))
