9d550ff42232e5d3038d7fa696bd24bc28e48cf2
ShaharShaul <128305593+ShaharShaul@users.noreply.github.com>
2023-07-05 17:36:33 +0300

    1

--- 04-Typescript/15-Project-desing/04-adding-friends/Home-puzzle/Lee/dist/index.js
+++ 04-Typescript/15-Project-desing/04-adding-friends/Home-puzzle/Lee/dist/index.js
@@ -6,6 +6,7 @@ var Vegetable = /** @class */ (function () {
         this.kind = kind;
         this.image = image;
         this.quantity = quantity;
+        this.isEdit = false;
         if (id) {
             this.id = id;
         }
@@ -13,6 +14,9 @@ var Vegetable = /** @class */ (function () {
             this.id = "id-" + new Date().getTime() + "-" + Math.random();
         }
     }
+    Vegetable.prototype.setEdit = function (set) {
+        this.isEdit = set;
+    };
     return Vegetable;
 }());
 var vegetables = getVegetablesFromStorage();
@@ -56,10 +60,70 @@ function renderAllVegetables(vegetables, htmlElement) {
     try {
         if (!htmlElement)
             throw new Error("no element");
-        var html = vegetables.map(function (vegetable) {
-            return "<p>" + vegetable.kind + "</p><img src=" + vegetable.image + "><p>" + vegetable.quantity + "</p>";
-        }).join(' ');
+        var html = vegetables.map(function (vegetable) { return renderVegCard(vegetable); }).join(' ');
         htmlElement.innerHTML = html;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+function renderVegCard(vegetable) {
+    try {
+        debugger;
+        if (vegetable.isEdit) {
+            return "<form  class=\"card\" onsubmit=\"handleSet(event)\" id=\"" + vegetable.id + "\">\n        <img src=\"" + vegetable.image + "\">\n        <input type=\"text\" name=\"kind\" value=\"" + vegetable.kind + "\">\n        <input type=\"text\" name=\"quantity\" value=\"" + vegetable.quantity + "\">\n        \n        <button onclick=\"handleDeleteVeg('" + vegetable.id + "')\">Delete</button>\n        <input type=\"submit\" value=\"SET\">\n    </form>";
+        }
+        else {
+            return "<form  class=\"card\" id=\"" + vegetable.id + "\">\n            <img src=\"" + vegetable.image + "\">\n            <p>" + vegetable.kind + "</p>\n            <p>" + vegetable.quantity + "</p>\n        \n        <button onclick=\"handleDeleteVeg('" + vegetable.id + "')\">Delete</button>\n        <button onclick=\"handleEdit('" + vegetable.id + "')\">Edit</button>\n       \n    </form>";
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return '';
+    }
+}
+function handleSet(ev) {
+    try {
+        debugger;
+        ev.preventDefault();
+        var quantity = ev.target.quantity.value;
+        var kind = ev.target.kind.value;
+        var vegId_1 = ev.target.id;
+        var vegi = vegetables.find(function (vegi) { return vegi.id === vegId_1; });
+        if (!vegi)
+            throw new Error("couldnt find friend");
+        vegi.kind = kind;
+        vegi.quantity = quantity;
+        vegi.setEdit(false);
+        console.log(vegi);
+        localStorage.setItem("vegetables", JSON.stringify(vegetables));
+        renderAllVegetables(vegetables, document.querySelector("#rootVegetables"));
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+function handleDeleteVeg(vegId) {
+    try {
+        var index = vegetables.findIndex(function (vegetable) { return vegetable.id === vegId; });
+        if (index === -1)
+            throw new Error("Could not find vegetable");
+        vegetables.splice(index, 1);
+        localStorage.setItem("vegetables", JSON.stringify(vegetables));
+        renderAllVegetables(vegetables, document.querySelector("#rootVegetables"));
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+function handleEdit(vegetableId) {
+    try {
+        debugger;
+        var vegetable = vegetables.find(function (vegetable) { return vegetable.id === vegetableId; });
+        if (!vegetable)
+            throw new Error("Could not find vegetable");
+        vegetable.setEdit(true);
+        renderAllVegetables(vegetables, document.querySelector("#rootVegetables"));
     }
     catch (error) {
         console.error(error);
