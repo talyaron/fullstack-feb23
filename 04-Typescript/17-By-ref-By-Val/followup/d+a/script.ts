935a4a29dc70586681606517325bc95b59135972
Doriel Shacham <dorielshacham@gmail.com>
2023-07-05 21:10:07 +0300

    .

--- 04-Typescript/17-By-ref-By-Val/followup/d+a/script.ts
+++ 04-Typescript/17-By-ref-By-Val/followup/d+a/script.ts
@@ -11,16 +11,31 @@ const family: Family[] = [
 
 const fam = family[1];
 const test = fam.lastName = "Bamba"
-console.log(test)
-console.log(family[1])
+// console.log(test)
+// console.log(family[1])
 
 //--- test 2
 
 let a = family[0].lastName
 const b = a
-console.log("this is a " + a)
-console.log("this is b " + b)
+// console.log("this is a " + a)
+// console.log("this is b " + b)
 
-console.log("this is primitive? " + b)
 a = "laki"
-console.log("this is primitive? " + b)
+// console.log("this is primitive? " + b)
+
+//-- test 3 copy obj
+const Person = {
+    name: "Doriel",
+    date: new Date("2023-07-05"),
+    friends: ["Max", "Ran"]
+  }
+  console.log(Person)
+  const clonePerson = structuredClone(Person)
+console.log(clonePerson)
+clonePerson.name = "suzuki"
+console.log(clonePerson.name)
+
+
+
+
