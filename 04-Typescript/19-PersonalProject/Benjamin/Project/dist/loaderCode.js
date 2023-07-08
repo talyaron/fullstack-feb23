27cf3e5d23c50953f33d930627a055238d5d1281
Benjamin Volodarsky <127942178+Benben3135@users.noreply.github.com>
2023-07-08 19:29:01 +0300

    my music app

---
+++ 04-Typescript/19-PersonalProject/Benjamin/Project/dist/loaderCode.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+    var loader = document.querySelector(".loader");
+    if (loader)
+        loader.classList.add("loader-hidden");
+    loader === null || loader === void 0 ? void 0 : loader.addEventListener("transitioned", function () {
+        document.body.removeChild(loader);
+    });
+});
