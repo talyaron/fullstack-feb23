fc8a012aa0e794515ede5620e81217ae4950beca
Eliyaho basson <basson131@gmail.com>
2023-07-09 18:29:52 +0300

    1

--- Projects/Ice_Cream_Truck/OpeningScreen/OpeningScreen.ts
+++ Projects/Ice_Cream_Truck/OpeningScreen/OpeningScreen.ts
@@ -1,32 +1,32 @@
 class Player {
-    id:string
-    constructor (public name:string, public points:number){
+    id: string
+    constructor(public name: string, public points: number) {
     }
 }
 
 
-function renderMain(rootElement:HTMLElement|null){
+function renderMain(rootElement: HTMLElement | null) {
     try {
-        if(!rootElement) throw new Error ("can not find root elemnt")
-        const html =`
-        <h1>ICE CREAM TRUCK</h1>
+        if (!rootElement) throw new Error("can not find root elemnt")
+        const html = `
+       
         <button onclick="handleRenderInstructions()">HOW TO PLAY</button>
         <form onsubmit="handleStartGame(event)">
-        <input type="text" name="name" placeholder="Enter your name">
+        <input type="text" name="name" placeholder="Enter your name" required>
         <input type="submit" value="START">
     </form>
         `
-        rootElement.innerHTML=html
+        rootElement.innerHTML = html
     } catch (error) {
         console.error(error)
     }
 }
 
-function handleRenderInstructions(){
+function handleRenderInstructions() {
     try {
-       const root = document.querySelector("#main")
-       if(!root) throw new Error ("can not find root elemnt")
-        const html =`
+        const root = document.querySelector("#main")
+        if (!root) throw new Error("can not find root elemnt")
+        const html = `
         <h3>
         You have to make ice cream cones for the <br> customers who come to your ice cream truck. <br>
         Use the arrows to select the desired flavor. <br>
@@ -37,15 +37,29 @@ function handleRenderInstructions(){
     <h2>Be careful! </h2>
     <h3>Customers are not so patient.
         Work quickly and accurately! </h3>
-    <button onclick="handleRenderMain">BACK</button>
+    <button onclick="handleRenderMain()">BACK</button>
         `
-        root.innerHTML=html
+        root.innerHTML = html
     } catch (error) {
         console.error(error)
     }
 
 }
-// const player = new Player {}
+
+function handleRenderMain(){
+renderMain(document.querySelector("#main"))
+
+}
+
+function handleStartGame(ev:any){
+ try {
+    ev.preventDefault()
+    const name = ev.target.name.value
+    const player = new Player (name,0)
+ } catch (error) {
+    console.error(error)
+ }
+}
 
 
 renderMain(document.querySelector("#main"))
--- Projects/Ice_Cream_Truck/OpeningScreen/dist/OpeningScreen.js
+++ Projects/Ice_Cream_Truck/OpeningScreen/dist/OpeningScreen.js
@@ -9,7 +9,7 @@ function renderMain(rootElement) {
     try {
         if (!rootElement)
             throw new Error("can not find root elemnt");
-        var html = "\n        <h1>ICE CREAM TRUCK</h1>\n        <button onclick=\"handleRenderInstructions()\">HOW TO PLAY</button>\n        <form onsubmit=\"handleStartGame(event)\">\n        <input type=\"text\" name=\"name\" placeholder=\"Enter your name\">\n        <input type=\"submit\" value=\"START\">\n    </form>\n        ";
+        var html = "\n       \n        <button onclick=\"handleRenderInstructions()\">HOW TO PLAY</button>\n        <form onsubmit=\"handleStartGame(event)\">\n        <input type=\"text\" name=\"name\" placeholder=\"Enter your name\" required>\n        <input type=\"submit\" value=\"START\">\n    </form>\n        ";
         rootElement.innerHTML = html;
     }
     catch (error) {
@@ -21,12 +21,24 @@ function handleRenderInstructions() {
         var root = document.querySelector("#main");
         if (!root)
             throw new Error("can not find root elemnt");
-        var html = "\n        <h3>\n        You have to make ice cream cones for the <br> customers who come to your ice cream truck. <br>\n        Use the arrows to select the desired flavor. <br>\n        Press enter to add an ice cream scoop to the cone. <br>\n        To go to customer selection press the space bar, <br> select the appropriate customer <br>\n        and press enter to deliver the ice cream. <br>\n    </h3>\n    <h2>Be careful! </h2>\n    <h3>Customers are not so patient.\n        Work quickly and accurately! </h3>\n    <button onclick=\"handleRenderMain\">BACK</button>\n        ";
+        var html = "\n        <h3>\n        You have to make ice cream cones for the <br> customers who come to your ice cream truck. <br>\n        Use the arrows to select the desired flavor. <br>\n        Press enter to add an ice cream scoop to the cone. <br>\n        To go to customer selection press the space bar, <br> select the appropriate customer <br>\n        and press enter to deliver the ice cream. <br>\n    </h3>\n    <h2>Be careful! </h2>\n    <h3>Customers are not so patient.\n        Work quickly and accurately! </h3>\n    <button onclick=\"handleRenderMain()\">BACK</button>\n        ";
         root.innerHTML = html;
     }
     catch (error) {
         console.error(error);
     }
 }
-// const player = new Player {}
+function handleRenderMain() {
+    renderMain(document.querySelector("#main"));
+}
+function handleStartGame(ev) {
+    try {
+        ev.preventDefault();
+        var name = ev.target.name.value;
+        var player = new Player(name, 0);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
 renderMain(document.querySelector("#main"));
