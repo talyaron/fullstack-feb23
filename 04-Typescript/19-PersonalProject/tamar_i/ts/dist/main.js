1e8fc5c0f1b5392b28d7d582737e6b8ed761e74b
itahTamar <125893355+itahTamar@users.noreply.github.com>
2023-07-10 12:26:45 +0300

    start login page&stayl&form(not finish)

--- 04-Typescript/19-PersonalProject/tamar_i/ts/dist/main.js
+++ 04-Typescript/19-PersonalProject/tamar_i/ts/dist/main.js
@@ -1,10 +1,25 @@
-"use strict";
-exports.__esModule = true;
-require("../ts/dist/red_ball");
-require("../ts/dist/bricks");
-require("../ts/dist/errow");
-require("../ts/dist/bombs");
-require("../ts/dist/shelves");
-require("../ts/dist/coins");
-require("../ts/dist/color_balls");
-//login
+// import ts files -> https://www.educative.io/answers/how-to-import-another-typescript-file
+//import redBall from '../ts/dist/red_ball';
+//import '../ts/dist/bricks';
+//import '../ts/dist/errow';
+//import '../ts/dist/bombs';
+//import '../ts/dist/shelves';
+//import '../ts/dist/coins';
+//import '../ts/dist/color_balls';
+//login form
+var Users = /** @class */ (function () {
+    function Users(userName) {
+        this.userName = userName;
+        this.id = Math.floor(Math.random() * 10000);
+    }
+    return Users;
+}());
+function handelSubmit(ev) {
+    try {
+        // ev.preventDefault();
+        console.dir(ev);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
--- 04-Typescript/19-PersonalProject/tamar_i/ts/main.ts
+++ 04-Typescript/19-PersonalProject/tamar_i/ts/main.ts
@@ -1,12 +1,31 @@
-@import '../ts/dist/red_ball';
-@import '../ts/dist/bricks';
-@import '../ts/dist/errow';
-@import '../ts/dist/bombs';
-@import '../ts/dist/shelves';
-@import '../ts/dist/coins';
-@import '../ts/dist/color_balls';
+// import ts files -> https://www.educative.io/answers/how-to-import-another-typescript-file
+//import redBall from '../ts/dist/red_ball';
+//import '../ts/dist/bricks';
+//import '../ts/dist/errow';
+//import '../ts/dist/bombs';
+//import '../ts/dist/shelves';
+//import '../ts/dist/coins';
+//import '../ts/dist/color_balls';
 
 
-//login
+//login form
+
+class Users {
+    id: number;
+    constructor(public userName: string){
+        this.id = Math.floor(Math.random()*10000);
+    }
+
+
+}
+
+function handelSubmit(ev: string) {
+    try {
+       // ev.preventDefault();
+        console.dir(ev);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 
