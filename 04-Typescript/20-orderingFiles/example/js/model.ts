63430738c0f860b22b765b4f28e4357fa821f6ed
Tal Yaron <tal.yaron@gmail.com>
2023-07-10 10:13:05 +0300

    cont

---
+++ 04-Typescript/20-orderingFiles/example/js/model.ts
@@ -0,0 +1,12 @@
+//classes
+
+class User{
+    name: string;
+    constructor(name: string){
+        this.name = name;
+    }
+}
+const users: User[] = [
+    new User('Fran'),
+    new User('Pepito'),
+];
