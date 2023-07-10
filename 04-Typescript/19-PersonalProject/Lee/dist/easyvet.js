ef52d22eca3dd13cf5bc680a868dc477436f46d9
Lee_D <leegreat21@gmail.com>
2023-07-10 20:23:40 +0300

    easyVet model

--- 04-Typescript/19-PersonalProject/Lee/dist/easyvet.js
+++ 04-Typescript/19-PersonalProject/Lee/dist/easyvet.js
@@ -1,29 +1,60 @@
-// Clients []
-// constructor: firstName, lastName, owner, image, yearOfBirth, id
-// exapmle:
-// - Nella Dekel 
-// - Oscar Dekel
-// - Hugo Ortman
-// - Dubi Ortman
-// - Arnav Ortman
-// - Cocus Ortman
-// - Jango Rechtman
-// Vaccines []
-// constructor: vaccine, id
-// exapmle: 
-// - Rabies
-// - FVRCP
-// - FHV,FHV-1
-// - Parvovirus
-// - Leptospirosis
-// - Distemper
-// - Canine Infectious Hepatitis
-// //TreatmentsAndSupplies []
-// //constructor: kind, id
-// exapmles: 
-// - Hospitalization per night
-// - Haircut 
-// - teeth cleaning
-// - Medical food conserves
-// - x-ray
-// - US
+var Client = /** @class */ (function () {
+    function Client(name, owner, image, birthYear) {
+        this.name = name;
+        this.owner = owner;
+        this.image = image;
+        this.birthYear = birthYear;
+        this.id = uid();
+    }
+    return Client;
+}());
+var clients = [
+    new Client("Nella", "Judith Dekel", "https://www.instagram.com/p/CX-9-hdIUFd/?img_index=1", 2021),
+    new Client("Jango", "Linor Monir", "https://media.istockphoto.com/id/1259203799/photo/portrait-of-a-young-happy-belgian-shepherd-dog-malinois-posing-outdoors.jpg?s=612x612&w=0&k=20&c=yxDw11q_2NAgrUdm0AuBkqY1UnY9MKmtiaoeWOgHNRg=", 2022)
+];
+var Vaccine = /** @class */ (function () {
+    function Vaccine(vacName, price) {
+        this.vacName = vacName;
+        this.price = price;
+        this.id = uid();
+    }
+    return Vaccine;
+}());
+var Vaccines = [
+    new Vaccine("Rabies", 100),
+    new Vaccine("FVRCP", 80),
+    new Vaccine("FHV", 60),
+    new Vaccine("FHV-1", 70),
+    new Vaccine("Parvovirus", 120),
+    new Vaccine("Leptospirosis", 50),
+    new Vaccine("Distemper", 200),
+    new Vaccine("Canine Infectious Hepatitis", 130)
+];
+var Treatment = /** @class */ (function () {
+    function Treatment(treatName, price) {
+        this.treatName = treatName;
+        this.price = price;
+        this.id = uid();
+    }
+    return Treatment;
+}());
+var treatments = [
+    new Treatment("Haircut", 200),
+    new Treatment("Hospitalization per night", 450),
+    new Treatment("teeth cleaning", 100),
+    new Treatment("x-ray", 100),
+    new Treatment("US", 100),
+];
+var Supply = /** @class */ (function () {
+    function Supply(supName, price) {
+        this.supName = supName;
+        this.price = price;
+        this.id = uid();
+    }
+    return Supply;
+}());
+var supplies = [
+    new Supply("Medical food 1 can", 17),
+    new Supply("Ampoule Anti-flea & Tick", 47),
+    new Supply("seringe", 10)
+];
