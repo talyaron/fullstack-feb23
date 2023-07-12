13137371630b855392d48abf3170c3110cd51744
tomsobia <tom162sobia@gmail.com>
2023-07-12 13:27:50 +0300

    update

--- 04-Typescript/19-PersonalProject/tomsobia/PRG-2/pro/store/dist/store.js
+++ 04-Typescript/19-PersonalProject/tomsobia/PRG-2/pro/store/dist/store.js
@@ -1,6 +1,18 @@
+var battle = /** @class */ (function () {
+    function battle(name, pass, email, number, public, id) {
+        this.name = name;
+        this.pass = pass;
+        this.email = email;
+        this.number = number;
+        id = id || "id-" + new Date().getTime() + "-" + Math.random();
+    }
+    return battle;
+}());
 var root = document.querySelector("#root");
 var Detailsstring = localStorage.getItem("user");
-var details = Detailsstring ? JSON.parse(Detailsstring) : { name: "" };
+var details = Detailsstring
+    ? JSON.parse(Detailsstring)
+    : { name: "" };
 if (root) {
     root.innerHTML = "Hello " + details.name;
 }
--- 04-Typescript/19-PersonalProject/tomsobia/PRG-2/pro/store/store.ts
+++ 04-Typescript/19-PersonalProject/tomsobia/PRG-2/pro/store/store.ts
@@ -1,7 +1,23 @@
+class battle {
+  id: string;
+  constructor(
+    public name,
+    public pass,
+    public email,
+    public number,
+    public ?id
+  ) {
+    id = id || `id-${new Date().getTime()}-${Math.random()}`;
+  }
+}
+
+
 
 const root: HTMLElement | null = document.querySelector("#root");
 const Detailsstring: string | null = localStorage.getItem("user");
-const details: { name: string } = Detailsstring ? JSON.parse(Detailsstring) : { name: "" };
+const details: { name: string } = Detailsstring
+  ? JSON.parse(Detailsstring)
+  : { name: "" };
 if (root) {
   root.innerHTML = `Hello ${details.name}`;
 }
