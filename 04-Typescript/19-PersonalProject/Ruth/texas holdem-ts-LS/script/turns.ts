27d061821984dff5a1e0d524682f1eb4bad0841c
ruttur66 <112100490+RuthBenTov@users.noreply.github.com>
2023-07-13 23:52:12 +0300

    ruth

--- 04-Typescript/19-PersonalProject/Ruth/texas holdem-ts-LS/script/turns.ts
+++ 04-Typescript/19-PersonalProject/Ruth/texas holdem-ts-LS/script/turns.ts
@@ -10,30 +10,28 @@ function performTurn(
   stage: HTMLElement,
   currentPlayerIndex: number,
 ) {
-    try{
-        const currentPlayer = players[currentPlayerIndex];
-        let activePlayers = players.filter((p) => p.isActive === true);
-      
-        activePlayers.map((p) => (p.isTurn = false));
-        currentPlayer.setActive();
-        currentPlayer.isTurn = true
-        currentPlayer.doingTurn(activePlayers, currentPlayerIndex);
-      
-        currentPlayerIndex++;
-      
-        if (currentPlayerIndex >= players.length && stage.children.length < 6) {
-          currentPlayerIndex = 0;
-          if (stage.children.length < 6) {
-            addCardToStage();
-          }
-        }
-      
-        setTimeout(() => performTurn(players, stage, currentPlayerIndex), 500);
+  try {
+    const currentPlayer = players[currentPlayerIndex];
+    let activePlayers = players.filter((p) => p.isActive === true);
 
+    activePlayers.map((p) => (p.isTurn = false));
+    currentPlayer.setActive();
+    currentPlayer.isTurn = true;
+    currentPlayer.doingTurn(activePlayers, currentPlayerIndex);
+
+    currentPlayerIndex++;
+
+    if (currentPlayerIndex >= players.length && stage.children.length < 6) {
+      currentPlayerIndex = 0;
+      if (stage.children.length < 6) {
+        addCardToStage();
+      }
     }
-    catch(error){
-        console.error(error)
-    }
+
+    setTimeout(() => performTurn(players, stage, currentPlayerIndex), 500);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // turnOrder(players);
@@ -57,8 +55,91 @@ function getMoveOption(activePlayers: Player[], thisIndex: number) {
   }
 }
 
-// function getChanceToBet(thisPlayer: Player) {
-//     const thisPCards = thisPlayer.allCards
-//     checkHighCard()
-//     checkFullHouse()checkTwoPairs()checkFourOfAKind()checkThreeOfAKind()checkTwoOfAKind()
-// }
+function getChanceToBet(thisPlayer: Player) {
+  const thisPCards = thisPlayer.allCards;
+  const setsResult: boolean[] = [
+    checkPair(thisPCards)!,
+    checkTwoPairs(thisPCards)!,
+    checkThreeOfAKind(thisPCards)!,
+    checkStraight(thisPCards)!,
+    checkFlush(thisPCards)!,
+    checkFullHouse(thisPCards)!,
+    checkFourOfAKind(thisPCards)!,
+    checkStraightFlush(thisPCards)!,
+    checkRoyalFlush(thisPCards)!,
+  ];
+  console.log(setsResult);
+
+  let maxPointsSet: number = 0;
+  setsResult.forEach((res, i) => {
+    if (res === true) maxPointsSet += i + 1;
+  });
+  return maxPointsSet;
+}
+
+function riseMove(players: Player[], currentPlayerIndex: number) {
+  const currentPlayer = players[currentPlayerIndex];
+  currentPlayer.movesInRound.push(PlayerMovesOption.rise);
+
+  localStorage.setItem("players", JSON.stringify(players));
+  //התור יעבור לבא אחריו
+}
+
+function callMove(players: Player[], currentPlayerIndex: number) {
+  if (currentPlayerIndex != 0 && players[currentPlayerIndex - 1].lastBet > 0) {
+    riseBetSizeInThisRound( players, currentPlayerIndex) > 0;
+
+    const currentPlayer = players[currentPlayerIndex];
+    currentPlayer.movesInRound.push(PlayerMovesOption.call);
+
+    const betToCall = players[currentPlayerIndex - 1].lastBet;
+    currentPlayer.lastBet = betToCall;
+
+    localStorage.setItem("players", JSON.stringify(players));
+  }
+  //התור יעבור לבא אחריו
+}
+
+function foldMove(players: Player[], currentPlayerIndex: number) {
+  const currentPlayer = players[currentPlayerIndex];
+  currentPlayer.movesInRound.push(PlayerMovesOption.fold);
+  currentPlayer.lastBet = 0;
+  currentPlayer.isActive = false;
+
+  localStorage.setItem("players", JSON.stringify(players));
+  //התור יעבור לבא אחריו
+}
+
+function checkMove(players: Player[], currentPlayerIndex: number) {
+  const currentPlayer = players[currentPlayerIndex];
+  if (
+    currentPlayerIndex == 0 ||
+    players[currentPlayerIndex - 1].movesInRound[length - 1] ==
+      PlayerMovesOption.check
+  ) {
+    currentPlayer.movesInRound.push(PlayerMovesOption.check);
+    currentPlayer.lastBet = 0;
+  }
+  localStorage.setItem("players", JSON.stringify(players));
+  //התור יעבור לבא אחריו
+}
+
+function riseBetSizeInThisRound(players: Player[], currentPlayerIndex: number) {
+ const PlayerRiseInThisRound:Player|undefined = getLastRisePLayer(players, currentPlayerIndex)
+ if(PlayerRiseInThisRound){
+   return PlayerRiseInThisRound.lastBet
+ }
+ return 0
+}
+
+function getLastRisePLayer(players: Player[], currentPlayerIndex: number) {
+  const currentPlayer = players[currentPlayerIndex];
+  const round = currentPlayer.movesInRound.length;
+  let playerRiseLastInThisRound 
+ players.forEach((p) => {
+    if (p.movesInRound[round - 1] == PlayerMovesOption.rise) {
+      playerRiseLastInThisRound = p;
+    }
+  });
+  return playerRiseLastInThisRound;
+}
