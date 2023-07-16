5c53bef0081fff0a90e4c205c1c0d4d437811ab2
Bar <beerya.yaron@gmail.com>
2023-07-16 15:50:59 +0300

    hp

--- 04-Typescript/19-PersonalProject/Bar/profile/dist/profile.js
+++ 04-Typescript/19-PersonalProject/Bar/profile/dist/profile.js
@@ -11,9 +11,9 @@ function showPosts(HTMLElement, users) {
             throw new Error('Root element is not found');
         var html = users
             .map(function (user) {
-            var postsHtml = user.images
+            var postsHtml = user.imagse
                 .map(function (img) {
-                return "\n              <div class=\"userPost\">\n                <div class=\"userPost__name\">\n                  <img src=\"" + user.imageProfile + "\">\n                  <h3>" + user.name + "</h3>\n                </div>\n                <div class=\"userPost__img\">\n                  <img src=\"" + img.image + "\">\n                </div>\n                <div class=\"userPost__buttons\">\n                <button class=\"deleteBtn\" onclick=\"handleDeletePost('" + user.id + "')\">Delete</button>\n                <button class=\"editBtn\" onclick=\"handleEditPost('" + user.id + "')\">Edit</button>\n              </div>\n              </div>";
+                return "\n              <div class=\"userPost\">\n                <div class=\"userPost__name\">\n                  <img src=\"" + user.imageProfile + "\">\n                  <h3>" + user.name + "</h3>\n                </div>\n                <div class=\"userPost__img\">\n                  <img src=\"" + img.image + "\">\n                </div>\n                <div class=\"userPost__buttons\">\n                <button class=\"deleteBtn\" onclick=\"handleDeletePost('" + img.id + "')\">Delete</button>\n                <button class=\"editBtn\" onclick=\"handleEditPost('" + img.id + "')\">Edit</button>\n              </div>\n              </div>";
             })
                 .join('');
             return postsHtml;
@@ -45,31 +45,45 @@ function showHeader(HTMLElement, user) {
     }
 }
 showHeader(document.querySelector('#header'), usersArray);
-// function handleDeletePost(userId: string, postId: string) {
-//   try {
-//     const user = usersArray.find((user) => user.id === userId);
-//     if (!user) throw new Error('User not found');
-//     const postIndex = user.images.findIndex((img) => img.id === postId);
-//     if (postIndex === -1) throw new Error('Post not found');
-//     user.images.splice(postIndex, 1);
-//     saveUserToLocalStorage(usersArray);
-//     showPosts(document.querySelector('#posts'), usersArray);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-// function handleEditPost(userId: string, postId: string) {
-//   try {
-//     const user = usersArray.find((user) => user.id === userId);
-//     if (!user) throw new Error('User not found');
-//     const post = user.images.find((img) => img.id === postId);
-//     if (!post) throw new Error('Post not found');
-//     const newImage = prompt('Enter new image URL', post.image);
-//     if (!newImage) return;
-//     post.image = newImage;
-//     saveUserToLocalStorage(usersArray);
-//     showPosts(document.querySelector('#posts'), usersArray);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+//delete post.
+function handleDeletePost(imageId) {
+    try {
+        var user_1 = usersArray.find(function (user) { return user.imagse.some(function (img) { return img.id === imageId; }); });
+        if (!user_1)
+            throw new Error('User or image not found');
+        var imageIndex = user_1.imagse.findIndex(function (img) { return img.id === imageId; });
+        if (imageIndex === -1)
+            throw new Error('Image not found');
+        user_1.imagse.splice(imageIndex, 1);
+        saveImgToLocalStorage(imagesArray);
+        saveUserToLocalStorage(usersArray);
+        saveUsersImgToLocalStorage(usersImgArray);
+        showPosts(document.querySelector('#posts'), usersArray);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+// //edit post.
+//edit post.
+function handleEditPost(imageId) {
+    try {
+        var user_2 = usersArray.find(function (user) { return user.imagse.some(function (img) { return img.id === imageId; }); });
+        if (!user_2)
+            throw new Error('User or image not found');
+        var image_1 = user_2.imagse.find(function (img) { return img.id === imageId; });
+        if (!image_1)
+            throw new Error('Image not found');
+        var newImageURL = prompt('Enter new image URL:');
+        if (newImageURL) {
+            image_1.image = newImageURL;
+            saveImgToLocalStorage(imagesArray);
+            saveUserToLocalStorage(usersArray);
+            saveUsersImgToLocalStorage(usersImgArray);
+            showPosts(document.querySelector('#posts'), usersArray);
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
