02ba7d95aac17b191bef9bd2d07b2d21d9e87e84
ziv leloneck <119840698+zivll1991@users.noreply.github.com>
2023-07-17 23:45:31 +0300

    ver1524

--- 04-Typescript/19-PersonalProject/zivll/js/script.ts
+++ 04-Typescript/19-PersonalProject/zivll/js/script.ts
@@ -1,5 +1,13 @@
 // model
 
+// this class used to create and define expence entitie
+class Expence {
+  constructor(
+    public name: string,
+    public category: string,
+    public amount: number
+  ) {}
+}
 // these class is used to devide all expences into categories
 class ExpenceCategory {
   categoryName: string;
@@ -12,16 +20,6 @@ class ExpenceCategory {
     Math.round(Math.random() * 1000000);
   }
 }
-
-class Expence {
-  // id: number;
-  constructor(
-    public name: string,
-    public category: string,
-    public amount: number // id: number, // public expiration?: string
-  ) {}
-}
-
 // these array contains all categories subjects
 const expenceCategorys: ExpenceCategory[] = [
   new ExpenceCategory(`תקשורת`),
@@ -36,25 +34,62 @@ const expenceCategorys: ExpenceCategory[] = [
 ];
 
 // these array contains all user expences
-const expences: Expence[] = [
-  new Expence(`טלפון `, `תקשורת`, 1000),
-  new Expence(`הלוואת רכב `, `הלוואות`, 980),
-  new Expence(`טלוויזיה `, `תקשורת`, 980),
-];
+const expences: Expence[] = [];
 
-// this function is used to render the income calculator
-// function renderIncomeCalculator(htmlElemnet: HTMLElement) {
-//   try {
-//   } catch (error) {
-//     console.error(error);
-//   }
-//   // const html=
-// }
+// controllers
+
+// this function is used to handle accordion click
+function handleAccordionClick() {
+  const accordion = document.querySelectorAll(`.thead`);
+
+  // this method will take care the accordion functionality
+  accordion.forEach((head) => {
+    // first we handling the fold and unfold functions
+    head.addEventListener(`click`, (ev) => {
+      head.classList.toggle(`active`);
+      const svgRoot = head.childNodes[1];
+      const createSvgDiv = document.createElement(`div`);
+      if (!head.parentElement?.nextElementSibling)
+        throw new Error(`next sibling not found`);
+      const nextElementSibling = head.parentElement.nextElementSibling;
+      if (!nextElementSibling) throw new Error(`nextElementSibling not found`);
+      if (nextElementSibling.classList.contains(`off`)) {
+        nextElementSibling.classList.remove(`off`);
+
+        // second we handling the svg replacing (plus, minus)
+        createSvgDiv.innerHTML = `<svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="collapse-icon"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M18 12H6"
+      />
+    </svg>`;
+        svgRoot.replaceChild(createSvgDiv, svgRoot.childNodes[1]);
+      } else {
+        nextElementSibling.classList.add(`off`);
+        createSvgDiv.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="collapse-icon">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" />
+    </svg>`;
+        svgRoot.replaceChild(createSvgDiv, svgRoot.childNodes[1]);
+      }
+    });
+  });
+}
 
 // this function is used to render the expenses calculator
 function renderExpenceCalculator() {
   const categoriesHtml = expenceCategorys
-    .map((category) => `<option value="">${category.categoryName}</option>`)
+    .map(
+      (category) =>
+        `<option value="${category.categoryName}">${category.categoryName}</option>`
+    )
     .join(` `);
   const expencesRoots = document.querySelector(`#expences-calculators`);
   if (!expencesRoots) throw new Error(`expencesRoots not found`);
@@ -62,45 +97,36 @@ function renderExpenceCalculator() {
   <div class="user-box">
     <input
       type="text"
-      name="expence-description"
-      id="expence-description"
+      name="expenceDescription"
+      id="expenceDescription" required
     />
-    <label for="expence-description">שם ההוצאה:</label>
+    <label for="expenceDescription">שם ההוצאה:</label>
   </div>
   <div class="user-box">
     <label for="categories"
       >מה הקטגוריה המתאימה עבור ההוצאה שלך?</label
     >
     <select name="categories" id="categories">
+    <option value="" disabled selected>בחר/י את הקטגוריה</option>
     ${categoriesHtml}
     </select>
   </div>
   <div class="user-box">
     <input
       type="number"
-      name="expence-amount"
-      id="expence-calculators"
+      name="expenceAmount"
+      id="expenceAmount" required
     />
-    <label for="expence-amount">מה סכום ההוצאה שלך?</label>
+    <label for="expenceAmount">מה סכום ההוצאה שלך?</label>
   </div>
-  <div class="user-box">
-    <input
-      type="date"
-      name="expiration"
-      id="expiration"
-      class="date"
-    />
-    <label for="expiration"
-      >מהו תוקף ההוצאה שלך? (במידה ורלוונטי)</label
-    >
-  </div>
+  
   <input type="submit" value="הוסף" />`;
   try {
   } catch (error) {
     console.error(error);
   }
 }
-
+// this function is used to render the expences table
 function renderExpencesTable(expences: Expence[]) {
   try {
     const sortedExpences = sortByCategory(expences);
@@ -115,9 +141,13 @@ function renderExpencesTable(expences: Expence[]) {
         userCategories.push(expense.category);
       }
     });
-
-    if (sortedExpences === null || sortedExpences.length === 0)
-      rootExpencesTable.innerHTML = `<h2>על מנת לצפות בטבלת ההוצאות יש להזין הוצאות בטבלה שמצד שמאל</h2>`;
+    if (
+      sortedExpences === null ||
+      sortedExpences.length === 0 ||
+      expences.length === 0 ||
+      expences === null
+    )
+      return (rootExpencesTable.innerHTML = `<h2>על מנת לצפות בטבלת ההוצאות יש להזין הוצאות בטבלה שמצד שמאל</h2>`);
     const htmlHeadersTable = userCategories
       .map(
         (category) =>
@@ -146,8 +176,6 @@ function renderExpencesTable(expences: Expence[]) {
     userCategories.forEach((category) => {
       const html = document.querySelector(`#${category}`);
       const htmlCreateTr = document.createElement(`tr`);
-      console.log(htmlCreateTr);
-
       if (!html) throw new Error(`html not found`);
       expences.forEach((expence) => {
         if (expence.category === category) {
@@ -159,35 +187,66 @@ function renderExpencesTable(expences: Expence[]) {
     console.error(error);
   }
 }
-function renderResult(htmlElemnet: Element, income?: string) {
+// this function is used to render the result area
+function renderResult(
+  htmlElemnet: Element,
+  income?: string | undefined | null,
+  expense?: number
+) {
   try {
-    htmlElemnet.innerHTML = `${income}&#8362;`;
+    if (income !== null || undefined)
+      htmlElemnet.innerHTML = `${income}&#8362;`;
+    if (expense) htmlElemnet.innerHTML = `${expense}&#8362;`;
+    if (!htmlElemnet) throw new Error(`htmlElemnet not found`);
+    calculateBalance();
   } catch (error) {
     console.error(error);
   }
 }
 
+// this function handling the expence submit
 function handleExpenceSubmit(ev) {
-  ev.preventDefault();
-  ev.reset();
   try {
+    ev.preventDefault();
+    const resultRoot = document.querySelector(`.total-number--expence`);
+    if (!resultRoot) throw new Error(`resultRoot not found`);
+    const expenceDescription = ev.target.expenceDescription.value;
+    const categories = ev.target.categories.value;
+    const expenceAmount = ev.target.expenceAmount.valueAsNumber;
+
+    expences.push(new Expence(expenceDescription, categories, expenceAmount));
+    renderExpencesTable(expences);
+    loadDataToLocalStorage(expences, `expences`);
+    loadDataToLocalStorage(expenceAmount, `expenceAmount`);
+    handleAccordionClick();
+    const allExpences = calculateTotalExpence(expences);
+    renderResult(resultRoot, null, allExpences);
+
+    // loadDataToLocalStorage()
+    ev.target.reset();
     console.log(ev);
   } catch (error) {
     console.error(error);
   }
 }
+// this funcion calculates the total expences
+function calculateTotalExpence(expences) {
+  let totalExpence = 0;
+  expences.forEach((expense) => {
+    totalExpence += expense.amount;
+  });
+  return totalExpence;
+}
+// this function handling the income submit
 function handleIncomeSubmit(ev) {
   try {
     ev.preventDefault();
     if (!ev || !ev.target) throw new Error(`event not found`);
     const income = ev.target.income.value;
     const resultRoot = document.querySelector(`.total-number--income`);
-    console.dir(resultRoot);
     if (!resultRoot) throw new Error(`resultRoot not found`);
-
     renderResult(resultRoot, income);
     loadDataToLocalStorage(income, `income`);
-
     calculateBalance();
     ev.target.reset();
   } catch (error) {
@@ -216,12 +275,12 @@ function calculateBalance() {
       balanceRoot.classList.remove(`total-number--income`);
       balanceRoot.classList.add(`total-number--expence`);
     }
-    loadDataToLocalStorage(incomeRoot, `income`);
+    // loadDataToLocalStorage(incomeRoot, `income`);
   } catch (error) {
     console.error(error);
   }
 }
-
+// this function sort the expences by category alfabetically
 function sortByCategory(expences: Expence[]) {
   try {
     const sortedExpences = expences.sort((a, b) => {
@@ -250,59 +309,34 @@ function loadPage() {
     const incomeRoot = document.querySelector(`.total-number--income`);
     const expenceRoot = document.querySelector(`.total-number--expence`);
     const balanceRoot = document.querySelector(`.total-number--balance`);
+    const expencesStringFromLocalStorage = localStorage.getItem(`expences`);
+    if (expencesStringFromLocalStorage === null)
+      throw new Error(`expences not found`);
+    const expencesFromLocalStorage: Expence[] = JSON.parse(
+      expencesStringFromLocalStorage
+    );
     if (!incomeRoot || !expenceRoot || !balanceRoot)
       throw new Error(`roots is missing`);
     const incomeFromLocalStorage = localStorage.getItem(`income`);
-    if (incomeFromLocalStorage)
-      incomeRoot.innerHTML = `${incomeFromLocalStorage}`;
-    balanceRoot.innerHTML = `${
-      parseFloat(incomeRoot.innerHTML) - parseFloat(expenceRoot.innerHTML)
-    }&#8362;`;
-    if (
-      parseFloat(incomeRoot.innerHTML) - parseFloat(expenceRoot.innerHTML) >
-      0
-    ) {
-      balanceRoot.classList.remove(`total-number--expence`);
-      balanceRoot.classList.add(`total-number--income`);
-    } else {
-      balanceRoot.classList.remove(`total-number--income`);
-      balanceRoot.classList.add(`total-number--expence`);
-    }
+    expences.push(...expencesFromLocalStorage);
+    const expenceAmountFromLocalStorage = localStorage.getItem(`expenceAmount`);
+    if (incomeFromLocalStorage != null)
+      incomeRoot.innerHTML = `${incomeFromLocalStorage}&#8362;`;
+    calculateBalance();
+    if (expenceAmountFromLocalStorage)
+      expenceRoot.innerHTML = `${expenceAmountFromLocalStorage}`;
+    calculateBalance();
+    if (expenceAmountFromLocalStorage) calculateBalance();
+    renderExpencesTable(expences);
   } catch (error) {
     console.error(error);
   }
 }
 // revoke function when loading page
 loadPage();
-
 // revoke expence calculator when loading page
 renderExpenceCalculator();
 // revoke expences table (if such) when loading or refreshing page
 renderExpencesTable(expences);
-
-const accordion = document.querySelectorAll(`.thead`);
-
-accordion.forEach((head) => {
-  head.addEventListener(`click`, (ev) => {
-    head.classList.toggle(`active`);
-    console.dir(head);
-    const nextElementSibling = head.parentElement?.nextElementSibling;
-    if (!nextElementSibling) throw new Error(`nextElementSibling not found`);
-    if (nextElementSibling.classList.contains(`off`)) {
-      nextElementSibling.classList.remove(`off`);
-    } else {
-      nextElementSibling.classList.add(`off`);
-      debugger;
-      const svg = head.childNodes[1];
-      const newSvg = document.createElement(`svg`);
-      const newSvgString =
-        document.createTextNode(`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" />
-    </svg>`);
-      newSvg.appendChild(newSvgString);
-      svg.replaceChild(newSvg, svg.childNodes[1]);
-
-      // console.dir(head.childNodes[1].childNodes[1]);
-    }
-  });
-});
+// revoke accordion functionality when loading or refreshing page
+handleAccordionClick();
