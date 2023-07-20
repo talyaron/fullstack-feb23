ae3b150f2761255d45b4fd116b2e55212867934a
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2023-07-20 18:48:31 +0300

    .

--- 04-Typescript/project_room3/dist/type.js
+++ 04-Typescript/project_room3/dist/type.js
@@ -11,5 +11,29 @@ var firstCard = false;
 var secondCard = false;
 //Items array
 var items = [
-    { name: "", image: (. / ) }
+    { name: "java", image: "java.png" },
+    { name: "c++", image: "C++.png" },
+    { name: "css", image: "css.webp" },
+    { name: "html", image: "html.png" },
+    { name: "js", image: "js.jpeg" },
+    { name: "python", image: "Python.png" },
+    { name: "react", image: "React.png" },
+    { name: "git", image: "git.png" },
 ];
+//Time
+var seconds = 0;
+var minutes = 0;
+//moves and win count
+var moveCount = 0;
+var winCount = 0;
+//Timer
+function Timer() {
+    seconds += 1;
+    if (seconds = 60) {
+        minutes += 1;
+        seconds = 0;
+    }
+}
+var secondsValue = seconds < 10 ? "0" + seconds : seconds;
+var minutesValue = seconds < 10 ? "0" + minutes : minutes;
+timeValue === null || timeValue === void 0 ? void 0 : timeValue.innerHTML = "<span>Time";
--- 04-Typescript/project_room3/type.ts
+++ 04-Typescript/project_room3/type.ts
@@ -1,10 +1,10 @@
-const moves= document.querySelector("#moves-count")
-const timeValue= document.querySelector("#time")
-const startButton= document.querySelector("#start")
-const stopButton= document.querySelector("#stop")
-const gameContainer= document.querySelector(".game-container")
-const result= document.querySelector("#result")
-const controls= document.querySelector(".controls-container")
+const moves= document.querySelector("#moves-count");
+const timeValue= document.querySelector("#time");
+const startButton= document.querySelector("#start");
+const stopButton= document.querySelector("#stop");
+const gameContainer= document.querySelector(".game-container");
+const result= document.querySelector("#result");
+const controls= document.querySelector(".controls-container");
 let cards;
 let interval;
 let firstCard= false;
@@ -13,5 +13,37 @@ let secondCard= false;
 
 //Items array
 const items= [
-    {name:"",image:(./)}
+    {name:"java",image:"java.png"},
+    {name:"c++",image:"C++.png"},
+    {name:"css",image:"css.webp"},
+    {name:"html",image:"html.png"},
+    {name:"js",image:"js.jpeg"},
+    {name:"python",image:"Python.png"},
+    {name:"react",image:"React.png"},
+    {name:"git",image:"git.png"},
 ]
+
+//Time
+let seconds= 0;
+let minutes= 0;
+
+//moves and win count
+let moveCount= 0;
+let winCount= 0;
+
+//Timer
+function Timer(){
+    seconds +=1;
+    if(seconds=60){
+        minutes += 1;
+        seconds= 0;
+    }
+}
+
+let secondsValue= seconds<10? `0${seconds}`: seconds;
+let minutesValue= seconds<10? `0${minutes}`: minutes;
+timeValue?.innerHTML= `<span>Time`
+
+
+
+
