4a38deee0a70043220b3c8f8a76746a2fc55d9d6
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2023-07-20 18:05:34 +0300

    .

---
+++ 04-Typescript/project_room3/dist/type.js
@@ -0,0 +1,15 @@
+var moves = document.querySelector("#moves-count");
+var timeValue = document.querySelector("#time");
+var startButton = document.querySelector("#start");
+var stopButton = document.querySelector("#stop");
+var gameContainer = document.querySelector(".game-container");
+var result = document.querySelector("#result");
+var controls = document.querySelector(".controls-container");
+var cards;
+var interval;
+var firstCard = false;
+var secondCard = false;
+//Items array
+var items = [
+    { name: "", image: (. / ) }
+];
---
+++ 04-Typescript/project_room3/type.ts
@@ -0,0 +1,17 @@
+const moves= document.querySelector("#moves-count")
+const timeValue= document.querySelector("#time")
+const startButton= document.querySelector("#start")
+const stopButton= document.querySelector("#stop")
+const gameContainer= document.querySelector(".game-container")
+const result= document.querySelector("#result")
+const controls= document.querySelector(".controls-container")
+let cards;
+let interval;
+let firstCard= false;
+let secondCard= false;
+
+
+//Items array
+const items= [
+    {name:"",image:(./)}
+]
