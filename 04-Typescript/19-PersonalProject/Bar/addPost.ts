f85eed0c8e16fce5f645c8bbb29b44a23b98bbae
Eliyaho basson <basson131@gmail.com>
2023-07-21 00:19:35 +0300

    1

---
+++ 04-Typescript/19-PersonalProject/Bar/addPost.ts
@@ -0,0 +1,25 @@
+//adding nw post.
+function addNewPost(
+    user: User[],
+    HTMLElement: HTMLElement | Element | null) {
+    try {
+        if (!HTMLElement) throw new Error('Root element is not found');
+        const html = `
+        <form class="addPost" onsubmit="handleAdd(event)">
+        <select name="user" id="user" required>
+        ${user.map((user) => {
+            return `<option value="${user.id}">${user.name}</option>`;
+        }).join('')}
+          <input type="text" name="image" placeholder="Image URL" required>
+          <button class="enterNewImage">+</button>
+          </form>`;
+
+        HTMLElement.innerHTML = html;
+
+        localStorage.setItem('userImgArray', JSON.stringify(userImgArray));
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+addNewPost(usersArray, document.querySelector('#addPost'));
---
+++ Projects/Ice_Cream_Truck/js/OpeningScreen.ts
@@ -0,0 +1,67 @@
+
+class Player {
+    id: string
+    constructor(public name: string, public points: number) {
+    }
+}
+
+
+function renderMain(rootElement: HTMLElement | null) {
+    try {
+        if (!rootElement) throw new Error("can not find root elemnt")
+        const html = `
+       
+        <button onclick="handleRenderInstructions()">HOW TO PLAY</button>
+        <form onsubmit="handleStartGame(event)">
+        <input type="text" name="name" placeholder="Enter your name" required>
+        <input type="submit" value="START">
+    </form>
+        `
+        rootElement.innerHTML = html
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function handleRenderInstructions() {
+    try {
+        const root = document.querySelector("#main")
+        if (!root) throw new Error("can not find root elemnt")
+        const html = `
+        <h3>
+        You have to make ice cream cones for the customers who come to your ice cream truck. <br>
+        Use the arrows to select the desired flavor. <br>
+        Press enter to add an ice cream scoop to the cone. <br>
+        To go to customer selection press the space bar, <br> select the appropriate customer
+        and press enter to deliver the ice cream. <br>
+    Be careful! <br>
+    Customers are not so patient.<br>
+        Work quickly and accurately! </h3>
+    <button onclick="handleRenderMain()">BACK</button>
+        `
+        root.innerHTML = html
+    } catch (error) {
+        console.error(error)
+    }
+
+}
+
+function handleRenderMain(){
+renderMain(document.querySelector("#main"))
+
+}
+
+function handleStartGame(ev:any){
+ try {
+    ev.preventDefault()
+    const name = ev.target.name.value
+    const player = new Player (name,0)
+    const playerToString = JSON.stringify(player)
+    localStorage.setItem("player", playerToString);
+    window.open("../Ice_Cream_Truck/pages/TheGame.html", "_self")
+ } catch (error) {
+    console.error(error)
+ }
+}
+
+renderMain(document.querySelector("#main"))
