28c0c7c14748a8444f6598823520e569f72bc443
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2023-07-20 12:35:33 +0300

    .

---
+++ 04-Typescript/19-PersonalProject/project1/login.js
@@ -0,0 +1,29 @@
+var Details = /** @class */ (function () {
+    function Details(name, pass, email, id) {
+        this.name = name;
+        this.pass = pass;
+        this.email = email;
+        if (id) {
+            this.id = id;
+        }
+        else {
+            this.id = "id-" + new Date().getTime() + "-" + Math.random();
+        }
+    }
+    return Details;
+}());
+var Detailsarray = [];
+var Detailsstring = localStorage.getItem("user");
+console.log(Detailsarray);
+function login(event) {
+    event.preventDefault();
+    var name = event.target.elements.name.value;
+    var pass = event.target.elements.pass.value;
+    var email = event.target.elements.email.value;
+    var id = event.target.elements.id;
+    var data = new Details(name, pass, email, id);
+    Detailsarray.push(data);
+    localStorage.setItem("user", JSON.stringify(Detailsarray));
+    event.target.reset();
+    window.location.href = "web.html";
+}
