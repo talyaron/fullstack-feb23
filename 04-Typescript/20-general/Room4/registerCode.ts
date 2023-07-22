de592845ca954757c708da8d26d8ad9dda6eb9bb
Benjamin Volodarsky <127942178+Benben3135@users.noreply.github.com>
2023-07-22 17:23:39 +0300

    .

--- 04-Typescript/20-general/Room4/registerCode.ts
+++ 04-Typescript/20-general/Room4/registerCode.ts
@@ -1,12 +1,12 @@
 const submitButton = document.querySelector("#registerSubmit") as HTMLElement
 const passwordINS = document.querySelector("#passwordINS") as HTMLElement
 
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
             entry.target.classList.add('show')
         }
-        else{
+        else {
             entry.target.classList.remove('show')
         }
 
@@ -14,27 +14,62 @@ const observer = new IntersectionObserver((entries)=>{
 })
 
 const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach((el)=> observer.observe(el))
+hiddenElements.forEach((el) => observer.observe(el))
 
-function passwordCheck(event:any){
+function passwordCheck(event: any) {
     event.preventDefault();
     let currentPassword = event.target.value;
-    if(currentPassword.length>=8){
+    if (currentPassword.length >= 8) {
         debugger;
         submitShow();
 
     }
-    else{
+    else {
         submitHide()
     }
 }
-function submitShow(){
-    submitButton.style.opacity= "1";
-    passwordINS.style.display= "none";
+function submitShow() {
+    submitButton.style.opacity = "1";
+    passwordINS.style.display = "none";
 
 }
-function submitHide(){
-    submitButton.style.opacity= "0";
-    passwordINS.style.display= "block";
+function submitHide() {
+    submitButton.style.opacity = "0";
+    passwordINS.style.display = "block";
 
+}
+// ----------------------------
+class profile {
+    constructor(public id: number, public firstName: string, public lastName: string, public username: string, public password: string) { }
+}
+const profilesArr: profile[] = [];
+function registerProfile(ev: any) {
+    try {
+        ev.preventDefault();
+        const firstName = ev.target.firstName.value;
+        const lastName = ev.target.lastName.value;
+        const username = ev.target.username.value;
+        const password = ev.target.password.value;
+        profilesArr.push(new profile((profilesArr.length + 1), firstName, lastName, username, password))
+        console.log(profilesArr);
+        storeProfile();
+
+
+    } catch (error) {
+        console.error(error);
+    }
+
+
+}
+function storeProfile(){
+    const profilesArrSTR = JSON.stringify(profilesArr);
+    localStorage.setItem("profiles", profilesArrSTR)
+    redirectPage()
+}
+function redirectPage(){
+    window.location.href = "Redirect.html"
+    redirectToMain()
+}
+
+function redirectToMain(){
 }
