6fdf50576b911ed87e3cbfc34dcff7b5e6e0e7f4
Lee_D <leegreat21@gmail.com>
2023-08-16 18:46:22 +0300

    task-3

--- 05-node/03-express/home-puzzle/Lee/server.ts
+++ 05-node/03-express/home-puzzle/Lee/server.ts
@@ -4,13 +4,27 @@ const port = process.env.PORT || 3000;
 
 
 app.use(express.static('public'));
+app.use(express.json());
 
-const myName = "The easiest way for vets!"
+const slogan = "The easiest way for vets!"
+let name = ""
+
+
+app.get('/slogan', (req, res) => {
+  console.log('request to slogan')
+  res.send({ slogan: slogan })
+});
 
 app.get('/name', (req, res) => {
-    console.log('request to name')
-    res.send({name: myName})
+  console.log('request to name')
+  res.send({ name: name })
 });
+
+app.post('/name', (req, res) => {
+  console.log('received name:', req.body.name)
+  name = req.body.name
+  res.send({ name: name })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
