c45efb460859d25940a9de32a930c38f23777bf3
ShaharShaul <128305593+ShaharShaul@users.noreply.github.com>
2023-08-20 16:01:56 +0300

    UPDATE

--- 05-node/02-ts/followups/shahar/dist/server.js
+++ 05-node/02-ts/followups/shahar/dist/server.js
@@ -1,1 +1,6 @@
-console.log("Hello client !!!!!!!!!!");
+"use strict";
+exports.__esModule = true;
+var helpers_1 = require("./cont/helpers");
+console.log("Hi, I'm a server");
+console.log(helpers_1["default"]);
+console.log(helpers_1.multi(5, helpers_1.y));
