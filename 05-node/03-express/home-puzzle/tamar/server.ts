5b39adf44d2fc14bdf79b7be0bc06e9daeabb8b7
itahTamar <125893355+itahTamar@users.noreply.github.com>
2023-08-20 11:25:01 +0300

    finish hw postmen used

--- 05-node/03-express/home-puzzle/tamar/server.ts
+++ 05-node/03-express/home-puzzle/tamar/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser'; //import bodyParser
+
 const app = express()
+app.use(bodyParser.json()); //use bodyParser middleware
+
 const port = process.env.PORT || 3000;
 
 //static files
 app.use(express.static('public'));
 
-const mytitle = "Tamar's Site is Here"
+let mytitle = "Tamar's Site is Here"
 
 //routes to get data from server
 app.get('/title', (req, res) => {
@@ -21,4 +25,12 @@ app.get('/description', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+//route to get data from user and sent it to server
+app.post('/updateTitle', (req, res) => {
+  console.log('request to update title');
+  const newTitle = req.body.title;
+  mytitle = newTitle;
+  res.send({message: 'Title updated successfully'});
 })
