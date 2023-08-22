5b6c756ff1b02d668591f1b61a548721d82667c1
itahTamar <tamar.verber@gmail.com>
2023-08-22 16:18:48 +0300

    ipdate2

--- 05-node/04-CRUD/home puzzle/tamar/public/index.ts
+++ 05-node/04-CRUD/home puzzle/tamar/public/index.ts
@@ -18,7 +18,7 @@ interface Friend {
       const instegram = event.target.instegram.value;
       const imgUrl = event.target.imgUrl.value;
       if (!fullName || !email || !phoneNumber) {
-        throw new Error('Please complete all first free fields');
+        throw new Error('Please complete all mandatory fields');
       }
   
       const friend: Friend = { fullName, email, phoneNumber, instegram, imgUrl };
@@ -32,13 +32,11 @@ interface Friend {
       });
   
       const result = await response.json();
-      console.log(result);
-  
+      console.log(result); 
   
     } catch (error) {
       console.error(error);
     }
-  
   }
   
   async function getfriends() {
@@ -66,7 +64,7 @@ interface Friend {
         <p>instegram: ${friends.instegram}</p>
         <form id="${friends.id}" onsubmit="handleUpdatefriends(event)">
         <input type="text" name="email" value="${friends.email}" placeholder="*e-mail">
-        <input type="text" name="phoneNumber"  value="${friends.phoneNumber}" placeholder="Price" />
+        <input type="text" name="phoneNumber"  value="${friends.phoneNumber}" placeholder="Price">
         <input type="text" name="instegram" value="${friends.instegram}" placeholder="instegram accaunte">
         <input type="url" name="imgUrl" value="${friends.imgUrl}" placeholder="my friend Image">
         <button type="submit">Update</button></form>
@@ -79,31 +77,28 @@ interface Friend {
     }
   }
   
-  function renderfriends(friends: Friend[], HTMLElement: HTMLDivElement) {
+  function renderfriends(friends: Friend[], HTMLElement: HTMLDivElement) { //show the list on screen
     try {
       if (!HTMLElement) throw new Error("HTMLElement not found")
       console.log(friends)
       if (!Array.isArray(friends)) throw new Error("friendss are not array");
-      const friendssHTML = friends.map(friends => renderfriendsHTML(friends)).join("")
-      HTMLElement.innerHTML = friendssHTML;
+      const friendsHTML = friends.map(friend => renderfriendsHTML(friend)).join("")
+      HTMLElement.innerHTML = friendsHTML;
     } catch (error) {
       console.error(error)
     }
   }
   
-  async function handleShowFriends() {
+  async function handleGetFriends() { //get the list from server
     const friends = await getfriends();
-  
     const root = document.querySelector('#root');
-    renderfriends(friends, root as HTMLDivElement);
-  
-  
+    renderfriends(friends, root as HTMLDivElement); //call the function to show the list on-screen
   }
   
   async function handleDeletefriends(id:string){
     try {
       const response = await fetch('/API/delete-friends', {
-        method: 'DELETE',
+        method: 'DELETE', //delet the data saved in server
         headers: {
           'Content-Type': 'application/json'
         },
@@ -113,32 +108,35 @@ interface Friend {
       console.log(result);
       const {friends} = result;
   
-      renderfriends(friends, document.querySelector('#root') as HTMLDivElement);
+      renderfriends(friends, document.querySelector('#root') as HTMLDivElement); //call the function to shoe the update list
   
     } catch (error) {
       console.error(error)
     }
   }
   
-  async function handleUpdatefriends(ev:any){
+  async function handleUpdatefriends(ev:any){ //deal with the update from user and save it in server
     try {
       ev.preventDefault();
-      const phoneNumber = ev.target.phoneNumber.valueAsNumber;
+      const phoneNumber = ev.target.phoneNumber.value;
+      const email = ev.target.email.value;
+      const instegram = ev.target.instegram.value;
+      const imgUrl = ev.target.imgUrl.value;
       const id = ev.target.id;
-      console.log(id, phoneNumber)
+      console.log(id, phoneNumber, email, instegram, imgUrl)
   
       const response = await fetch('/API/update-friends', {
-        method: 'PATCH',
+        method: 'PATCH', //catch the update and save it in server
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({id, phoneNumber})
+        body: JSON.stringify({id, phoneNumber, email, instegram, imgUrl})
       });
   
       const result = await response.json();
       console.log(result);
-      const {friendss} = result;
-      renderfriendss(friendss, document.querySelector('#root') as HTMLDivElement);
+      const {friends} = result;
+      renderfriends(friends, document.querySelector('#root') as HTMLDivElement);
   
     } catch (error) {
       console.error(error)
