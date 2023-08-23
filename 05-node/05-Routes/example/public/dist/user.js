14446da0f169cb05b92d5ee933df74019da43673
itahTamar <tamar.verber@gmail.com>
2023-08-23 20:35:23 +0300

    cont

---
+++ 05-node/05-Routes/example/public/dist/user.js
@@ -0,0 +1,5 @@
+function handleGetUser() {
+    var toHtml = document.querySelector('#userRoot');
+    var html = "\n    <h2>Add User</h2>\n    <form onsubmit=\"handleAddUser(event)\">\n        <input type=\"text\" name=\"name\" placeholder=\"user name\">\n        <input type=\"number\" name='phoneNum' placeholder=\"phoneNum\">\n        <input type=\"url\" name=\"imgUrl\" placeholder=\"Image URL\">\n        <button type=\"submit\">ADD</button>\n    </form>";
+    toHtml.innerHTML = html;
+}
