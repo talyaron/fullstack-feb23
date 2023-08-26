586e99e7b86122d6cc6b0849ffc85c96377afe75
Lee_D <leegreat21@gmail.com>
2023-08-26 13:10:21 +0300

    homeworkUpdate

--- 05-node/05-Routes/home-puzzles/Lee/API/Tasks/tasksRouter.ts
+++ 05-node/05-Routes/home-puzzles/Lee/API/Tasks/tasksRouter.ts
@@ -1,8 +1,12 @@
 import express from 'express'
+import { addTasks, deleteTask, getTasks, updateTaskStatus } from './tasksCont'
 const router = express.router()
 
-router.get('/get-tasks', (req, res) => {
-    res.send({tasks})
-})
+
+router
+    .get('/get-tasks', getTasks)
+    .post('/add-task', addTasks)
+    .deleteTask('/delete-task', deleteTask)
+    .patch('/add-task', updateTaskStatus)
 
 export default router;
