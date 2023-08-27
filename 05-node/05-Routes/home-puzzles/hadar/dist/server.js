d1de80fc920657397c3a92f3ffb1ad63f7085f06
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2023-08-27 13:30:42 +0300

    .

--- 05-node/05-Routes/home-puzzles/hadar/dist/server.js
+++ 05-node/05-Routes/home-puzzles/hadar/dist/server.js
@@ -8,6 +8,8 @@ const app = express_1.default();
 const port = process.env.PORT || 3000;
 app.use(express_1.default.static('public'));
 app.use(express_1.default.json());
+const router_1 = __importDefault(require("./API/router"));
+app.use('/API', router_1.default);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
