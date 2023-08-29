df8577526b0080bd3be74e4eb42b677839bb57d6
Lee_D <leegreat21@gmail.com>
2023-08-29 14:11:54 +0300

    statusUpdate

--- 05-node/05-Routes/home-puzzles/Lee/API/Tasks/dist/tasksRouter.js
+++ 05-node/05-Routes/home-puzzles/Lee/API/Tasks/dist/tasksRouter.js
@@ -4,7 +4,7 @@ var express_1 = require("express");
 var tasksCont_1 = require("./tasksCont");
 var router = express_1["default"].Router();
 router.get('/get-tasks', tasksCont_1.getTasks)
-    .post('/add-task', tasksCont_1.addTask);
-// .delete('/delete-task', deleteTask)
-// .patch('/update-task', updateTaskStatus)
+    .post('/add-task', tasksCont_1.addTask)
+    // .delete('/delete-task', deleteTask)
+    .patch('/update-task-status', tasksCont_1.updateTaskStatus);
 exports["default"] = router;
