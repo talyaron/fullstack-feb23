8b815c251d51eda657d555c97af42ec923de56c3
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2023-08-30 19:16:44 +0300

    .

--- 05-node/05-Routes/home-puzzles/hadar/server.ts
+++ 05-node/05-Routes/home-puzzles/hadar/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 app.use(express.static('public'));
 app.use(express.json());
