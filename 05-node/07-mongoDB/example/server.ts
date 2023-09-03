25721be19977e5206669184c358c4550098e3388
Tal <tal.yaron@gmail.com>
2023-09-03 18:32:58 +0300

    Mongoose

---
+++ 05-node/07-mongoDB/example/server.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from 'mongoose';
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+//static files
+app.use(express.static("public"));
+
+//body
+app.use(express.json());
+
+//connect to mongoDB with mongoose
+mongoose.connect("mongodb+srv://tal:mwtBquKP1ZHIIQGH@cluster0.0hzknon.mongodb.net/test")
+.then(()=>{
+  console.info("MongoDB connected")
+})
+.catch(err=>{
+  console.error(err)
+})
+
+//mwtBquKP1ZHIIQGH
+
+
+
+//router to products
+
+// get router from userRouter
+import userRouter from "./API/users/userRouter";
+//tells express to use userRouter on the intial route "/API/users"
+app.use("/API/users", userRouter)
+
+import tasksRouter from "./API/tasks/tasksRoutes";
+app.use("/API/tasks", tasksRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
