ba7af045be59b6905d426ac73ec31be2347978c4
Lee_D <leegreat21@gmail.com>
2023-09-05 00:37:18 +0300

    myTree

---
+++ 05-node/08-personal-project/Lee/dist/server.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const app = express_1.default();
+const port = process.env.PORT || 3000;
+//static files
+app.use(express_1.default.static("public"));
+//body
+app.use(express_1.default.json());
+mongoose_1.default.connect("mongodb+srv://leedee:Omer2021!@cluster0.pe6xuzr.mongodb.net/test").then(() => {
+    console.info("MongoDB connected");
+}).catch(err => { console.error(err); });
+//router to products
+// get router from userRouter
+const userRouter_1 = __importDefault(require("./API/users/userRouter"));
+//tells express to use userRouter on the intial route "/API/users"
+app.use("/API/users", userRouter_1.default);
+const tasksRoutes_1 = __importDefault(require("./API/tasks/tasksRoutes"));
+app.use("/API/tasks", tasksRoutes_1.default);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
