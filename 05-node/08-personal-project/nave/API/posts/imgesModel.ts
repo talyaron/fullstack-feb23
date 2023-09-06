586f493af78a49260784b2673dcc45d2c458f68b
navered <128045047+navered@users.noreply.github.com>
2023-09-06 15:15:15 +0300

    nave

---
+++ 05-node/08-personal-project/nave/API/posts/imgesModel.ts
@@ -0,0 +1,17 @@
+import { User } from "../users/userModel";
+
+export class Image{
+    imgUrl:string;
+    title:string;
+    constructor ({imgUrl,title}: {imgUrl:string , title:string}){
+        this.imgUrl = imgUrl;
+        this.title = title;
+
+    }}
+    export const images:Image[] = []
+    export class UserImage{
+        constructor(public user:User , public img:Image){}
+
+    }
+    export const UserImages:UserImage[] = []
+    
