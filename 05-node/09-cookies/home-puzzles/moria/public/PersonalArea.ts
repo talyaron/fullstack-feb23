21a7e507fae7f6da94534416cf743dd5b61e3cc8
moria-avraham <128312303+moria-avraham@users.noreply.github.com>
2023-09-15 10:13:14 +0300

    .

--- 05-node/09-cookies/home-puzzles/moria/public/PersonalArea.ts
+++ 05-node/09-cookies/home-puzzles/moria/public/PersonalArea.ts
@@ -150,3 +150,14 @@ async function handleGetAppointments() {
     const appointments = document.querySelector("#appointments");
     renderAppointments(PersonalAppointments, appointments as HTMLDivElement);
 }
+async function handleGetUser() {
+    try {
+        //ask server to get the user
+        const response = await fetch("API/user/get-user");
+        const data = await response.json();
+        console.log(data)
+
+    } catch (error) {
+
+    }
+}
