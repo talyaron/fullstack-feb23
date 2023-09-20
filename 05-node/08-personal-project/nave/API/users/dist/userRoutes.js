41b9b124e9f8dfe918bd59251d003f3fc8507574
navered <128045047+navered@users.noreply.github.com>
2023-09-20 19:15:09 +0300

    nave

--- 05-node/08-personal-project/nave/API/users/dist/userRoutes.js
+++ 05-node/08-personal-project/nave/API/users/dist/userRoutes.js
@@ -5,6 +5,7 @@ var userCont_1 = require("./userCont");
 // import { isAdmin } from './userMiddle';
 var router = express_1["default"].Router();
 router
+    .post('/register-user', userCont_1.registerUser)
     .get('/get-user-login', userCont_1.getUserLogin)
     .get('/get-users', userCont_1.getUsers)
     .get('/register-user', userCont_1.getUser);
