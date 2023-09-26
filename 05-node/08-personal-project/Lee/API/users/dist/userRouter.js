96af05a5c2bd8152dd9a4b7f88b449b5a7a3cc84
Lee_D <leegreat21@gmail.com>
2023-09-26 15:47:34 +0300

    myTree-admin-functionality

--- 05-node/08-personal-project/Lee/API/users/dist/userRouter.js
+++ 05-node/08-personal-project/Lee/API/users/dist/userRouter.js
@@ -7,5 +7,6 @@ var router = express_1["default"].Router();
 router
     .post("/register", userCont_1.registerUser)
     .post("/login", userCont_1.login)
-    .get("/userWithRelatives", middlewareUsers_1.isAdmin, userCont_1.getUserAndRelatives);
+    .get("/userWithRelatives", middlewareUsers_1.isAdmin, userCont_1.getUserAndRelatives) // Fetch user data with relatives
+    .get("/allUsersWithRelatives", middlewareUsers_1.isAdmin, userCont_1.getAllUsersAndRelatives); // Fetch all users with relatives
 exports["default"] = router;
