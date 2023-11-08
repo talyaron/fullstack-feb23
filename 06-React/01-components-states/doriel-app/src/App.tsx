866ff049de95ac7707db0bbff8944651952bca90
Doriel Shacham <dorielshacham@gmail.com>
2023-11-08 20:24:03 +0200

    adding a blog and color to each creation of a blog Apps.tsx

--- 06-React/01-components-states/doriel-app/src/App.tsx
+++ 06-React/01-components-states/doriel-app/src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+type Blog = {
+  id: number;
+  title: string;
+  content: string;
+  image: string;
+};
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function App() {
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [backgroundColor, setBackgroundColor] = useState('#ffffff');
+
+  const addBlog = () => {
+    const newBlog: Blog = {
+      id: blogs.length + 1,
+      title: 'Blog Title',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      image: 'https://via.placeholder.com/150',
+    };
+    setBlogs([...blogs, newBlog]);
+
+    setBackgroundColor(getRandomColor());
+  };
+
+  const removeBlog = () => {
+    if (blogs.length > 0) {
+      const updatedBlogs = [...blogs];
+      updatedBlogs.pop();
+      setBlogs(updatedBlogs);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor }}>
+      <h1>Welcome to GamesEngines Forum</h1>
+
+      <div className="blog-list">
+        {blogs.map((blog) => (
+          <div className="blog" key={blog.id}>
+            <img src={blog.image} alt="Blog" />
+            <h2>{blog.title}</h2>
+            <p>{blog.content}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="buttons">
+        <button onClick={addBlog}>Add Blog</button>
+        <button onClick={removeBlog}>Remove Blog</button>
+      </div>
     </div>
   );
 }
