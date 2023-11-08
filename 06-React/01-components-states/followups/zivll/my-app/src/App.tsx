bbca8446e356ed191b59c7c78cf575051bc46879
ziv leloneck <zivll1991@gmail.com>
2023-11-08 20:40:09 +0200

    solving class puzzle

--- 06-React/01-components-states/followups/zivll/my-app/src/App.tsx
+++ 06-React/01-components-states/followups/zivll/my-app/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [multiplyCounter, setMultiplyCounter] = useState(2);
+  const addOne = () => {
+    setCounter(counter + 1);
+  };
+  const multiply = () => {
+    setMultiplyCounter(multiplyCounter * counter);
+    console.log(multiplyCounter);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>counter: {counter}</p>
+      <button onClick={addOne}>Add 1</button>
+      <button onClick={multiply}>Multiply</button>
     </div>
   );
 }
