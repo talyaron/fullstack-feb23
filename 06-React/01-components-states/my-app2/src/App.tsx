0dfb7eaf63ab2e26299c40b7336c435ed287bd9c
gili <gilimena95@gmail.com>
2023-11-08 19:43:05 +0200

    Update App.tsx

--- 06-React/01-components-states/my-app2/src/App.tsx
+++ 06-React/01-components-states/my-app2/src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counterState, setCounterState] = useState(0)
+  const text = "my name is gili"
+  let counter = 0;
+  console.log("component is rendered")
+
+  const addOne = () => {
+    counter++
+    console.log(counter)
+  }
+  const addOneState = () => {
+   setCounterState(counterState + 1)
+    console.log(counterState)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello! {text}</p>
+      <p>count: {counter}</p>
+      <p>count State: {counterState}</p>
+      <button onClick={addOne}>ADD</button>
+      <button onClick={addOneState}>ADD To state</button>
     </div>
   );
 }
