90ac2acb14ac1f5b4fe7c6b84cc41923f1cf16f6
shiranlasry <128288093+shiranlasry@users.noreply.github.com>
2023-11-08 19:50:32 +0200

    create react app

---
+++ 06-React/01-components-states/followups/shiranL/my-app2/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+const {
+
+  return (
+    <div className="App">
+    <div>hello</div>
+    </div>
+  );
+}
+
+export default App;
