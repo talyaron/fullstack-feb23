7559425d3d33012aac93ed709dcc8f2925923cb7
navered <128045047+navered@users.noreply.github.com>
2023-11-12 17:35:01 +0200

    clean folder

--- 06-React/01-components-states/followups/nave/my-app/src/App.tsx
+++ 06-React/01-components-states/followups/nave/my-app/src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+type Color = [number, number, number, number];
+
 function App() {
+  const [counter, setCounter] = useState<number>(0);
+  const [text, setText] = useState<string>("");
+  const [color, setColor] = useState<Color | undefined>(undefined);
+  const [circleColor, setCircleColor] = useState<Color | undefined>(undefined);
+
+  const addOne = () => {
+    setCounter(counter + 1);
+  }
+
+  const removeOne = () => {
+    setCounter(counter - 1);
+  }
+
+  const changeName = () => {
+    const username = prompt("Add your name!");
+    if (username) {
+      setText(username);
+    } else {
+      alert("Please provide a name!");
+    }
+  }
+
+  const changeColor = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedColor = event.target.value;
+    setColor(selectedColor !== "" ? selectedColor.split(',').map(Number) as Color : undefined);
+    setCircleColor(selectedColor !== "" ? selectedColor.split(',').map(Number) as Color : undefined);
+  }
+
+  const textStyle = {
+    color: color ? `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${color[3]})` : 'black',
+  };
+
+  const circleStyle = {
+    backgroundColor: circleColor ? `rgba(${circleColor[0]}, ${circleColor[1]}, ${circleColor[2]}, ${circleColor[3]})` : 'transparent',
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p style={textStyle}>Hello {text}!</p>
+      <p>Count: {counter}</p>
+      <button onClick={addOne}>ADD 1</button>
+      <button onClick={removeOne}>REMOVE 1</button>
+      <button onClick={changeName}>ADD NAME</button>
+      <button>
+        <label htmlFor="colorInput">ADD COLOR</label>
+        <input id="colorInput" type="color" onChange={changeColor} />
+      </button>
+      <div className="circle" style={circleStyle}></div>
     </div>
   );
 }
