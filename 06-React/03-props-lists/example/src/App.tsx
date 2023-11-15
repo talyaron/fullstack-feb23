a20d691b8b6cf77aea92004b783b3c84fd972850
gili <gilimena95@gmail.com>
2023-11-15 20:30:25 +0200

    changes

--- 06-React/03-props-lists/example/src/App.tsx
+++ 06-React/03-props-lists/example/src/App.tsx
@@ -2,18 +2,56 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Title from "./components/Title/Title";
-
+import Paragraph from "./components/Paragraph/Paragraph";
+import { users } from "./util/users";
+import UserCard from "./components/userCard/UserCard";
 
 function App() {
-  const testHeader = "gili"
   return (
     <div className="App">
-      <Title header="this is title"/>
-      <Title header="hello"/>
-      <Title header="world"/>
-      <Title header={testHeader}/>
+      {users.map((user) => {
+        return <UserCard key={user.email} email={user.email} username={user.username} />;
+      })}
+      {users.map((user) => {
+        return (
+          <div key={user.email}>
+            <h1>{user.email}</h1>
+            <p>{user.username}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
 export default App;
+
+// const testHeader = "gili"
+
+// const userTest = {
+//   name: "gili",
+//   age: 28
+// }
+
+// const hello = () => {
+//   return <p>hello</p>
+// }
+{
+  /* <div className="App">
+      <Title header="this is title"/>
+      <Title header="hello"/>
+      <Title header="world"/>
+      <Title header="test"/>
+      <Title header={testHeader}/>
+      <Paragraph color="red">
+        <h1>hello</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet doloribus rem aliquid voluptatum est illum laboriosam quos odit consequatur deleniti, qui quo tempore, in, ipsam sed incidunt quia laudantium iure?</p>
+      </Paragraph>
+      <Paragraph color="blue">this is gili</Paragraph>
+      <Paragraph>no color</Paragraph>
+      <Paragraph color="blue">3</Paragraph>
+      <Paragraph color="blue">{hello()}</Paragraph>
+      <Paragraph color="blue"></Paragraph>
+      <Paragraph color="blue">{JSON.stringify(user)}</Paragraph>
+    </div> */
+}
