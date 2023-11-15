e300ceac796d4af0833aca3e7d9fa7157188ab89
itahTamar <tamar.verber@gmail.com>
2023-11-15 17:33:18 +0200

    extra

--- 06-React/02-JSX-states-components/home-extras/tamar_extra/src/App.tsx
+++ 06-React/02-JSX-states-components/home-extras/tamar_extra/src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+import CustomCursor from'./Cursor.tsx'
 
 function App() {
-  const [redFollow, serRedFollow] = useState()
-  // const [count, setCount] = useState(0)
 
   return (
     <>
+      <CustomCursor></CustomCursor>
       <div className='color'></div>
-      <div className='follow'></div>
     </>
   )
 }
