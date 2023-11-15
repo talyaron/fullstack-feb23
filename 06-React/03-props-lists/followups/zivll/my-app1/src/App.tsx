dda73fb7088236eee89fca7fb923abd1ed475b95
ziv leloneck <zivll1991@gmail.com>
2023-11-15 19:59:38 +0200

    class puzzle

--- 06-React/03-props-lists/followups/zivll/my-app1/src/App.tsx
+++ 06-React/03-props-lists/followups/zivll/my-app1/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import User from './components/User/User';
 
 function App() {
   return (
     <div className="App">
-    
-    </div>
+      <div className="cards">
+        <User id={Math.random().toString(36).substr(2, 9)} name={'Ziv'} userName={'zivll'} email={'zivll1991@gmail.com'} phone={'0543172207'} website={'ziv.co.il'} age={32} />
+
+        <User id={Math.random().toString(36).substr(2, 9)} name={'Moshe'} userName={'Moshead'} email={'Moshead@gmail.com'} phone={'0543171549'} website={'Moshe.co.il'} age={32} />
+
+        <User id={Math.random().toString(36).substr(2, 9)} name={'Dina'} userName={'Dinabe'} email={'Dina@gmail.com'} phone={'0543175469'} website={'Dina.co.il'} age={32} />
+
+      </div></div>
+
+
+
   );
 }
 
