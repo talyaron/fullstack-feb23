33f8f34a936f997e469ec542ceeeb0e019c9efb8
shiranlasry <128288093+shiranlasry@users.noreply.github.com>
2023-11-15 23:24:44 +0200

    home work step 1

--- 06-React/03-props-lists/followups/shiranL/my-app1/src/App.tsx
+++ 06-React/03-props-lists/followups/shiranL/my-app1/src/App.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import User from './components/User/User';
+import Product from './components/Product/Product';
 import './App.scss';
 
 import { users } from './util/users';
+import {products} from './util/products';
 
 function App() {
   return (
     <div className="App"> 
+       <div className='productsList'>
+        <h1>Products List</h1>
+      </div>
+      
+    <div className='products'>
+    {products.map((product) => {
+        return <Product key={product.id} name={product.name} price={product.price} id={0} category={product.category} >
+          <p>i am children</p>
+          </Product>;
+      })}
+      </div>
+   
     
-      <User name={users[0].name} username={users[0].username} email={users[0].email} phone={users[0].phone} website={users[0].website} age={users[0].age} >
-        <p>Hi, I'm a child</p>
-      </User>
-      <User name={users[1].name} username={users[1].username} email={users[1].email} phone={users[1].phone} website={users[1].website} age={users[1].age} >
-        <p>Hi, I'm Super child</p>
-      </User>
+    
+   
     </div>
   );
 }
---
+++ 06-React/03-props-lists/followups/shiranL/my-app1/src/util/products.ts
@@ -0,0 +1,122 @@
+export const products = [
+    {
+    id : 1,
+    name : "milk",
+    price : 5,
+    category : "food"
+},
+{
+    id : 2,
+    name : "bread",
+    price : 3,
+    category : "food"
+},
+{
+    id : 3,
+    name : "cheese",
+    price : 10,
+    category : "food"
+},
+{
+    id : 4,
+    name : "tomato",
+    price : 2,
+    category : "food"
+},
+{
+    id : 5,
+    name : "cucumber",
+    price : 3,
+    category : "food"
+},
+{
+    id : 6,
+    name : "chocolate",
+    price : 20,
+    category : "food"
+},
+{
+    id : 7,
+    name : "banana",
+    price : 5,
+    category : "food"
+},
+{
+    id : 8,
+    name : "apple",
+    price : 3,
+    category : "food"
+},
+{
+    id : 9,
+    name : "orange",
+    price : 4,
+    category : "food"
+},
+{
+    id : 10,
+    name : "mango",
+    price : 6,
+    category : "food"
+},
+{
+    id : 11,
+    name : "chicken",
+    price : 20,
+    category : "food"
+},
+{
+    id : 12,
+    name : "beef",
+    price : 30,
+    category : "food"
+},
+{
+    id : 13,
+    name : "fish",
+    price : 25,
+    category : "food"
+},
+{
+    id : 14,
+    name : "salmon",
+    price : 30,
+    category : "food"
+},
+{
+    id : 15,
+    name : "tuna",
+    price : 25,
+    category : "food"
+},
+{
+    id : 16,
+    name : "pasta",
+    price : 10,
+    category : "food"
+},
+{
+    id : 17,
+    name : "rice",
+    price : 5,
+    category : "food"
+},
+{
+    id : 18,
+    name : "potato",
+    price : 3,
+    category : "food"
+},
+{
+    id : 19,
+    name : "onion",
+    price : 2,
+    category : "food"
+},
+{
+    id : 20,
+    name : "garlic",
+    price : 2,
+    category : "food"
+}
+]
