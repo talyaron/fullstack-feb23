3e7c1cf7a2a37713ef01c8715fa31b683a197476
ziv leloneck <zivll1991@gmail.com>
2023-11-17 12:53:31 +0200

    Exercise number 3

---
+++ 06-React/03-props-lists/followups/zivll/my-app2/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import AnimalCard from './components/animal-card/AnimalCard';
+import { animals } from './utilities/animals';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="cards">
+        {animals.map((animal) => { return <AnimalCard key={animal.name} type={animal.type} name={animal.name} age={animal.age} /> })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
