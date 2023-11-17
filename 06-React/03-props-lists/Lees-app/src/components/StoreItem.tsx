f16ad1839070a124c67babbe5bfc0e1ccfb8d21c
Lee_D <leegreat21@gmail.com>
2023-11-17 12:18:20 +0200

    sideBar

--- 06-React/03-props-lists/Lees-app/src/components/StoreItem.tsx
+++ 06-React/03-props-lists/Lees-app/src/components/StoreItem.tsx
@@ -1,15 +1,22 @@
 import { Button, Card } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
+import { useShoppingCart } from "../context/shoppingCartContext";
 
 type StoreItemProps = {
-  id: string;
+  id: number;
   name: string;
   price: number;
   imgUrl: string;
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-  const quantity = 0;
+  const {
+    getItemQuantity,
+    increaseCartQuantity, 
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart()
+const quantity = getItemQuantity(id);
   return (
     <Card className="h-100">
       <Card.Img
@@ -25,7 +32,7 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         </Card.Title>
         <div className="mt-auto">
           {quantity === 0 ? (
-            <Button className="w-100">+ Add To Cart</Button>
+            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>+ Add To Cart</Button>
           ) : (
             <div
               className="d-flex align-items-center flex-column"
@@ -35,15 +42,15 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                 className="d-flex align-item-center justify-content-center"
                 style={{ gap: ".5rem" }}
               >
-                <Button>+</Button>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
                 <div>
                 <span className="fs-3">{quantity}</span> In Cart
             
                 </div>
 
-                <Button>-</Button>
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
               </div>
-              <Button variant="danger" size="sm" >Remove</Button>
+              <Button onClick={() => removeFromCart(id)} variant="danger" size="sm" >Remove</Button>
         
             </div>
           )}
