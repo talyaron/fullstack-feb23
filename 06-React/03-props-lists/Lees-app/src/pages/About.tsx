47a7a0ff14d65e1ef70646fd72523b78ded24b08
Lee_D <leegreat21@gmail.com>
2023-11-18 15:00:41 +0200

    about

--- 06-React/03-props-lists/Lees-app/src/pages/About.tsx
+++ 06-React/03-props-lists/Lees-app/src/pages/About.tsx
@@ -1,3 +1,40 @@
-export function About () {
-    return <h1>About</h1>
+export function About() {
+  return (
+    <>
+      <h1>Welcome to MyStore</h1>
+      <h3>Vintage and Beyond!</h3>
+      <p>
+        Step into a world where the past meets the present, and every item tells
+        a unique story. At MyStore, we curate a collection of exceptional
+        vintage finds and beyond, offering a diverse array of treasures that
+        transcend time.
+      </p>
+      <p>
+        Discover the charm of bygone eras with our carefully selected vintage
+        pieces, each with its own character and history. From retro furniture to
+        classic clothing, our collection reflects the timeless beauty of
+        yesteryears.
+      </p>
+      <p>
+        But that's not all – at MyStore, "beyond" means embracing the modern as
+        well. Explore contemporary items that blend seamlessly with the vintage
+        aesthetic, creating a shopping experience that bridges the gap between
+        nostalgia and the latest trends.
+      </p>
+      <p>
+        Whether you're a vintage enthusiast or someone seeking a touch of
+        classic elegance in your modern life, MyStore is your destination.
+        Immerse yourself in the unique blend of vintage and beyond, and find
+        something truly special that resonates with your style and personality.
+      </p>
+      <p>
+        Join us on a journey through time at MyStore: where the past meets the
+        present, and every purchase becomes a part of your story.
+      </p>
+      <a href="/store" style={{
+    color: 'black', 
+    textDecoration: 'none', 
+  }}>Happy exploring! 🕰️✨</a>
+    </>
+  );
 }
--- 06-React/03-props-lists/Lees-app/src/pages/Home.tsx
+++ 06-React/03-props-lists/Lees-app/src/pages/Home.tsx
@@ -1,16 +1,21 @@
 export function Home() {
   return (
-    <div>
+    <div style={{ backgroundColor: '#f8f9fa', minHeight: '100vh' }}>
       <div className="jumbotron">
-        <h1 className="display-4 mx-3 ">Welcome to MyStore!</h1>
-        <p className="lead mx-3 ">Discover amazing vintage products & beyond</p>
-        <a className="btn btn-primary btn-lg mx-3 my-3" href="/store" role="button">
+        <h1 className="display-4 mx-4">Welcome to MyStore!</h1>
+        <p className="lead mx-4 my-2">Discover amazing vintage products & beyond</p>
+        <a
+          className="btn btn-primary btn-lg mx-4 my-3"
+          href="/store"
+          role="button"
+          style={{ backgroundColor: '#007BFF', border: 'none' }}
+        >
           Shop Now
         </a>
       </div>
 
       <div className="container">
-        <div className="card mb-3">
+        <div className="card mb-3" style={{ margin: '0 13px' }}>
           <img
             src="/imgs/versatileDesk.jpg"
             className="card-img-top"
@@ -18,14 +23,16 @@ export function Home() {
           />
           <div className="card-body">
             <h5 className="card-title">Versatile Desk</h5>
-            <p className="card-text">L Shaped Desk with Shelves, Corner Computer Office Desk for Home Office</p>
+            <p className="card-text">
+              L Shaped Desk with Shelves, Corner Computer Office Desk for Home Office
+            </p>
             <a href="/store" className="btn btn-primary">
               View Details
             </a>
           </div>
         </div>
 
-        <div className="card mb-3">
+        <div className="card mb-3" style={{ margin: '0 13px' }}>
           <img
             src="/imgs/car2.jpg"
             className="card-img-top"
