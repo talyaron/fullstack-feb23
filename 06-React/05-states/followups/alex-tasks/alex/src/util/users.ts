4733faef100e8182de000728a0e1d9dc2d199038
inalexa81 <athegreat5@hotmail.com>
2023-11-22 23:46:19 +0200

    alex-p&l-hw

---
+++ 06-React/05-states/followups/alex-tasks/alex/src/util/users.ts
@@ -0,0 +1,21 @@
+import { User } from '../components/User';
+export const users: UserCard  [] = [
+    {
+      id: 1,
+      name: "Pepito",
+      lastName: "Gonzales",
+      counter : 0
+    },
+    {
+        id: 2,
+        name: "Fifty",
+        lastName: "Cent",
+        counter : 0
+      },
+      {
+        id: 3,
+        name: "Amit",
+        lastName: "Kabel",
+        counter : 0
+      },
+    ]
