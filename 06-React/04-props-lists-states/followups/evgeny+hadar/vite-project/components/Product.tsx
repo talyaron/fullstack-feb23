b9d51921f49e54dadf3d69fadcda0eaac48b585b
Evgeny <zakman336@gmail.com>
2023-11-23 12:32:33 +0200

    s

--- 06-React/04-props-lists-states/followups/evgeny+hadar/vite-project/components/Product.tsx
+++ 06-React/04-props-lists-states/followups/evgeny+hadar/vite-project/components/Product.tsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { products } from '../src/assets/products'
 
-const Product = () => {
 
+
+const Product = ({products}) => {
+  
   const productList = products.products
+  const [productsArr,setProducts]= useState<any[]>(productList)
 
 
+  const handleRemove=(ev)=>{
+    setProducts(
+      productsArr.filter((productsList) => {
+        return productsList.id != ev.target.value;
+      })
+    );
+  }
+   
   return (
-    <div>
-      <h1>hi</h1>
-      {productList[1].id}
+    <div className='productlist__items'>
+
+      {productsArr.map((products) => {
+        return (
+          <div className='productlist__items__item' key={products.id} >
+            <img src={products.thumbnail} alt="" />
+            <h3>Category:{products.category}</h3>
+            <h3>name: {products.title}</h3>
+            <h3>Brand:{products.brand}</h3>
+            <h3>Rating:{products.rating}</h3>
+            <h3>Price:{products.price}</h3>
+            
+            <button className='buttonstyle' onClick={handleRemove}  value={products.id}> Remove</button>
+            </div>
+        );
+      })}
     </div>
   )
 }
--- 06-React/04-props-lists-states/followups/evgeny+hadar/vite-project/src/App.tsx
+++ 06-React/04-props-lists-states/followups/evgeny+hadar/vite-project/src/App.tsx
@@ -3,21 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
 import { products } from './assets/products'
+import Product from './../components/Product';
+import Button from '../components/Button'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
   const productList = products.products
+  const [productsArr,setProducts]= useState<any[]>(productList)
+  const [showList, setShowList] = useState(false);
+  
   let userId = productList[0].id
   console.log(userId)
 
+  function renderList(){
+    setShowList(!showList)
+  }
+
+  
+  
   return (
-    <>
-     <div>
-      
-     </div>
-     
-    </>
+    <div className='mainlist'>
+    
+     <Button handleClick={renderList} freetext='showlist' > </Button>
+     {showList && (
+      <div className='productlist'>
+      <Product products={products} />
+      </div>
+     ) }
+
+    </div>
   )
 }
 
