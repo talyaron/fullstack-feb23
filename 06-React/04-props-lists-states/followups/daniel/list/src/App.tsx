50566199409dcce024a764632fd5d7a14d003eee
danieltovbin <tovbindaniel@gmail.com>
2023-11-25 15:52:52 +0200

    ProductCard-Done

--- 06-React/04-props-lists-states/followups/daniel/list/src/App.tsx
+++ 06-React/04-props-lists-states/followups/daniel/list/src/App.tsx
@@ -2,13 +2,8 @@ import './App.scss'
 import ProductCard from './components/product/ProductCard'
 
 
-// 4. by button press, show only products above 4.5 ratings.
-
-
 
 function App() {
-
-
   return (
     <>
       <ProductCard />
