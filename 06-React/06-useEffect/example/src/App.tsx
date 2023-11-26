6c5b3e0433f43e9b053fcea9f1edd3f9d5759244
gili <gilimena95@gmail.com>
2023-11-26 12:24:42 +0200

    task followups

--- 06-React/06-useEffect/example/src/App.tsx
+++ 06-React/06-useEffect/example/src/App.tsx
@@ -2,39 +2,39 @@ import { useEffect, useState } from "react";
 import "./App.scss";
 import UserCard, { User } from "./components/UserCard";
 import { CircularProgress, Typography, Box } from "@mui/material";
-import axios from "axios";
+// import axios from "axios";
 
 function App() {
-  const [user, setUser] = useState({
-    id: 1,
-    name: "Leanne Graham",
-    username: "Bret",
-    email: "Sincere@april.biz",
-    address: {
-      street: "Kulas Light",
-      suite: "Apt. 556",
-      city: "Gwenborough",
-      zipcode: "92998-3874",
-      geo: {
-        lat: "-37.3159",
-        lng: "81.1496",
-      },
-    },
-    phone: "1-770-736-8031 x56442",
-    website: "hildegard.org",
-    company: {
-      name: "Romaguera-Crona",
-      catchPhrase: "Multi-layered client-server neural-net",
-      bs: "harness real-time e-markets",
-    },
-  });
+  // const [user, setUser] = useState({
+  //   id: 1,
+  //   name: "Leanne Graham",
+  //   username: "Bret",
+  //   email: "Sincere@april.biz",
+  //   address: {
+  //     street: "Kulas Light",
+  //     suite: "Apt. 556",
+  //     city: "Gwenborough",
+  //     zipcode: "92998-3874",
+  //     geo: {
+  //       lat: "-37.3159",
+  //       lng: "81.1496",
+  //     },
+  //   },
+  //   phone: "1-770-736-8031 x56442",
+  //   website: "hildegard.org",
+  //   company: {
+  //     name: "Romaguera-Crona",
+  //     catchPhrase: "Multi-layered client-server neural-net",
+  //     bs: "harness real-time e-markets",
+  //   },
+  // });
 
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
   const getAllUsers = async () => {
     try {
-      const {data} = await axios.get("https://jsonplaceholder.typicode.com/users")
+      // const {data} = await axios.get("https://jsonplaceholder.typicode.com/users")
       // console.log(data)
       const response = await fetch("https://jsonplaceholder.typicode.com/users");
       const usersDB = await response.json();
