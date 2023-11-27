2895c9c290331dd56904862bc29649f560fedc84
itahTamar <tamar.verber@gmail.com>
2023-11-27 16:11:52 +0200

    Update User.tsx

--- 06-React/05-states/followups/tamar/user_card/src/components/User.tsx
+++ 06-React/05-states/followups/tamar/user_card/src/components/User.tsx
@@ -21,19 +21,19 @@ const handleAdd = () => {
     setCounter(counter - 1)
   }
 
-  // const randomColor = () => {
-  //   return '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
-  // }
-
-  // {counter=>5 ? bg={randomColor} : null}
+//   const randomColor = () => { 
+//     return '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+//    }
+// {counter >= 5 ? {randomColor} : null}
 
   return (
-    <div className='userCard'>
+    <div className='userCard' style={{backgroundColor: }}>
       <h3>{name}</h3>
       <h3>{lastname}</h3>
       <button onClick={handleAdd} value={id}>+</button>
       <b>{counter}</b>
       <button onClick={handleRemove} value={id}>-</button>
+      
     </div>
   )
 }
