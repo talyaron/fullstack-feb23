7512b77c3ff250f191927866392bcd9c876a2ffb
Doriel Shacham <dorielshacham@gmail.com>
2023-11-29 21:34:03 +0200

    last commit on main
    sorry gili last commit on main :sweat_smile:

--- 06-React/06-useEffect/followups/doriel/my-app/src/header/Header.tsx
+++ 06-React/06-useEffect/followups/doriel/my-app/src/header/Header.tsx
@@ -4,13 +4,13 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">Motorcycle & Parts</span>
-        <span className="headerTitleLg">BLOG</span>
+        <span className="headerTitleSm">Game Classes & Skills</span>
+        <span className="headerTitleLg">MMO's</span>
       </div>
       <img
         className="headerImg"
-        src="https://wallpaperaccess.com/full/245492.jpg"
-        alt=""
+        src="https://i.ytimg.com/vi/WtmlT7_GaTk/maxresdefault.jpg"
+        alt="Hero Img"
       />
     </div>
   );
