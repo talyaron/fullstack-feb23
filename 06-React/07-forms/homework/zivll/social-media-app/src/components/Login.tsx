318b110f0112f70fb8d8c75c0d071b9284497082
ziv leloneck <zivll1991@gmail.com>
2023-12-04 16:46:21 +0200

    exercise 1

--- 06-React/07-forms/homework/zivll/social-media-app/src/components/Login.tsx
+++ 06-React/07-forms/homework/zivll/social-media-app/src/components/Login.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [visible, setVisible] = useState(false)
     const handleSubmit = () => { };
     return (
         <div className="login">
@@ -8,7 +9,10 @@ const Login = () => {
             <h2>Login</h2>
             <form onSubmit={handleSubmit}>
                 <input type="text" name="user name" id="user name" placeholder='User name' />
-                <input type="password" name="password" id="password" placeholder='Password' />
+                <div className="password-wrapper">
+                    <img src="../public/Images/eye-slash-regular.svg" alt="show password" className='eye-slash' onClick={() => setVisible(!visible)} />
+                    <input type={visible ? "text" : "password"} name="password" id="password" placeholder='Password' className='password-field' />
+                </div>
                 <input type="submit" value="Login" className='login-btn' />
                 {/* <button className='ligin-btn'>Login</button> */}
                 <button className='new-password'>forget your password?</button>
