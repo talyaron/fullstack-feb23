da73cc7344ddad96dcb30f6c41ecd454b875f77c
ziv leloneck <zivll1991@gmail.com>
2023-12-04 11:10:36 +0200

    change color by controlled input

---
+++ 06-React/07-forms/followups/zivll/vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import ColorChange from './components/ColorChange'
+
+function App() {
+
+  return (
+    <>
+      <ColorChange />
+    </>
+  )
+}
+
+export default App
