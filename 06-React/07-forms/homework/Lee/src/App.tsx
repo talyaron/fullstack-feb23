a8cde71983fa73eaa39640d337f8ce83a32b3510
Lee_D <leegreat21@gmail.com>
2023-12-05 15:01:28 +0200

    Login-app-auth

---
+++ 06-React/07-forms/homework/Lee/src/App.tsx
@@ -0,0 +1,11 @@
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <main className="App">
+      <Login />
+    </main>
+  );
+}
+
+export default App;
---
+++ 06-React/07-forms/homework/Lee/src/components/Login.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { useRef, useState, useEffect } from "react";
+
+const Login = (): JSX.Element => {
+  const userRef = useRef<HTMLInputElement>(null);
+  const errRef = useRef<HTMLParagraphElement>(null);
+
+  const [user, setUser] = useState<string>("");
+  const [pwd, setPwd] = useState<string>("");
+  const [errMsg, setErrMsg] = useState<string>("");
+  const [success, setSuccess] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (userRef.current) {
+      userRef.current.focus();
+    }
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log(user, pwd);
+    setUser("");
+    setPwd("");
+    setSuccess(true);
+  };
+
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <p>Lee's App</p>
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              id="username"
+              placeholder="Username"
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required
+            />
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              ref={userRef}
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
+            <button>Login</button>
+          </form>
+          <p>Signup</p>
+        </section>
+      )}
+    </>
+  );
+};
+
+export default Login;
