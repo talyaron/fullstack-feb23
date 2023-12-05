bfdbe798d14ac3be24243bad4c6046640e83e4ee
danieltovbin <tovbindaniel@gmail.com>
2023-12-05 07:29:56 +0200

    Update App.tsx

--- 06-React/06-useEffect/followups/daniel/classtask/src/App.tsx
+++ 06-React/06-useEffect/followups/daniel/classtask/src/App.tsx
@@ -4,11 +4,36 @@ import WindowSize from './components/windowSize/WindowSize'
 import CountdownTimer from './components/countdownTimer/CountdownTimer'
 import TitleTask from './components/titleTask/TitleTask';
 import Practice from './components/practice/Practice';
+import { useEffect, useState } from 'react';
 
+// # advanced
+
+// 1. Create a UserCard component that fetches data from JSONPlaceholder API (https://jsonplaceholder.typicode.com/). show the users in app.tsx.
+
+// 2. create UserPosts component that fetches and displays a list of posts for a given user ID using the JSONPlaceholder API (https://jsonplaceholder.typicode.com/). Add this component to each user card, and only show it after that user card has been clicked. show this component only when the user card is clicked.
+
+// # Expactions
+
+// 1.  Each of the components should only fetch data when it mounts.
 
 
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const fetchUserData = ()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response =>{
+      return response.json()
+    })
+    .then(data => {
+      setUsers(data)
+    })
+  }
+
+  useEffect(()=>{
+    fetchUserData()
+  },[])
  
   return (
     <>
@@ -23,6 +48,14 @@ function App() {
     <WindowSize /> */}
 
     <Practice/>
+    <WindowSize />
+    <div>
+      <ul>
+        {users.map(user => (
+          <p key={user.id}>{user.name}</p>
+        ))}
+      </ul>
+    </div>
     </>
   )
 }
