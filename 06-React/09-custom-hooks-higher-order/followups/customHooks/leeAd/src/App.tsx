fa64c21dc338d8eb7d6ac7602e7ce2269cb9805f
Lee_D <leegreat21@gmail.com>
2023-12-12 12:41:13 +0200

    timer-app

---
+++ 06-React/09-custom-hooks-higher-order/followups/customHooks/leeAd/src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./App.css";
+import React from "react";
+
+function App() {
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+
+  React.useEffect(() => {
+    let interval: number | null = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval!);
+    }
+
+    return () => clearInterval(interval!);
+  }, [timerOn]);
+
+  return (
+    <div className="App">
+      <div>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div>
+        {!timerOn && time == 0 && (
+          <button onClick={() => setTimerOn(true)}>Start</button>
+        )}
+        {timerOn && <button onClick={() => setTimerOn(false)}>Stop</button>}
+
+        {!timerOn && time !== 0 && (
+          <button onClick={() => setTimerOn(true)}>Resume</button>
+        )}
+        {!timerOn && time > 0 && (
+          <button onClick={() => setTime(0)}>Reset</button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
