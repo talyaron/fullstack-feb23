2b4d638537c9dc93b400c8dffd4902cd9c01d7a4
Lee_D <leegreat21@gmail.com>
2023-12-12 14:53:19 +0200

    folders-order

--- 06-React/09-custom-hooks-higher-order/followups/customHooks/leeAd/src/App.tsx
+++ 06-React/09-custom-hooks-higher-order/followups/customHooks/leeAd/src/App.tsx
@@ -1,64 +1,11 @@
-// App.tsx
 import React from "react";
 import "./App.css";
-import useTimer from "./hooks/useTimer";
+import Timer from "./components/Timer";
 
 const App: React.FC = () => {
-  const { time, timerOn, startTimer, stopTimer, resumeTimer, resetTimer } =
-    useTimer();
-
   return (
     <div className="App">
-      <div>
-        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
-      </div>
-      <div>
-        {!timerOn && time === 0 && (
-          <button
-            className="start"
-            onClick={() => {
-              startTimer();
-            }}
-          >
-            Start
-          </button>
-        )}
-
-        {timerOn && (
-          <button
-            className="stop"
-            onClick={() => {
-              stopTimer();
-            }}
-          >
-            Stop
-          </button>
-        )}
-
-        {!timerOn && time !== 0 && (
-          <button
-            className="resume"
-            onClick={() => {
-              resumeTimer();
-            }}
-          >
-            Resume
-          </button>
-        )}
-
-        {!timerOn && time > 0 && (
-          <button
-            className="reset"
-            onClick={() => {
-              resetTimer();
-            }}
-          >
-            Reset
-          </button>
-        )}
-      </div>
+      <Timer />
     </div>
   );
 };
