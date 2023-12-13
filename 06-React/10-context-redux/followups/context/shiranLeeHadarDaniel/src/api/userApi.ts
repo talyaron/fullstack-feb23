5d8ea1542e9797bab8a6db6590f60f91a5ba2997
shiranlasry <128288093+shiranlasry@users.noreply.github.com>
2023-12-13 21:52:04 +0200

    finish followup

--- 06-React/10-context-redux/followups/context/shiranLeeHadarDaniel/src/api/userApi.ts
+++ 06-React/10-context-redux/followups/context/shiranLeeHadarDaniel/src/api/userApi.ts
@@ -1,3 +1,8 @@
 import axios from "axios";
 
+export const getUserdb = async () => {
+  const { data } = await axios.get( "https://jsonplaceholder.typicode.com/users/1");
+
+  return data;
+};
  
--- 06-React/10-context-redux/followups/context/shiranLeeHadarDaniel/src/components/card/Card.tsx
+++ 06-React/10-context-redux/followups/context/shiranLeeHadarDaniel/src/components/card/Card.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
+import Home from "../home/Home";
+import { useContext } from "react";
+import { UserContext } from "../../context/userContext";
 
 const Card = () => {
-  return (
-    <div>
-      <h1>Card</h1>
-    </div>
-  )
-}
+    const user = useContext(UserContext);
 
-export default Card
+
+    return (
+        <div>
+            <h1>Card</h1>
+            <h2>Hello from CARD {user?.name}</h2>
+            <NavLink to="/minicard">Mini Card</NavLink>
+            <NavLink to="/">Home</NavLink>
+           
+            
+          
+        </div>
+    );
+};
+
+export default Card;
