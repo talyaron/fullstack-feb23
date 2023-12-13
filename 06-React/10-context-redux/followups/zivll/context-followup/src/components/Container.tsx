0a107d391100f789bc293e278a71f34957f8fd07
ziv leloneck <zivll1991@gmail.com>
2023-12-13 20:57:16 +0200

    adjustments

--- 06-React/10-context-redux/followups/zivll/context-followup/src/components/Container.tsx
+++ 06-React/10-context-redux/followups/zivll/context-followup/src/components/Container.tsx
@@ -9,6 +9,11 @@ const Container = () => {
         <div>
             <p>
                 my name is: {user ? user.name : null}<br />
+                my address is: {user ? user.address.city : null},&nbsp;
+                {user ? user.address.street : null},&nbsp;
+                {user ? user.address.suite : null},&nbsp;
+                {user ? user.address.zipcode : null}<br />
+                {/* {/* and my zipcode is: {user ? user.zipcode : null}<br /> */}
                 you can reach me at: {user ? user.phone : null}<br />
             </p>
         </div>
