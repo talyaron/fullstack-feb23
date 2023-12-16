c13152d1c72a431295f3fdb1064dcbe993003eee
hadaritzhaki <127751975+hadaritzhaki@users.noreply.github.com>
2023-12-17 01:16:42 +0200

    easy

---
+++ 06-React/09-custom-hooks-higher-order/followups/customHooks/hadar/hadar_app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Title_1 from './components/Title_1'
+import Title_2 from './components/Title_2'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+    <h1>hi</h1>
+    <Title_1/>
+    <Title_2/>
+      
+    </>
+  )
+}
+
+export default App
