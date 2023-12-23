ec99f642e65c5af72af1fc6f7c99521b95bbde0a
Lee_D <leegreat21@gmail.com>
2023-12-23 16:18:07 +0200

    home-page

--- 06-React/12-mini-project/Lee's-App/src/App.tsx
+++ 06-React/12-mini-project/Lee's-App/src/App.tsx
@@ -1,63 +1,10 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import AppRouter from "./routers/AppRouter";
 
 function App() {
-  const [dogBreeds, setDogBreeds] = useState<string[]>([]);
-  const [filteredBreeds, setFilteredBreeds] = useState<string[]>([]);
-  const [search, setSearch] = useState<string | null>(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    async function fetchData() {
-      setLoading(true);
-
-      const data = await fetch("https://dog.ceo/api/breeds/list/all").then(
-        (res) => res.json()
-      );
-
-      const breeds = Object.keys(data.message);
-      setDogBreeds(breeds);
-      setLoading(false);
-    }
-
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    // Filter dog breeds based on the search term
-    const filtered =
-      search && search.trim() !== ""
-        ? dogBreeds.filter((breed) =>
-            breed.toLowerCase().includes(search.toLowerCase())
-          )
-        : dogBreeds;
-
-    setFilteredBreeds(filtered);
-  }, [search, dogBreeds]);
-
   return (
     <div className="App">
       <AppRouter />
-      {window.location.pathname === "/gallery" && (
-        <>
-          <input
-            type="search"
-            placeholder="Search"
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            <></>
-            // <ul>
-            //   {filteredBreeds.map((breed) => (
-            //     <li key={breed}>{breed}</li>
-            //   ))}
-            // </ul>
-          )}
-        </>
-      )}
     </div>
   );
 }
--- 06-React/12-mini-project/Lee's-App/src/pages/hp.tsx
+++ 06-React/12-mini-project/Lee's-App/src/pages/hp.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../scss/hp.scss";
 
 const Home: React.FC = () => {
-  return <div>Home Page</div>;
+  const dogBreeds = [
+    { id: 1, name: "Bagle", image: "../src/imgs/dachshund.jpeg" },
+    { id: 2, name: "Jango", image: "../src/imgs/jango.jpeg" },
+  ];
+
+  return (
+    <div className="hp-container">
+      <h1>Dog Breeds Gallery</h1>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {dogBreeds.map((breed) => (
+          <div key={breed.id} className="dog-breed-card">
+            <img
+              src={breed.image}
+              alt={breed.name}
+              className="dog-breed-image"
+            />
+            <h3>{breed.name}</h3>
+          </div>
+        ))}
+      </div>
+      <Link to="/gallery">
+        <button style={{ marginTop: "20px" }}>Go to Gallery</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
