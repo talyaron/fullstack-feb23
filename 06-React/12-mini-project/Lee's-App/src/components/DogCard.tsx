bd12d621a7f66334f9f273481611663fb4017b78
Lee_D <leegreat21@gmail.com>
2023-12-23 16:23:37 +0200

    Gallery-styling

--- 06-React/12-mini-project/Lee's-App/src/components/DogCard.tsx
+++ 06-React/12-mini-project/Lee's-App/src/components/DogCard.tsx
@@ -27,11 +27,11 @@ const DogCard: React.FC<DogCardProps> = ({ breed }) => {
       style={{
         border: "1px solid #ccc",
         padding: "10px",
-        margin: "10px",
+        margin: "7px",
         borderRadius: "8px",
         textAlign: "center",
-        width: "270px",
-        height: "270px",
+        width: "250px",
+        height: "300px",
         boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
       }}
     >
@@ -47,7 +47,10 @@ const DogCard: React.FC<DogCardProps> = ({ breed }) => {
         // alt={breed}
       />
       <h3>{breed}</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit..</p>
+      <p>
+        The term 'a dog is a man's best friend' was first used way back in 1789
+        by King Frederick of Prussia..
+      </p>
     </div>
   );
 };
