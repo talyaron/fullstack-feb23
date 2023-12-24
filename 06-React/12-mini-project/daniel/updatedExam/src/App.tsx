ccb142592b35ce92e1d5574fa757930563a9f318
danieltovbin <tovbindaniel@gmail.com>
2023-12-24 19:16:54 +0200

    clearRoutes

--- 06-React/12-mini-project/daniel/updatedExam/src/App.tsx
+++ 06-React/12-mini-project/daniel/updatedExam/src/App.tsx
@@ -1,24 +1,13 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import DogsGallery from './pages/Gallery/DogsGallery';
-import DogDiscussion from './pages/DogDiscussion/DogDiscussion';
-import NavbarWrapper from './views/layouts/NavbarWrapper';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/routes';
 
 
 
 function App() {
 
-  return (
-    <>
-      <Router>
-      <NavbarWrapper />
-      <Routes>
-        <Route path="dogs-gallery" element={<DogsGallery />} />
-        <Route path="dog-discussion/:breedName" element={<DogDiscussion />} />
-      </Routes>
-    </Router>
-    </>
-  )
+  return <RouterProvider router={router} />
+
 }
 
 export default App
--- 06-React/12-mini-project/daniel/updatedExam/src/pages/DogDiscussion/DogDiscussion.tsx
+++ 06-React/12-mini-project/daniel/updatedExam/src/pages/DogDiscussion/DogDiscussion.tsx
@@ -30,18 +30,19 @@ const DogDiscussion = () => {
   return (
     <div>
       <TopBar />
-      <h2>{breedName}</h2>
+      <h1 style={{margin: 0}}>{breedName}</h1>
       <div style={{ display: 'flex', gap: '2%', height: '700px', width: '1200px' }}>
         {breedImg && (
           <div style={{ flex: '0 0 73%', height: '100%' }}>
             <img src={breedImg} alt={breedName || 'Breed Image'} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
           </div>
         )}
-        <div style={{ flex: '0 0 25%', height: '100%', border: '1.5px solid gray', borderRadius: '9px' }}>
-          <h6 style={{ backgroundColor: 'beige', margin: 0, padding: '2px', borderRadius: '9px 9px 0px 0px' }}>Chat</h6>
+        <div style={{ position: 'relative', flex: '0 0 25%', height: '100%', border: '1.5px solid gray', borderRadius: '9px' }}>
+          <h6 style={{ backgroundColor: 'beige', margin: 0, padding: '4px', borderRadius: '9px 9px 0px 0px' }}>Chat</h6>
           <p>{message}</p>
-          <div>
-            <input style={{padding:'2px'}} type="text" placeholder="Type your message here" onChange={handleInputChange} />
+          <div style={{ position: 'absolute', bottom: 10, width: '100%', display: 'flex', justifyContent: ' space-evenly', alignItems: 'flex-end' }}>
+            <input style={{ width: '80%', height: '22px', padding: '2px', borderRadius: '4px', border: '1px solid gray' }} type="text" placeholder="Type your message here" onChange={handleInputChange} />
+            <button style={{ backgroundColor: '#1976d2', height: '30px', color: 'white', padding: '1px' }}>Send</button>
           </div>
         </div>
       </div>
