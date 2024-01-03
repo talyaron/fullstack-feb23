a9fb5cc6653363926dff4e6329f35280c01b12f5
gili <gilimena95@gmail.com>
2024-01-03 19:18:12 +0200

    WIP

--- 05-node/13-review/review/server/server.ts
+++ 05-node/13-review/review/server/server.ts
@@ -11,6 +11,29 @@ const PORT = process.env.PORT || 4000
 app.use(express.json())
 
 // app.use(express.static('public'))
+import booksRoutes from "./API/books/booksRoutes"
+import connection from './DB/database';
+app.use("/api/v1/books", booksRoutes)
+
+app.post("/api/create-database", async (req, res) => {
+    try {
+        const {adminPassword} = req.body;
+        if (!adminPassword) throw new Error("no password")
+        if (adminPassword === "123456") {
+            const query = "CREATE DATABASE library"
+            connection.query(query, (err, results) => {
+                try {
+                    if(err) throw err;
+                    res.send({ok: true, message: "DB was created!"})
+                } catch (error) {
+                    res.status(500).send({ok:false, error})
+                }
+            })
+        }
+    } catch (error) {
+        res.status(500).send({ok:false, error})
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
