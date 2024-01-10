f78dabe60ea66daecf028f49b046638407d1d34b
navered <128045047+navered@users.noreply.github.com>
2024-01-10 19:16:58 +0200

    push
    push

--- 06-React/14-practice/class-task/music-player/client/src/components/Register.tsx
+++ 06-React/14-practice/class-task/music-player/client/src/components/Register.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { register } from "module"
+import { useState } from "react"
 
-const Register = () => {
-    return (
-        <div>Register</div>
-    )
-}
 
-export default Register
+
+const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+
+  // This is redux
+  const [user, setUser] = useState(null)
+
+  const handleSubmit = async (ev: React.FormEvent<HTMLFormElement>) => {
+    try {
+      ev.preventDefault()
+      const response = await register(email, password)
+      console.log(response)
+
+      setUser(response?.data.results[0])
+      // if (data.ok) {
+      //   navigate("/home")
+      // }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  // getUser
+  // go to server, look for user with the id in cookie
+  // if user return = set State and add user
+  // is no user navigate to login
+  useEffect(() => {
+    // getUser()
+  },[])
