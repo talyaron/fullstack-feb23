dae357622af4cdb5ab192d3946000d2cb41f4e07
navered <128045047+navered@users.noreply.github.com>
2024-02-07 16:24:17 +0200

    push

--- 06-React/14-practice/class-task/music-player/client/src/components/NavBar.tsx
+++ 06-React/14-practice/class-task/music-player/client/src/components/NavBar.tsx
@@ -12,9 +12,7 @@ const Navbar = () => {
         <img className="logo" src={logo} alt="logo" />
         <NavLink to="/home-page">Home</NavLink>
         <NavLink to="/about-us">About Us</NavLink>
-        <NavLink to="/genres">Genres</NavLink>
         <NavLink to="/playlist">playlist</NavLink>
-        <NavLink to="/contact">Contact Us </NavLink>
         <NavLink to="/login-page">
           <img className="user" src={userImage} alt="" />   </NavLink>
 
