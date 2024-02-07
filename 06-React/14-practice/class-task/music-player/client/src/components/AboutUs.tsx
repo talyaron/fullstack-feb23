05bc10fd0dd86ee22eba63266ae1efe73a1d3295
ziv leloneck <zivll1991@gmail.com>
2024-02-07 16:56:25 +0200

    ccc

--- 06-React/14-practice/class-task/music-player/client/src/components/AboutUs.tsx
+++ 06-React/14-practice/class-task/music-player/client/src/components/AboutUs.tsx
@@ -3,6 +3,7 @@ const AboutUs = () => {
     return (
         <div className="about-us-container">
             <div className="video-background">
+                <iframe width="1000" height="562" src="https://www.youtube.com/embed/WCcRz-WOw3E" title="Spotify Inspired PowerPoint Presentation" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <div className="overlay"></div> {/* השכבת הרקע הצללה שחורה */}
                 <video autoPlay muted loop>
 
