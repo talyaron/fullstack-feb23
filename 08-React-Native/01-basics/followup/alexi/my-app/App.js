451dae1f315ddf71387ac413f12bf559ff1e5411
inalexa81 <athegreat5@hotmail.com>
2024-02-28 19:22:18 +0200

    alex

---
+++ 08-React-Native/01-basics/followup/alexi/my-app/App.js
@@ -0,0 +1,168 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View
+    style={{
+        padding: 50,
+        backgroundColor: 'skyblue',
+        width: '100%',
+        height: 700,
+        justifyContent: 'start',
+        alignItems: 'stretch',
+    }}>
+    <View
+        style={{
+            // backgroundColor: 'green',
+            // fontSize: '100px',
+            height: 40,
+            // flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: 10
+        }}>
+        <Text>Checkout</Text>
+    </View>
+    <View
+        style={{
+            // backgroundColor: 'blue',
+            // flex: 1,
+            height: 40,
+            justifyContent: 'center',
+            alignItems: 'start',
+            // marginBottom: 10
+        }}>
+        <Text>Delivery</Text>
+    </View>
+    <View
+        style={{
+            backgroundColor: 'white',
+            // flex: 1,
+            height: 40,
+            justifyContent: 'center',
+            alignItems: 'censtartter',
+            borderTopLeftRadius: 15,
+            borderTopRightRadius: 15,
+            borderBottomLeftRadius: 15,
+            borderBottomRightRadius: 15,
+            padding: 10
+        }}>
+        <Text>Delivery Address</Text>
+    </View>
+    <View
+        style={{
+            backgroundColor: 'white',
+            // flex: 1,
+            height: 40,
+            justifyContent: 'center',
+            alignItems: 'censtartter',
+            borderTopLeftRadius: 15,
+            borderTopRightRadius: 15,
+            borderBottomLeftRadius: 15,
+            borderBottomRightRadius: 15,
+            marginTop: 10,
+            marginBottom: 20,
+            padding: 10
+        }}>
+        <Text>Phone</Text>
+    </View>
+    <View
+        style={{
+            // backgroundColor: 'blue',
+            // flex: 1,
+            height: 40,
+            justifyContent: 'center',
+            alignItems: 'start',
+            // marginBottom: 10
+        }}>
+        <Text>Payment</Text>
+    </View>
+    <View
+        style={{
+            backgroundColor: 'white',
+            // flex: 1,
+            height: 40,
+            justifyContent: 'center',
+            alignItems: 'censtartter',
+            borderTopLeftRadius: 15,
+            borderTopRightRadius: 15,
+            borderBottomLeftRadius: 15,
+            borderBottomRightRadius: 15,
+            // marginTop: 10,
+            marginBottom: 10,
+            padding: 10
+        }}>
+        <Text>Card Holder Name</Text>
+    </View>
+    <View
+        style={{
+            backgroundColor: 'white',
+            // flex: 1,
+            height: 40,
+            justifyContent: 'center',
+            alignItems: 'censtartter',
+            borderTopLeftRadius: 15,
+            borderTopRightRadius: 15,
+            borderBottomLeftRadius: 15,
+            borderBottomRightRadius: 15,
+            // marginTop: 10,
+            marginBottom: 10,
+            padding: 10
+        }}>
+        <Text>Card Holder Name</Text>
+    </View>
+    <View     style={{
+        flexDirection: 'row',
+        justifyCo'__registeredStyleBrand
+    }}
+    >
+    <View
+        style={{
+            backgroundColor: 'white',
+            // flex: 1,
+            height: 40,
+            justifyContent: 'center',
+            alignItems: 'censtartter',
+            borderTopLeftRadius: 15,
+            borderTopRightRadius: 15,
+            borderBottomLeftRadius: 15,
+            borderBottomRightRadius: 15,
+            // marginTop: 10,
+            marginBottom: 10,
+            padding: 10
+        }}>
+        <Text>Card Holder Name</Text>
+        </View>
+        <View
+        style={{
+            backgroundColor: 'white',
+            // flex: 1,
+            height: 40,
+            justifyContent: 'center',
+            alignItems: 'censtartter',
+            borderTopLeftRadius: 15,
+            borderTopRightRadius: 15,
+            borderBottomLeftRadius: 15,
+            borderBottomRightRadius: 15,
+            // marginTop: 10,
+            marginBottom: 10,
+            padding: 10
+        }}>
+        <Text>Card Holder Name</Text>
+        </View>
+    </View>
+</View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "pink",
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     // flexDirection: 'row',
+
+//   },
+// });
