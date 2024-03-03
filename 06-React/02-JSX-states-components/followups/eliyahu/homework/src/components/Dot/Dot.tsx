af43ab51d1707bffdfdd28fd6ea75b3f2c48c8eb
Eliyaho basson <basson131@gmail.com>
2024-03-03 14:30:10 +0200

    extre homework lesson 48

--- 06-React/02-JSX-states-components/followups/eliyahu/homework/src/components/Dot/Dot.tsx
+++ 06-React/02-JSX-states-components/followups/eliyahu/homework/src/components/Dot/Dot.tsx
@@ -2,22 +2,36 @@ import { useState } from 'react'
 import './Dot.scss'
 
 const Dot = () => {
-  let [xPosition, setXPosition] = useState(0)
+  let [xPosition, setXPosition] = useState<number>(0)
   let [yPosition, setYPosition] = useState(0)
+  const [counter, setCounter] = useState(0)
+  const [clickCounter, setClickCounter] = useState(0)
 
   const getCursorPosition = (event: any) => {
     const x = event.clientX
-    setXPosition(xPosition = x)
+    setXPosition(xPosition = x - 6)
     const y = event.clientY
-    setYPosition(yPosition = y)
+    setYPosition(yPosition = y - 6)
     console.log(x, y)
+  }
+
+  const addTow = () => {
+    setCounter(counter + 2)
+    setClickCounter(clickCounter + 1)
+  }
+  const removeTow = () => {
+    setCounter(counter - 2)
+    setClickCounter(clickCounter + 1)
   }
 
   return (
     <div className='wrapper' onMouseMove={getCursorPosition}>
 
-      < div className='dot' style={{ left: xPosition + 'px', top: yPosition + 'px' }}></div>
-
+      < div className='dot' style={{ left: + xPosition + 'px', top: yPosition + 'px' }}></div>
+      <p>count:{counter}</p>
+      <button onClick={addTow}>+2</button>
+      <button onClick={removeTow}>-2</button>
+      <p>click count:{clickCounter}</p>
     </div>
   )
 }
