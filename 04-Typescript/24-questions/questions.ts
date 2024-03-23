4554aae1e07541b311d0d096699d964cbc8ba13c
gili <gilimena95@gmail.com>
2024-03-23 19:16:03 +0200

    Create questions.ts

---
+++ 04-Typescript/24-questions/questions.ts
@@ -0,0 +1,42 @@
+// [...] => { 15: [...], 21: [...] }
+type Person = {
+    name: string,
+    age: number
+};
+
+function groupByAge(people: Person[]): Record<number, Person[]> {
+    return {};
+}
+
+
+//1. Write a JavaScript function to check if a given string is a palindrome (reads the same forwards and backwards). 
+
+function isPalindrome(string: string): boolean {
+    let result: boolean = false
+
+    return result
+}
+
+// 2. Write a JavaScript program to reverse a given string. 
+
+function reverseString(string: string): string {
+    let result: string = ""
+
+    return result
+}
+
+// 3. Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers. 
+
+function filterEvenNumbers(numbers: number[]): number[] {
+
+    return []
+}
+
+// 4. Write a JavaScript program to find the largest element in a nested array. 
+function findLargestElement(nestedArray) {
+
+    let largest = nestedArray[0][0];
+
+    return largest;
+
+} 
